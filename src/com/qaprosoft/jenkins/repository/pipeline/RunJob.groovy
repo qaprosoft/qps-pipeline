d520a4c0934bf392ecb5e6ab48862695e2983013
Maksim Sarychau <shark.raph.work@gmail.com>
2018-04-04 21:01:59 +0200

    Added new env

--- src/com/qaprosoft/jenkins/repository/pipeline/RunJob.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/RunJob.groovy
@@ -238,6 +238,9 @@ def setupGoalsForAndroid(Map<String, String> goalMap) {
 	    break
 	  case "QA_RENDERED_FILED_DELETION_PROD":
 	    goalMap.put("capabilities.app", "s3://smule.qaprosoft.com/SingAndroid_Sa15040builds_SingAndroidQaSa15040prod/sing_android-playstore-sa_15040-prod-.*.apk")
+	    break
+	  case "QA_SA15312_INDIAN_LANG_BATCH1_PROD":
+	    goalMap.put("capabilities.app", "s3://smule.qaprosoft.com/SingAndroid_Sa15312indianLangBatch1builds_SingAndroidQaSa15312indianLangBatch1pr/sing_android-playstore-SA_15312_indian_lang_batch1-prod-.*.apk")
 	    break	    
 	  default:
 	    throw new RuntimeException("Unknown env: " + params["env"]);
