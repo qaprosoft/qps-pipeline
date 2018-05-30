8a5f4db528668fd3bdf04f63589be367bb3d4d14
vdelendik <vadim.delendik@gmail.com>
2018-05-31 00:39:04 +0300

    test

--- src/com/qaprosoft/jenkins/repository/jobdsl/v2/Creator2.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/v2/Creator2.groovy
@@ -1,11 +1,18 @@
 package com.qaprosoft.jenkins.repository.jobdsl.v2
 
-def creator = new Creator3()
+def creator = new Creator3(this)
 creator.test()
 
 class Creator3 {
+	//pipeline context to provide access to existing pipeline methods like echo, sh etc...
+	protected def context
+	
+	public Creator3(context) {
+		this.context = context
+	}
+	
 	public void test() {
 		 //do nothing
-		echo "qwe"
+		context.echo "qwe"
 	}
 }
