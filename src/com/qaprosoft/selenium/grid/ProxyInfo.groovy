f798ea830ace44ddc2c67de83c5cdef075bfb756
vdelendik <vadim.delendik@gmail.com>
2018-05-31 11:27:59 +0300

    improve object modeling

--- src/com/qaprosoft/selenium/grid/ProxyInfo.groovy
+++ src/com/qaprosoft/selenium/grid/ProxyInfo.groovy
@@ -4,14 +4,13 @@ import groovy.json.JsonSlurper;
 
 class ProxyInfo {
 	//TODO: reused grid/admin/ProxyInfo to get atual list of iOS/Android devices
-	public static List<String> getDevicesList(context, String platform) {
+	public static List<String> getDevicesList(String selenium, String platform) {
 		def baseDeviceList = ["DefaultPool", "ANY"]
 		//context.println(baseDeviceList)
 		
 		def deviceList = []
 		//TODO: reuse selenium host/port/protocol from env jobVars
-		def envVars = context.env.getEnvironment()
-		def proxyInfoUrl = envVars.get("SELENIUM_HOST")
+		def proxyInfoUrl = selenium
 		context.println("ProxyInfo url: ${proxyInfoUrl}")
 		def json = new JsonSlurper().parse("http://smule.qaprosoft.com:14444/grid/admin/ProxyInfo".toURL())
 		//context.println(json)
