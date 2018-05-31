1f7f6256ec7d47907a2ccbb2f165b17f89334ef4
vdelendik <vadim.delendik@gmail.com>
2018-05-31 11:29:15 +0300

    removed context

--- src/com/qaprosoft/selenium/grid/ProxyInfo.groovy
+++ src/com/qaprosoft/selenium/grid/ProxyInfo.groovy
@@ -6,17 +6,17 @@ class ProxyInfo {
 	//TODO: reused grid/admin/ProxyInfo to get atual list of iOS/Android devices
 	public static List<String> getDevicesList(String selenium, String platform) {
 		def baseDeviceList = ["DefaultPool", "ANY"]
-		//context.println(baseDeviceList)
+		//println(baseDeviceList)
 		
 		def deviceList = []
 		//TODO: reuse selenium host/port/protocol from env jobVars
 		def proxyInfoUrl = selenium
-		context.println("ProxyInfo url: ${proxyInfoUrl}")
+		println("ProxyInfo url: ${proxyInfoUrl}")
 		def json = new JsonSlurper().parse("http://smule.qaprosoft.com:14444/grid/admin/ProxyInfo".toURL())
-		//context.println(json)
+		//println(json)
 		json.each {
 			if (platform.equalsIgnoreCase(it.configuration.capabilities.platform)) {
-				context.println("platform: " + it.configuration.capabilities.platform[0] + "; device: " + it.configuration.capabilities.browserName[0])
+				println("platform: " + it.configuration.capabilities.platform[0] + "; device: " + it.configuration.capabilities.browserName[0])
 				deviceList.add(it.configuration.capabilities.browserName[0]);
 			}
 		}
