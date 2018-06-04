a1a33ba33e1245bef2f216de33a3a3b518cd3e21
tsvrko <UCs7JCmP>
2018-06-04 14:51:28 +0300

    queued testrun response content extracted

--- src/com/qaprosoft/zafira/ZafiraClient.groovy
+++ src/com/qaprosoft/zafira/ZafiraClient.groovy
@@ -70,7 +70,7 @@ class ZafiraClient {
             url: this.serviceURL + "/api/tests/runs/queue"
 			
 		//TODO: analyze response and put info about registered or not registered test run here
-        context.echo "Queued testrun: ${response}"
+        context.echo "Queued testrun: ${response.content}"
     }
 
 	public void smartRerun(jobParams) {
