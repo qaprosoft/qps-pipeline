1dee0b039913706d52a757399344ef4f485a09fd
tsvrko <tsvrko@gmail.com>
2018-06-20 18:54:06 +0300

    upstream job output #15

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Runner.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Runner.groovy
@@ -88,7 +88,7 @@ class Runner extends Executor {
 	public void runJob() {
 		jobParams = initParams(context.currentBuild)
 		jobVars = initVars(context.env)
-        String JOB_URL = vars.get("JOB_URL")
+        String JOB_URL = jobVars.get("JOB_URL")
         context.println("CAUSE " + context.currentBuild.rawBuild.getCauses())
 
 		context.currentBuild.rawBuild.actions.each { action ->
