4da8295f90e8a392a1777eb43fc3d631989b4840
vdelendik <vadim.delendik@gmail.com>
2018-06-21 01:45:02 +0300

    return map after sorting

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Runner.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Runner.groovy
@@ -594,7 +594,7 @@ class Runner extends Executor {
 	
 	
 	protected void sortPipelineList() {
-		listPipelines.sort { map1, map2 -> !map1.order ? !map2.order ? 0 : 1 : !map2.order ? -1 : map1.order.toInteger() <=> map2.order.toInteger() }
+		listPipelines = listPipelines.sort { map1, map2 -> !map1.order ? !map2.order ? 0 : 1 : !map2.order ? -1 : map1.order.toInteger() <=> map2.order.toInteger() }
 	}
 
 	protected void parsePipeline(jobVars, jobParams, String filePath) {
