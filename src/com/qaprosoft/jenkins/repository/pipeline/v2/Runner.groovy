cdc472d92654da83373518e2e173ffc9bf2c6d9d
tsvrko <tsvrko@gmail.com>
2018-07-06 13:22:41 +0300

    static modificator removed

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Runner.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Runner.groovy
@@ -33,7 +33,6 @@ class Runner extends Executor {
 		super(context)
 		scmClient = new GitHub(context)
         configurator = new Configurator()
-		Configurator.load(context)
 	}
 	
 	public void runCron() {
