1108c844876f33fe276fd589eaee61c55fa4186c
tsvrko <tsvrko@gmail.com>
2018-07-06 18:07:59 +0300

    load call output

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Configurator.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Configurator.groovy
@@ -131,9 +131,12 @@ class Configurator {
 
     @NonCPS
     public void load() {
+        context.println("LOAD METHOD CALLED")
         //1. load all Parameter key/values to args
         Parameter.values().each { parameter ->
             args.put(parameter.getKey(), parameter.getValue())
+            context.println(parameter.getKey())
+            context.println(parameter.getValue())
         }
         //2. load all string keys/values from env
         def envVars = context.env.getEnvironment()
