dac45ab79db0b4723d27502054b585de6442d5a0
tsvrko <tsvrko@gmail.com>
2018-07-10 16:00:49 +0300

    run using configurator #4

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Scanner.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Scanner.groovy
@@ -20,7 +20,7 @@ class Scanner extends Executor {
 		
 		context.node('master') {
 			context.timestamps {
-				scmClient.clone(jobParams, jobVars)
+				scmClient.clone()
 				this.scan(jobParams, jobVars)
 				this.clean()
 			}
