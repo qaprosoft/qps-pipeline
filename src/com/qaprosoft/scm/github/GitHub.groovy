5b1c1e5bd58e5a69005f176dbd13e4b8e056570a
tsvrko <tsvrko@gmail.com>
2018-07-10 19:24:25 +0300

    GitHub class params got from enum

--- src/com/qaprosoft/scm/github/GitHub.groovy
+++ src/com/qaprosoft/scm/github/GitHub.groovy
@@ -15,22 +15,20 @@ class GitHub implements ISCM {
 		context.stage('Checkout GitHub Repository') {
 			context.println("GitHub->clone")
 
-			boolean fork = Configurator.get("fork").toBoolean()
-			def branch = Configurator.get("branch")
-			def project = Configurator.get("project")
+			boolean fork = Configurator.get(Configurator.Parameter.FORK).toBoolean()
+            def branch = Configurator.get(Configurator.Parameter.BRANCH)
+			def PROJECT = Configurator.get(Configurator.Parameter.PROJECT)
+            def userId = Configurator.get(Configurator.Parameter.BUILD_USER_ID)
+			def GITHUB_SSH_URL = Configurator.get(Configurator.Parameter.GITHUB_SSH_URL)
+			def GITHUB_HOST = Configurator.get(Configurator.Parameter.GITHUB_HOST)
 
-			def GITHUB_SSH_URL = Configurator.get("GITHUB_SSH_URL")
-			def userId = Configurator.get("BUILD_USER_ID")
-			//context.println("userId: ${userId}")
-			def GITHUB_HOST = Configurator.get("GITHUB_HOST")
-
-			def gitUrl = "${GITHUB_SSH_URL}/${project}"
-			context.println("gitUrl: " + gitUrl)
+			def GIT_URL = "${GITHUB_SSH_URL}/${PROJECT}"
+			context.println("GIT_URL: " + GIT_URL)
 			context.println("forked_repo: " + fork)
 			if (!fork) {
 				context.checkout scm: [$class: 'GitSCM', branches: [[name: '${branch}']], \
 						doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 15], [$class: 'CloneOption', noTags: true, reference: '', shallow: true, timeout: 15]], \
-						submoduleCfg: [], userRemoteConfigs: [[url: gitUrl]]], \
+						submoduleCfg: [], userRemoteConfigs: [[url: GIT_URL]]], \
 						changelog: false, poll: false
 			} else {
 				def token_name = 'token_' + "${userId}"
@@ -44,19 +42,19 @@ class GitHub implements ISCM {
 					token_value =  tempToken
 				}
 				if (token_value != null) {
-					gitUrl = "https://${token_value}@${GITHUB_HOST}/${userId}/${project}"
-					context.println "fork repo url: ${gitUrl}"
+					GIT_URL = "https://${token_value}@${GITHUB_HOST}/${userId}/${PROJECT}"
+					context.println "fork repo url: ${GIT_URL}"
 					context.checkout scm: [$class: 'GitSCM', branches: [[name: '${branch}']], \
 							doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 15], [$class: 'CloneOption', noTags: true, reference: '', shallow: true, timeout: 15]], \
-							submoduleCfg: [], userRemoteConfigs: [[url: gitUrl]]], \
+							submoduleCfg: [], userRemoteConfigs: [[url: GIT_URL]]], \
 							changelog: false, poll: false
 				} else {
 					throw new RuntimeException("Unable to run from fork repo as ${token_name} token is not registered on CI!")
 				}
 			}
 			//TODO: remove git_branch after update ZafiraListener: https://github.com/qaprosoft/zafira/issues/760
-			Configurator.set("git_url", gitUrl)
-			Configurator.set("scm_url", gitUrl)
+			Configurator.set("git_url", GIT_URL)
+			Configurator.set("scm_url", GIT_URL)
 			//TODO: init git_commit as well
 		}
 	}
