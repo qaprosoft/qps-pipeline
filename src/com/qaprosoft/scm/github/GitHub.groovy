9063255f88773fdf2de9953c8f7970e9b8af0c49
tsvrko <tsvrko@gmail.com>
2018-07-10 19:26:05 +0300

    GitHub class params got from enum

--- src/com/qaprosoft/scm/github/GitHub.groovy
+++ src/com/qaprosoft/scm/github/GitHub.groovy
@@ -18,7 +18,7 @@ class GitHub implements ISCM {
 			boolean fork = Configurator.get(Configurator.Parameter.FORK).toBoolean()
             def branch = Configurator.get(Configurator.Parameter.BRANCH)
 			def PROJECT = Configurator.get(Configurator.Parameter.PROJECT)
-            def userId = Configurator.get(Configurator.Parameter.BUILD_USER_ID)
+            def USER_ID = Configurator.get(Configurator.Parameter.BUILD_USER_ID)
 			def GITHUB_SSH_URL = Configurator.get(Configurator.Parameter.GITHUB_SSH_URL)
 			def GITHUB_HOST = Configurator.get(Configurator.Parameter.GITHUB_HOST)
 
@@ -31,18 +31,18 @@ class GitHub implements ISCM {
 						submoduleCfg: [], userRemoteConfigs: [[url: GIT_URL]]], \
 						changelog: false, poll: false
 			} else {
-				def token_name = 'token_' + "${userId}"
+				def token_name = 'token_' + "${USER_ID}"
 				//context.println("token_name: ${token_name}")
 				def token_value = Configurator.get(token_name)
 				//context.println("token_value: ${token_value}")
 				//if token_value contains ":" as delimiter then redefine build_user_id using the 1st part
 				if (token_value != null && token_value.contains(":")) {
 					def (tempUserId, tempToken) = token_value.tokenize( ':' )
-					userId = tempUserId
+					USER_ID = tempUserId
 					token_value =  tempToken
 				}
 				if (token_value != null) {
-					GIT_URL = "https://${token_value}@${GITHUB_HOST}/${userId}/${PROJECT}"
+					GIT_URL = "https://${token_value}@${GITHUB_HOST}/${USER_ID}/${PROJECT}"
 					context.println "fork repo url: ${GIT_URL}"
 					context.checkout scm: [$class: 'GitSCM', branches: [[name: '${branch}']], \
 							doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 15], [$class: 'CloneOption', noTags: true, reference: '', shallow: true, timeout: 15]], \
