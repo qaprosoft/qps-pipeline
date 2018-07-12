f6edc163d2f41eddcca0074ce6372cb585e0bb2c
vdelendik <vadim.delendik@gmail.com>
2018-07-13 01:04:34 +0300

    renamed default to sync with carina configuration class

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Configurator.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Configurator.groovy
@@ -6,6 +6,8 @@ import java.util.List
 public class Configurator {
 
     private def context
+	
+	privare final String mustOverride = "{must_override}"
 
     public Configurator(context) {
         this.context = context
@@ -29,33 +31,38 @@ public class Configurator {
     public enum Parameter {
 
         //vars
-        ADMIN_EMAILS("ADMIN_EMAILS", "qps-auto@qaprosoft.com"),
         CARINA_CORE_VERSION("CARINA_CORE_VERSION", "5.2.4.106"),
         CORE_LOG_LEVEL("CORE_LOG_LEVEL", "INFO"),
-        GITHUB_HOST("GITHUB_HOST", "REPLACE_ME"),
-        GITHUB_API_URL("GITHUB_API_URL", "REPLACE_ME"),
-        GITHUB_ORGANIZATION("GITHUB_ORGANIZATION", "REPLACE_ME"),
-        GITHUB_HTML_URL("GITHUB_HTML_URL", "REPLACE_ME"),
-        GITHUB_OAUTH_TOKEN("GITHUB_OAUTH_TOKEN", "REPLACE_ME"),
-        GITHUB_SSH_URL("GITHUB_SSH_URL", "REPLACE_ME"),
-        JACOCO_BUCKET("JACOCO_BUCKET", "jacoco.qaprosoft.com"),
-        JACOCO_ENABLE("JACOCO_ENABLE", "false"),
-        JOB_MAX_RUN_TIME("JOB_MAX_RUN_TIME", "60"),
-        QPS_PIPELINE_GIT_BRANCH("QPS_PIPELINE_GIT_BRANCH", "REPLACE_ME"),
-        QPS_PIPELINE_GIT_URL("QPS_PIPELINE_GIT_URL", "REPLACE_ME"),
-        SELENIUM_PROTOCOL("SELENIUM_PROTOCOL", "REPLACE_ME"),
-        SELENIUM_HOST("SELENIUM_HOST", "REPLACE_ME"),
-        SELENIUM_PORT("SELENIUM_PORT", "REPLACE_ME"),
-        SELENIUM_URL("SELENIUM_URL", "REPLACE_ME"),
-        ZAFIRA_ACCESS_TOKEN("ZAFIRA_ACCESS_TOKEN", "REPLACE_ME"),
+		JACOCO_BUCKET("JACOCO_BUCKET", "jacoco.qaprosoft.com"),
+		JACOCO_ENABLE("JACOCO_ENABLE", "false"),
+		JOB_MAX_RUN_TIME("JOB_MAX_RUN_TIME", "60"),
+	
+		QPS_PIPELINE_GIT_BRANCH("QPS_PIPELINE_GIT_BRANCH", mustOverride),
+		QPS_PIPELINE_GIT_URL("QPS_PIPELINE_GIT_URL", mustOverride),
+		ADMIN_EMAILS("ADMIN_EMAILS", mustOverride),
+		
+        GITHUB_HOST("GITHUB_HOST", mustOverride),
+        GITHUB_API_URL("GITHUB_API_URL", mustOverride),
+        GITHUB_ORGANIZATION("GITHUB_ORGANIZATION", mustOverride),
+        GITHUB_HTML_URL("GITHUB_HTML_URL", mustOverride),
+        GITHUB_OAUTH_TOKEN("GITHUB_OAUTH_TOKEN", mustOverride),
+        GITHUB_SSH_URL("GITHUB_SSH_URL", mustOverride),
+
+        SELENIUM_PROTOCOL("SELENIUM_PROTOCOL", mustOverride),
+        SELENIUM_HOST("SELENIUM_HOST", mustOverride),
+        SELENIUM_PORT("SELENIUM_PORT", mustOverride),
+        SELENIUM_URL("SELENIUM_URL", mustOverride),
+		
+        ZAFIRA_ACCESS_TOKEN("ZAFIRA_ACCESS_TOKEN", mustOverride),
         ZAFIRA_SERVICE_URL("ZAFIRA_SERVICE_URL", "http://zafira:8080/zafira-ws"),
-        JOB_URL("JOB_URL", ""),
-        JOB_NAME("JOB_NAME", ""),
-        JOB_BASE_NAME("JOB_BASE_NAME", ""),
-        BUILD_NUMBER("BUILD_NUMBER", ""),
-        NGINX_HOST("NGINX_HOST", "localhost"),
-        NGINX_PORT("NGINX_PORT", "80"),
-        NGINXT_PROTOCOL("NGINXT_PROTOCOL", "http"),
+		
+        JOB_URL("JOB_URL", mustOverride),
+        JOB_NAME("JOB_NAME", mustOverride),
+        JOB_BASE_NAME("JOB_BASE_NAME", mustOverride),
+        BUILD_NUMBER("BUILD_NUMBER", mustOverride),
+        NGINX_HOST("NGINX_HOST", mustOverride),
+        NGINX_PORT("NGINX_PORT", mustOverride),
+        NGINXT_PROTOCOL("NGINXT_PROTOCOL", mustOverride),
 
         private final String key;
         private final String value;
@@ -103,10 +110,12 @@ public class Configurator {
     public void loadContext() {
         //1. load all Parameter key/values to vars
         def enumValues  = Parameter.values()
-        for (enumValue in enumValues) {
-                vars.put(enumValue.getKey(), enumValue.getValue())
-        }
-        for (var in vars) {
+		for (enumValue in enumValues) {
+			if (!enumValue.getValue().equals(mustOverride)){
+				vars.put(enumValue.getKey(), enumValue.getValue())
+			}
+		}
+		for (var in vars) {
             context.println(var)
         }
         //2. load all string keys/values from env
