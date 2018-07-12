ac6d4c4ede9f292abcb7903185f1bd5af649f898
vdelendik <vadim.delendik@gmail.com>
2018-07-13 01:07:07 +0300

    more way to declare final String

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Configurator.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Configurator.groovy
@@ -7,7 +7,7 @@ public class Configurator {
 
     private def context
 	
-	private final String mustOverride = "{must_override}"
+	private final static def mustOverride = "{must_override}"
 
     public Configurator(context) {
         this.context = context
