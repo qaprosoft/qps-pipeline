2a5482336bf569b127e23a117b46f8c67e24ff8c
vdelendik <vadim.delendik@gmail.com>
2018-07-24 17:44:38 +0300

    merge several fixes into the common pipeline

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Scanner.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Scanner.groovy
@@ -188,7 +188,9 @@ class Scanner extends Executor {
 
 						}
 					} catch (FileNotFoundException e) {
-						context.println("Unable to find suite: " + suite.path)
+						context.println("ERROR! Unable to find suite: " + suite.path)
+					} catch (Exception e) {
+						context.println("ERROR! Unable to parse suite: " + filePath, e)
 					}
 				}
 			}
