25f9cafa538e76907d1c43f21b6536df1667a19c
tsvrko <tsvrko@gmail.com>
2018-07-25 18:07:35 +0300

    first factory creation attempt

--- src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateJob.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateJob.groovy
@@ -1,6 +1,10 @@
 package com.qaprosoft.jenkins.repository.jobdsl.v2
 
 import com.qaprosoft.jenkins.repository.jobdsl.v2.Creator
+import com.qaprosoft.jenkins.repository.jobdsl.factory.BuildFactory
 
 def creator = new Creator(this)
 creator.createJob()
+
+def buildJobFactory = new BuildFactory(this)
+buildJobFactory.emptyJob()
