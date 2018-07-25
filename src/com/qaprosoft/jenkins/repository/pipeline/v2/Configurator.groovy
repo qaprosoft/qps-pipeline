a29e48b046725c1268e2b6b063e3f08f622e0fc5
tsvrko <tsvrko@gmail.com>
2018-07-25 16:36:53 +0300

    overriden environment output

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Configurator.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Configurator.groovy
@@ -104,7 +104,7 @@ public class Configurator {
 		def enumValues  = Parameter.values()
 		def envVars = context.env.getEnvironment()
 
-        context.println("ENV DUMP" + context.env.getUrlName())
+        context.println("ENV DUMP" + context.env.getOverriddenEnvironment())
 		for (enumValue in enumValues) {
 			//a. set default values from enum
 			vars.put(enumValue.getKey(), enumValue.getValue())
