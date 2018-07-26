db01af94d09c2bdf13633a1536c2a7db3214ae7e
tsvrko <tsvrko@gmail.com>
2018-07-26 19:56:32 +0300

    put into view newly generated job

--- src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateJob.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateJob.groovy
@@ -9,7 +9,7 @@ def creator = new Creator(this)
 creator.createJob()
 
 def buildJobFactory = new BuildJobFactory(this)
-buildJobFactory.job("Free Style Job", "Factory job")
+buildJobFactory.job("Factory-Generated-Job", "Factory job")
 
 def buildListViewFactory = new BuildListViewFactory(this)
-buildListViewFactory.listView("Automation", "view", "", "API-Demo-Test")
+buildListViewFactory.listView("Automation", "view", "", "Factory-Generated-Job")
