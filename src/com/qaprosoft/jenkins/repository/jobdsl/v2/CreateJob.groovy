85d41fc23058463c8082645bb8f62be90b28184c
tsvrko <tsvrko@gmail.com>
2018-07-26 18:41:20 +0300

    lisrViewFactory call

--- src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateJob.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateJob.groovy
@@ -2,9 +2,14 @@ package com.qaprosoft.jenkins.repository.jobdsl.v2
 
 import com.qaprosoft.jenkins.repository.jobdsl.v2.Creator
 import com.qaprosoft.jenkins.repository.jobdsl.factory.BuildJobFactory
+import com.qaprosoft.jenkins.repository.jobdsl.factory.ListViewFactory
+
 
 def creator = new Creator(this)
 creator.createJob()
 
 def buildJobFactory = new BuildJobFactory(this)
 buildJobFactory.job("Free Style Job", "Factory job")
+
+def listViewFactory = new ListViewFactory(this)
+listViewFactory.listView("Automation", "", "")
