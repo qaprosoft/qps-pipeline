4e92a9c84fbf98a7549f3ffd66b1b9a6a8959a25
tsvrko <tsvrko@gmail.com>
2018-07-27 13:37:54 +0300

    cron view creating using factory

--- src/com/qaprosoft/jenkins/repository/jobdsl/factory/CreateViewFactory.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/factory/CreateViewFactory.groovy
@@ -3,7 +3,34 @@ package com.qaprosoft.jenkins.repository.jobdsl.factory
 import groovy.transform.InheritConstructors
 
 @InheritConstructors
-public class BuildViewFactory extends ViewFactory{
+public class CreateViewFactory extends ViewFactory{
+
+    def scannerListView(jobName, jobFolder, view, descFilter, propagate) {
+        def _view = factoryListView(jobFolder, view)
+        _view.with {
+            job {
+                name(jobName)
+            }
+
+            stringParameterValue {
+                name('folder')
+                value(jobFolder)
+            }
+
+            stringParameterValue {
+                name('view')
+                value(view)
+            }
+
+            stringParameterValue {
+                name('descFilter')
+                value(descFilter)
+            }
+
+            propagate(propagate)
+       }
+        return _view
+    }
 
     def listView(folder, viewName, descFilter, jobNames) {
         def view = factoryListView(folder, viewName)
