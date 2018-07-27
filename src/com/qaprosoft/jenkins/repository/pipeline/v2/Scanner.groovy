abb9d56a3df70a18f86ccb5520232d969a3dcf76
tsvrko <tsvrko@gmail.com>
2018-07-27 16:15:54 +0300

    binding variables dump

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Scanner.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Scanner.groovy
@@ -36,7 +36,7 @@ class Scanner extends Executor {
 
 	protected void scan() {
         context.println('DUMP')
-        context.println(context.binding.dump())
+        context.println(context.binding.variables.dump())
 
 		context.stage("Scan Repository") {
 			def BUILD_NUMBER = Configurator.get(Configurator.Parameter.BUILD_NUMBER)
