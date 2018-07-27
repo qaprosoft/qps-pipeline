b504b4ba497fc5ee910f95393b8c4f7bedbcf8a9
tsvrko <tsvrko@gmail.com>
2018-07-27 13:40:15 +0300

    createviewfactory imported

--- src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateJob.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateJob.groovy
@@ -1,16 +1,6 @@
 package com.qaprosoft.jenkins.repository.jobdsl.v2
 
 import com.qaprosoft.jenkins.repository.jobdsl.v2.Creator
-import com.qaprosoft.jenkins.repository.jobdsl.factory.BuildJobFactory
-import com.qaprosoft.jenkins.repository.jobdsl.factory.CreateViewFactory
-
 
 def creator = new Creator(this)
 creator.createJob()
-
-def buildJobFactory = new BuildJobFactory(this)
-buildJobFactory.job("Automation/Factory-Generated-Job", "Factory job")
-
-def createViewFactory = new CreateViewFactory(this)
-createViewFactory.listView("Automation", "listView", "", "Factory-Generated-Job")
-createViewFactory.categorizedView("Automation", "categorizedView", "", "Factory-Generated-Job")
