aec5932b0ffeeb77b0c3c48a94a14c5dbca92e6c
vdelendik <vadim.delendik@gmail.com>
2018-07-30 18:00:44 +0300

    improvements into the factory definitions

---
+++ src/com/qaprosoft/jenkins/repository/jobdsl/factory/view/ListViewFactory2.groovy
@@ -0,0 +1,43 @@
+package com.qaprosoft.jenkins.repository.jobdsl.factory.view
+
+import groovy.transform.InheritConstructors
+
+@InheritConstructors
+public class ListViewFactory2 extends ViewFactory {
+	def folder
+	def viewName
+	def descFilter
+	
+	void ListViewFactory2(folder, viewName, descFilter) {
+		this.folder = folder
+		this.viewName = viewName
+		this.descFilter = descFilter
+	}
+
+    def create() {
+        def view = factoryListView(folder, viewName)
+        view.with {
+            columns {
+                status()
+                weather()
+                name()
+                lastSuccess()
+                lastFailure()
+                lastDuration()
+                buildButton()
+            }
+
+            if (!"${descFilter}".isEmpty()) {
+                jobFilters {
+                    regex {
+                        matchType(MatchType.INCLUDE_MATCHED)
+                        matchValue(RegexMatchValue.DESCRIPTION)
+                        regex(".*${descFilter}.*")
+                    }
+                }
+            }
+        }
+        return view
+    }
+
+}
