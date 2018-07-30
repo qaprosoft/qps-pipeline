d395a16394fbde0bc74e8b4913cfe13c23c2db60
tsvrko <tsvrko@gmail.com>
2018-07-30 13:17:36 +0300

    ListViewFactory is created using reflection 4

--- src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateView.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateView.groovy
@@ -7,11 +7,8 @@ import com.qaprosoft.jenkins.repository.jobdsl.factory.view.CategorizedViewFacto
 //TODO: read parameters from pipeline and created file to determine valid factory class and params
 //def suite_name = context.readFileFromWorkspace("suite_name.txt")
 
-def reflectionListViewFactory = this.class.classLoader.loadClass( 'com.qaprosoft.jenkins.repository.jobdsl.factory.view.ListViewFactory', true, false )?.newInstance(this)
-reflectionListViewFactory.create('Automation', 'CRON', 'cron', '')
+def listViewFactory = this.class.classLoader.loadClass('ListViewFactory')?.newInstance(this)
+listViewFactory.create('Automation', 'CRON', 'cron', '')
 
-//def listViewFactory = new ListViewFactory(this)
-//listViewFactory.create('Automation', 'CRON', 'cron', '')
-//
-def categorizedViewFactory = new CategorizedViewFactory(this)
+def categorizedViewFactory = this.class.classLoader.loadClass('com.qaprosoft.jenkins.repository.jobdsl.factory.view.CategorizedViewFactory', true, false )?.newInstance(this)
 categorizedViewFactory.create('Automation', 'CRON2', '.*cron.*', '')
