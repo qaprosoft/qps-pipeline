e4c143cc3a4264ee5815820100ef589c79152e92
vdelendik <vadim.delendik@gmail.com>
2018-07-31 00:39:39 +0300

    added freeStyle job

--- src/com/qaprosoft/jenkins/repository/jobdsl/factory/job/JobFactory.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/factory/job/JobFactory.groovy
@@ -5,11 +5,32 @@ import groovy.transform.InheritConstructors
 
 @InheritConstructors
 public class JobFactory extends DslFactory {
+	
+	def name
+	def description
+	def logRotator
+	
+	public JobFactory(name, description, logRotator) {
+		this.name = name
+		this.description = description
+		this.logRotator = logRotator
+	}
 
-    def freeStyleJob(_name, _description) {
-        return dslFactory.freeStyleJob(_name){
-            description "DSL MANAGED: - $_description"
-            logRotator { numToKeep 100 }
-        }
-    }
+	public JobFactory(name, description) {
+		this(name, descritption, 100)
+	}
+	
+	public JobFactory(name) {
+		this(name, "descritption", 100)
+	}
+	
+	
+
+	def create() {
+		def job = _dslFactory.freeStyleJob("${name}"){
+			description "${description}"
+			logRotator { numToKeep "${logRotator}" }
+		}
+		return job
+	}
 }
