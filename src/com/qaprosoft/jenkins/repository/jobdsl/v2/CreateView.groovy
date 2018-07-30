00cf375c2a622a51a688d0d6bd9293649cd968aa
tsvrko <tsvrko@gmail.com>
2018-07-30 13:31:01 +0300

    ListViewFactory is created using Class.forName 2

--- src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateView.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateView.groovy
@@ -7,7 +7,7 @@ import com.qaprosoft.jenkins.repository.jobdsl.factory.view.CategorizedViewFacto
 //TODO: read parameters from pipeline and created file to determine valid factory class and params
 //def suite_name = context.readFileFromWorkspace("suite_name.txt")
 
-def listViewFactory = Class.forName("ListViewFactory")?.newInstance(this)
+def listViewFactory = Class.forName("com.qaprosoft.jenkins.repository.jobdsl.factory.view.ListViewFactory")?.newInstance(this)
 listViewFactory.create('Automation', 'CRON', 'cron', '')
 
 def categorizedViewFactory = this.class.classLoader.loadClass('com.qaprosoft.jenkins.repository.jobdsl.factory.view.CategorizedViewFactory')?.newInstance(this)
