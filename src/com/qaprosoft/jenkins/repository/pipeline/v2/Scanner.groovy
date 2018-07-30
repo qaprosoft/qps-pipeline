240aa7e64b3d03aed64b0cf6bb04ef9c19c2885e
vdelendik <vadim.delendik@gmail.com>
2018-07-30 23:01:37 +0300

    commeneted CRON3

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Scanner.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Scanner.groovy
@@ -148,7 +148,7 @@ class Scanner extends Executor {
 					suiteName = suiteName.substring(suiteName.lastIndexOf(testngFolder) + testngFolder.length(), suiteName.indexOf(".xml"))
 					context.writeFile file: "suite_name.txt", text: suiteName
 
-					viewFactories.put("cron3", new CategorizedViewFactory(jobFolder, 'CRON3', 'API|Web'))
+					//viewFactories.put("cron3", new CategorizedViewFactory(jobFolder, 'CRON3', 'API|Web'))
 					viewFactories.put("cron", new ListViewFactory(jobFolder, 'CRON', 'cron'))
 					
 					viewFactories.put(project, new ListViewFactory(jobFolder, project.toUpperCase(), project))
