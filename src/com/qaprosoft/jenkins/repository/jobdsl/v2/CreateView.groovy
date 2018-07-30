b0d3accede696716a8fe1abf8e66b827d75f8657
vdelendik <vadim.delendik@gmail.com>
2018-07-30 18:21:35 +0300

    experiment

--- src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateView.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateView.groovy
@@ -1,23 +1,11 @@
 package com.qaprosoft.jenkins.repository.jobdsl.v2
 
 import groovy.json.JsonSlurper
-import com.qaprosoft.jenkins.repository.jobdsl.factory.view.ViewType
 
-//TODO: read parameters from pipeline and created file to determine valid factory class and params
-String factoryDataMap = readFileFromWorkspace("factory_data.txt")
-def slurper = new JsonSlurper()
-Map<String, ViewType> factories = new HashMap(slurper.parseText(factoryDataMap))
-
-factories.each{
-    ViewType viewData = it.value
-    println(it.value)
-    def listViewFactory = Class.forName(viewData.factory)?.newInstance(this)
-    listViewFactory.create(viewData.folder, viewData.viewName, viewData.descFilter)
-}
-
-
+println(this.dump())
 factoryDataMap = readFileFromWorkspace("factories.txt")
 factories = new HashMap(slurper.parseText(factoryDataMap))
+
 
 factories.each{
 	println(it.value)
