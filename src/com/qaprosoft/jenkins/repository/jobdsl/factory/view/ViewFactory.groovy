1f7c8ad8e7e8c7ac360ba2d1ee5129600749915e
vdelendik <vadim.delendik@gmail.com>
2018-07-30 18:51:59 +0300

    declared empty constructor

--- src/com/qaprosoft/jenkins/repository/jobdsl/factory/view/ViewFactory.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/factory/view/ViewFactory.groovy
@@ -4,9 +4,13 @@ public class ViewFactory {
 
     def _dslFactory
 
-    ViewFactory(dslFactory){
+    ViewFactory(dslFactory) {
         _dslFactory = dslFactory
     }
+	
+	ViewFactory() {
+		_dslFactory = null
+	}
 
 	def setDsl(dsl) {
 		this._dslFactory = dsl
