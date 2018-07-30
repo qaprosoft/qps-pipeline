d13c3539daac1127f3e1e9ffb7bf435425038758
tsvrko <tsvrko@gmail.com>
2018-07-30 15:44:36 +0300

    ViewType import

--- src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateView.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateView.groovy
@@ -3,7 +3,7 @@ package com.qaprosoft.jenkins.repository.jobdsl.v2
 import com.qaprosoft.jenkins.repository.jobdsl.factory.view.ListViewFactory
 import com.qaprosoft.jenkins.repository.jobdsl.factory.view.CategorizedViewFactory
 import groovy.json.JsonSlurper
-
+import com.qaprosoft.jenkins.repository.jobdsl.factory.view.ViewType
 
 //TODO: read parameters from pipeline and created file to determine valid factory class and params
 String factoryDataMap = readFileFromWorkspace("factory_data.txt")
