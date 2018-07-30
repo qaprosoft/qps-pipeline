0be406a4476b06e0a3d3ff0feba406aec2218e97
vdelendik <vadim.delendik@gmail.com>
2018-07-30 19:38:24 +0300

    removed useless code

--- src/com/qaprosoft/jenkins/repository/jobdsl/factory/view/ListViewFactory.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/factory/view/ListViewFactory.groovy
@@ -4,31 +4,50 @@ import groovy.transform.InheritConstructors
 
 @InheritConstructors
 public class ListViewFactory extends ViewFactory {
+	def folder
+	def viewName
+	def descFilter
+	
+	public ListViewFactory(folder, viewName, descFilter) {
+		this.folder = folder
+		this.viewName = viewName
+		this.descFilter = descFilter
+	}
+	
+	public init(args) {
+		this.folder = args.get("folder")
+		this.viewName = args.get("viewName")
+		this.descFilter = args.get("descFilter")
+	}
+	
+	def get() {
+		return folder + "/" + viewName + "/" + descFilter
+	}
 
-    def create(folder, viewName, descFilter) {
-        def view = factoryListView(folder, viewName)
-        view.with {
-            columns {
-                status()
-                weather()
-                name()
-                lastSuccess()
-                lastFailure()
-                lastDuration()
-                buildButton()
-            }
+	def create() {
+		def view = factoryListView(folder, viewName)
+		view.with {
+			columns {
+				status()
+				weather()
+				name()
+				lastSuccess()
+				lastFailure()
+				lastDuration()
+				buildButton()
+			}
 
-            if (!"${descFilter}".isEmpty()) {
-                jobFilters {
-                    regex {
-                        matchType(MatchType.INCLUDE_MATCHED)
-                        matchValue(RegexMatchValue.DESCRIPTION)
-                        regex(".*${descFilter}.*")
-                    }
-                }
-            }
-        }
-        return view
-    }
+			if (!"${descFilter}".isEmpty()) {
+				jobFilters {
+					regex {
+						matchType(MatchType.INCLUDE_MATCHED)
+						matchValue(RegexMatchValue.DESCRIPTION)
+						regex(".*${descFilter}.*")
+					}
+				}
+			}
+		}
+		return view
+	}
 
 }
