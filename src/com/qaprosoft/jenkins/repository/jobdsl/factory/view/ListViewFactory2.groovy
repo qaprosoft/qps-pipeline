b8efd7d5e50de2a04a0603916303989cbebb5c3b
vdelendik <vadim.delendik@gmail.com>
2018-07-30 19:11:33 +0300

    removed non-default constructor

--- src/com/qaprosoft/jenkins/repository/jobdsl/factory/view/ListViewFactory2.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/factory/view/ListViewFactory2.groovy
@@ -8,7 +8,7 @@ public class ListViewFactory2 extends ViewFactory {
 	def viewName
 	def descFilter
 	
-	public ListViewFactory2(folder, viewName, descFilter) {
+	public set(folder, viewName, descFilter) {
 		this.folder = folder
 		this.viewName = viewName
 		this.descFilter = descFilter
