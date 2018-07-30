979be7044c20f0b78ab0e7855ace4cafa2493cf6
vdelendik <vadim.delendik@gmail.com>
2018-07-30 18:59:02 +0300

    return to working variant

--- src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateView.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateView.groovy
@@ -2,6 +2,23 @@ package com.qaprosoft.jenkins.repository.jobdsl.v2
 
 import groovy.json.JsonSlurper
 
+import com.qaprosoft.jenkins.repository.jobdsl.factory.view.ViewType
+
+//TODO: read parameters from pipeline and created file to determine valid factory class and params
+String factoryDataMap = readFileFromWorkspace("factory_data.txt")
+def slurper = new JsonSlurper()
+Map<String, ViewType> factories = new HashMap(slurper.parseText(factoryDataMap))
+
+factories.each{
+	ViewType viewData = it.value
+	println(it.value)
+	def listViewFactory = Class.forName(viewData.factory)?.newInstance(this)
+	println("listViewFactory")
+	println(listViewFactory.dump())
+	listViewFactory.create(viewData.folder, viewData.viewName, viewData.descFilter)
+}
+
+/*
 println("this")
 println(this.dump())
 
@@ -17,7 +34,6 @@ def listViewFactory2 = Class.forName("com.qaprosoft.jenkins.repository.jobdsl.fa
 println("listViewFactory2")
 println(listViewFactory2.dump())
 	
-def slurper = new JsonSlurper()
 String factoryDataMap = readFileFromWorkspace("factories.txt")
 def factories = new HashMap(slurper.parseText(factoryDataMap))
 
@@ -27,7 +43,7 @@ factories.each{
 	it.setDsl(listViewFactory.getDsl())
 	it.create()
 }
-
+*/
 
 //def categorizedViewFactory = this.class.classLoader.loadClass('com.qaprosoft.jenkins.repository.jobdsl.factory.view.CategorizedViewFactory')?.newInstance(this)
 //categorizedViewFactory.create('Automation', 'CRON2', 'API|Web')
