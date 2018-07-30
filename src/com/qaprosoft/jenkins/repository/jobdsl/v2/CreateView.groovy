85bef6e787594e24994abea0f2f3e96d93da6f2f
vdelendik <vadim.delendik@gmail.com>
2018-07-30 19:35:41 +0300

    removed useless linesкуьщмув гыудуыы дштуы

--- src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateView.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/v2/CreateView.groovy
@@ -1,41 +1,8 @@
 package com.qaprosoft.jenkins.repository.jobdsl.v2
 
 import groovy.json.JsonSlurper
-/*
-import com.qaprosoft.jenkins.repository.jobdsl.factory.view.ViewType
-
-//TODO: read parameters from pipeline and created file to determine valid factory class and params
-String factoryDataMap = readFileFromWorkspace("factory_data.txt")
-def slurper = new JsonSlurper()
-Map<String, ViewType> factories = new HashMap(slurper.parseText(factoryDataMap))
-
-factories.each{
-	ViewType viewData = it.value
-	println(it.value)
-	def listViewFactory = Class.forName(viewData.factory)?.newInstance(this)
-	println("listViewFactory")
-	println(listViewFactory.dump())
-	listViewFactory.create(viewData.folder, viewData.viewName, viewData.descFilter)
-}*/
-
-
 def slurper = new JsonSlurper()
 
-println("this")
-println(this.dump())
-
-println("this.binding")
-println(this.binding.dump())
-
-
-def listViewFactory = new com.qaprosoft.jenkins.repository.jobdsl.factory.view.ViewFactory(this)
-println("listViewFactory")
-println(listViewFactory.dump())
-
-def listViewFactory2 = Class.forName("com.qaprosoft.jenkins.repository.jobdsl.factory.view.ViewFactory")?.newInstance(this)
-println("listViewFactory2")
-println(listViewFactory2.dump())
-	
 String factoryDataMap = readFileFromWorkspace("factories.txt")
 println("factoryDataMap: " + factoryDataMap)
 def factories = new HashMap(slurper.parseText(factoryDataMap))
