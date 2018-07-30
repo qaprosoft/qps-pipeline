14406ca448865f64eb8df9cb66c108a07ca82725
vdelendik <vadim.delendik@gmail.com>
2018-07-31 02:34:06 +0300

    moved name, desc and folder to DslFactory

--- src/com/qaprosoft/jenkins/repository/jobdsl/factory/job/JobFactory.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/factory/job/JobFactory.groovy
@@ -5,21 +5,14 @@ import groovy.transform.InheritConstructors
 
 @InheritConstructors
 public class JobFactory extends DslFactory {
-	def folder
-	def name
-	def description
 	def logRotator = 100
 	
 	public JobFactory(folder, name, description) {
-		this.folder = folder
-		this.name = name
-		this.description = description
+		super(folder, name, description)
 	}
 	
 	public JobFactory(folder, name, description, logRotator) {
-		this.folder = folder
-		this.name = name
-		this.description = description
+		super(folder, name, description)
 		this.logRotator = logRotator
 	}
 	
