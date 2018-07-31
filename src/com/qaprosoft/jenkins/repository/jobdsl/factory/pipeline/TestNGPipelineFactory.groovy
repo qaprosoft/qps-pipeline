6023798a049ce6822040472caeb249009a951801
vdelendik <vadim.delendik@gmail.com>
2018-07-31 03:45:58 +0300

    another parser approach

--- src/com/qaprosoft/jenkins/repository/jobdsl/factory/pipeline/TestNGPipelineFactory.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/factory/pipeline/TestNGPipelineFactory.groovy
@@ -4,7 +4,6 @@ package com.qaprosoft.jenkins.repository.jobdsl.factory.pipeline
 
 import org.testng.xml.Parser;
 import org.testng.xml.XmlSuite;
-import com.cloudbees.groovy.cps.NonCPS
 import groovy.transform.InheritConstructors
 
 @InheritConstructors
@@ -21,8 +20,15 @@ public class TestNGPipelineFactory extends PipelineFactory {
 	}
 	
 	def create() {
-		currentSuite = parseSuite(suitePath)
+		
+		def xmlFile = new Parser(path)
+		xmlFile.setLoadClasses(false)
+		
+		List<XmlSuite> suiteXml = xmlFile.parseToList()
+		currentSuite = suiteXml.get(0)
+
 		this.name = currentSuite.getParameter("jenkinsJobName").toString()
+		println("name: " + name)
 		
 		def pipelineJob = super.create()
 		pipelineJob.with {
@@ -223,14 +229,4 @@ public class TestNGPipelineFactory extends PipelineFactory {
 		return pipelineJob
 	}
 	
-	
-	@NonCPS
-	private XmlSuite parseSuite(String path) {
-		def xmlFile = new Parser(path)
-		xmlFile.setLoadClasses(false)
-		
-		List<XmlSuite> suiteXml = xmlFile.parseToList()
-		XmlSuite currentSuite = suiteXml.get(0)
-		return currentSuite
-	}
 }
