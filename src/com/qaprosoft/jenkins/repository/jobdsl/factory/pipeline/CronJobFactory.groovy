1c8cdec44d1c9749327b32d87c600dda853ceb8f
tsvrko <tsvrko@gmail.com>
2018-07-31 14:42:43 +0300

    InheritConstructors import to createcronfactory

--- src/com/qaprosoft/jenkins/repository/jobdsl/factory/pipeline/CronJobFactory.groovy
+++ src/com/qaprosoft/jenkins/repository/jobdsl/factory/pipeline/CronJobFactory.groovy
@@ -1,8 +1,12 @@
 package com.qaprosoft.jenkins.repository.jobdsl.factory.pipeline
 
+@Grab('org.testng:testng:6.8.8')
+
 import org.testng.xml.Parser
 import org.testng.xml.XmlSuite
+import groovy.transform.InheritConstructors
 
+@InheritConstructors
 public class CronJobFactory extends PipelineFactory {
 
     def project
