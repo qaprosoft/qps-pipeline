93dc05bc6b73b0121f0f195003cdab073085c22c
tsvrko <tsvrko@gmail.com>
2018-08-02 23:06:09 +0300

    proxyInfo class is uncommented

--- src/com/qaprosoft/selenium/grid/ProxyInfo.groovy
+++ src/com/qaprosoft/selenium/grid/ProxyInfo.groovy
@@ -8,27 +8,27 @@ class ProxyInfo {
 		def baseDeviceList = ["DefaultPool", "ANY"]
 
 		return baseDeviceList
-		//println(baseDeviceList)
+		println(baseDeviceList)
 		
-//		def deviceList = []
-//		//TODO: reuse selenium host/port/protocol from env jobVars
-//		def proxyInfoUrl = selenium + "/grid/admin/ProxyInfo"
-//		//println("ProxyInfo url: ${proxyInfoUrl}")
-//
-//		try {
-//			def json = new JsonSlurper().parse(proxyInfoUrl.toURL())
-//			//println(json)
-//			json.each {
-//				if (platform.equalsIgnoreCase(it.configuration.capabilities.platform)) {
-//					println("platform: " + it.configuration.capabilities.platform[0] + "; device: " + it.configuration.capabilities.browserName[0])
-//					deviceList.add(it.configuration.capabilities.browserName[0]);
-//				}
-//			}
-//		} catch (Exception e) {
-//			//TODO: find a way to write message in static methods
-//			println(e.getMessage())
-//		}
-//
-//		return baseDeviceList + deviceList.sort()
+		def deviceList = []
+		//TODO: reuse selenium host/port/protocol from env jobVars
+		def proxyInfoUrl = selenium + "/grid/admin/ProxyInfo"
+		//println("ProxyInfo url: ${proxyInfoUrl}")
+
+		try {
+			def json = new JsonSlurper().parse(proxyInfoUrl.toURL())
+			//println(json)
+			json.each {
+				if (platform.equalsIgnoreCase(it.configuration.capabilities.platform)) {
+					println("platform: " + it.configuration.capabilities.platform[0] + "; device: " + it.configuration.capabilities.browserName[0])
+					deviceList.add(it.configuration.capabilities.browserName[0]);
+				}
+			}
+		} catch (Exception e) {
+			//TODO: find a way to write message in static methods
+			println(e.getMessage())
+		}
+
+		return baseDeviceList + deviceList.sort()
 	}
 }
