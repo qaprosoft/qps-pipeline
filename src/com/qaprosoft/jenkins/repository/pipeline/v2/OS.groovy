21dd37e22a2069d495f691e6e2227c7adfd10d88
tsvrko <tsvrko@gmail.com>
2018-08-10 11:58:29 +0300

    browserstack-like naming

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/OS.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/OS.groovy
@@ -2,19 +2,20 @@ package com.qaprosoft.jenkins.repository.pipeline.v2
 
 public enum OS {
 
-    WINDOWS_10("Windows 10", "windows", '10'),
-    WINDOWS_8_1("Windows 8.1", "windows", '8.1'),
-    WINDOWS_8("Windows 8", "windows", '8'),
-    WINDOWS_7("Windows 7", "windows", '7'),
-    WINDOWS_XP("Windows XP", "windows", 'XP'),
+    WINDOWS_10("Windows 10", "Windows", '10'),
+    WINDOWS_8_1("Windows 8.1", "Windows", '8.1'),
+    WINDOWS_8("Windows 8", "Windows", '8'),
+    WINDOWS_7("Windows 7", "Windows", '7'),
+    WINDOWS_XP("Windows XP", "Windows", 'XP'),
 
-    OS_X_HIGH_SIERRA("OS X High Sierra", "osx", "High Sierra"),
-    OS_X_SIERRA("OS X Sierra", "osx", "Sierra"),
-    OS_X_YOSEMITE("OS X Yosemite", "osx", "El Capitan"),
-    OS_X_MAVERICKS("OS X Mavericks", "osx", "High Mavericks"),
-    OS_X_MOUNTAIN_LION("OS X Mountain Lion", "osx", "Mountain Lion"),
-    OS_X_LION("OS X Lion", "osx", "Lion"),
-    OS_X_SNOW_LEOPARD("OS X Snow Leopard", "osx", "Snow Leopard")
+    OS_X_HIGH_SIERRA("OS X High Sierra", "OS X", "High Sierra"),
+    OS_X_SIERRA("OS X Sierra", "OS X", "Sierra"),
+    OS_X_EL_CAPITAN("OS X El Capitan", "OS X", "El Capitan"),
+    OS_X_YOSEMITE("OS X Yosemite", "OS X", "Yosemite"),
+    OS_X_MAVERICKS("OS X Mavericks", "OS X", "High Mavericks"),
+    OS_X_MOUNTAIN_LION("OS X Mountain Lion", "OS X", "Mountain Lion"),
+    OS_X_LION("OS X Lion", "OS X", "Lion"),
+    OS_X_SNOW_LEOPARD("OS X Snow Leopard", "OS X", "Snow Leopard")
 
     private final String os
     private final String name
