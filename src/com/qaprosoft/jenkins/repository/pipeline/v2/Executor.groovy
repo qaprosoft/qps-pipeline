957cc806d5047121af6c8a6a8054107f8c38ad2a
tsvrko <tsvrko@gmail.com>
2018-08-16 19:15:32 +0300

    @NonCPS getCangeLogSets

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Executor.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Executor.groovy
@@ -52,6 +52,25 @@ public abstract class Executor {
 		return content
 	}
 
+	@NonCPS
+	protected void getChangeLogSets() {
+		def changeLogSets = context.currentBuild.changeSets
+		context.println("CHANGESET: " + changeLogSets)
+		for (int i = 0; i < changeLogSets.size(); i++) {
+			def entries = changeLogSets[i].items
+			for (int j = 0; j < entries.length; j++) {
+				def entry = entries[j]
+				context.echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+				def files = new ArrayList(entry.affectedFiles)
+				for (int k = 0; k < files.size(); k++) {
+					def file = files[k]
+					context.echo "  ${file.editType.name} ${file.path}"
+				}
+			}
+		}
+	}
+
+
     /* Checks if current job started as rebuild */
     protected Boolean isRebuild(String jobName) {
         Boolean isRebuild = false
