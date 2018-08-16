6265b38b7ad7c9feabc941903a6d57b09d9ace6a
tsvrko <tsvrko@gmail.com>
2018-08-16 14:02:12 +0300

    changeSets output

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Scanner.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Scanner.groovy
@@ -43,6 +43,20 @@ class Scanner extends Executor {
 
 				scmClient.clone(QPS_PIPELINE_GIT_URL, QPS_PIPELINE_GIT_BRANCH, "qps-pipeline")
 
+				def changeLogSets = context.currentBuild.changeSets
+				for (int i = 0; i < changeLogSets.size(); i++) {
+					def entries = changeLogSets[i].items
+					for (int j = 0; j < entries.length; j++) {
+						def entry = entries[j]
+						echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+						def files = new ArrayList(entry.affectedFiles)
+						for (int k = 0; k < files.size(); k++) {
+							def file = files[k]
+							echo "  ${file.editType.name} ${file.path}"
+						}
+					}
+				}
+
 				this.scan()
 				this.clean()
 			}
