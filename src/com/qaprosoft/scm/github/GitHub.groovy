77b61c7f29e735125b9e3c6b3a96cfecf49ad483
tsvrko <tsvrko@gmail.com>
2018-08-16 16:30:30 +0300

    changelog: true

--- src/com/qaprosoft/scm/github/GitHub.groovy
+++ src/com/qaprosoft/scm/github/GitHub.groovy
@@ -30,7 +30,7 @@ class GitHub implements ISCM {
 				def checkoutResult = context.checkout scm: [$class: 'GitSCM', branches: [[name: '${branch}']], \
 						doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 15], [$class: 'CloneOption', noTags: true, reference: '', shallow: true, timeout: 15]], \
 						submoduleCfg: [], userRemoteConfigs: [[url: gitUrl]]], \
-						changelog: false, poll: false
+						changelog: true, poll: false
 				context.println("CHECKOUT RESULT: " + checkoutResult)
 			} else {
 				def token_name = 'token_' + "${userId}"
