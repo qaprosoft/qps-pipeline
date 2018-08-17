4a5d71edc73e8bc1c6b54eade74618aabebe2a7f
tsvrko <tsvrko@gmail.com>
2018-08-17 13:02:30 +0300

    entry.dump()

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Scanner.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Scanner.groovy
@@ -60,6 +60,7 @@ class Scanner extends Executor {
         def changeLogSets = context.currentBuild.rawBuild.changeSets
         for (changeLogSet in changeLogSets) {
             for (entry in changeLogSet.getItems()) {
+                context.println("ENTRY: " + entry.dump())
                 for (path in entry.getPaths()) {
                     context.println(path.dump())
                 }
