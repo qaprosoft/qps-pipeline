d931517ed41966d0720b2bd73c58d5a1f9db8e8d
tsvrko <tsvrko@gmail.com>
2018-08-21 17:52:02 +0300

    paths dump

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Executor.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Executor.groovy
@@ -69,7 +69,7 @@ public abstract class Executor {
             context.println("changeLogSet: " + changeLogSet.dump())
             for (entry in changeLogSet.getItems()) {
                 context.println("ENTRY: " + entry.dump())
-                context.println("AFFECTED FILES: " + entry.getAffectedFiles().dump())
+                context.println("AFFECTED FILES: " + entry.paths.dump())
                 entry.getAffectedFiles().each { path ->
                     context.println("PATH DUMP: " + path.dump())
                     context.println("PATH: " + path.getPath())
