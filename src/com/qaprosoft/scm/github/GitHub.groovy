b852e0a0f792b5c20cce1266d1c290c1d717f102
tsvrko <tsvrko@gmail.com>
2018-08-21 18:32:11 +0300

    depth property removed

--- src/com/qaprosoft/scm/github/GitHub.groovy
+++ src/com/qaprosoft/scm/github/GitHub.groovy
@@ -28,7 +28,7 @@ class GitHub implements ISCM {
 			context.println("forked_repo: " + fork)
 			if (!fork) {
 				context.checkout scm: [$class: 'GitSCM', branches: [[name: branch]], \
-						doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 15], [$class: 'CloneOption', depth: 2, noTags: true, reference: '', shallow: true, timeout: 15]], \
+						doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 15], [$class: 'CloneOption', noTags: true, reference: '', shallow: true, timeout: 15]], \
 						submoduleCfg: [], userRemoteConfigs: [[url: gitUrl]]], \
 						changelog: true, poll: false
 			} else {
