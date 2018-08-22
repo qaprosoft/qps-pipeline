679497cd3913b763bb6bb31381450a73abb6ac6a
tsvrko <tsvrko@gmail.com>
2018-08-22 16:10:28 +0300

    checkout parameters modification

--- src/com/qaprosoft/scm/github/GitHub.groovy
+++ src/com/qaprosoft/scm/github/GitHub.groovy
@@ -33,8 +33,10 @@ class GitHub implements ISCM {
 						doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 15], [$class: 'CloneOption', depth: 1, noTags: true, reference: '', shallow: false, timeout: 15]], \
 						submoduleCfg: [], userRemoteConfigs: [[url: gitUrl]]], \
 						changelog: true, poll: false]
+                def subfolderExtension = [[$class: 'RelativeTargetDirectory', relativeTargetDir: subFolder]]
+                checkoutParams.get("scm")["extensions"] = subfolderExtension
+                context.println("MODIFIED PARAMS: " + checkoutParams)
 
-                context.println("EXTENSIONS CLASS: " + checkoutParams.get("scm")["extensions"].getClass())
 				context.checkout checkoutParams
 			} else {
 				def token_name = 'token_' + "${userId}"
