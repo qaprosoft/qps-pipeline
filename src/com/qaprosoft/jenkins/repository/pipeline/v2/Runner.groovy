7e25962a7e17eb306151102a837c21aea3263689
tsvrko <tsvrko@gmail.com>
2018-08-23 15:59:20 +0300

    temporary local folderName

--- src/com/qaprosoft/jenkins/repository/pipeline/v2/Runner.groovy
+++ src/com/qaprosoft/jenkins/repository/pipeline/v2/Runner.groovy
@@ -40,7 +40,7 @@ class Runner extends Executor {
 		//TODO: remove master node assignment
 		context.node(nodeName) {
             context.println("FOLDER NAME: " + getFolderName())
-            this.folderName = getFolderName()
+            def folderName = getFolderName()
             scmClient.clone()
 
 			def WORKSPACE = this.getWorkspace()
