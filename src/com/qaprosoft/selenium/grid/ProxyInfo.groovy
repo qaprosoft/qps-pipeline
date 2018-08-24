a3ff6c530395a6d72ad029d2ee12682193ed09c4
tsvrko <tsvrko@gmail.com>
2018-08-24 14:11:20 +0300

    singleton deviceList initialization closes #92

--- src/com/qaprosoft/selenium/grid/ProxyInfo.groovy
+++ src/com/qaprosoft/selenium/grid/ProxyInfo.groovy
@@ -3,25 +3,28 @@ package com.qaprosoft.selenium.grid
 import groovy.json.JsonSlurper;
 
 class ProxyInfo {
+
+    def private static deviceList = []
+    def private static baseDeviceList = ["DefaultPool", "ANY"]
+
 	//TODO: reused grid/admin/ProxyInfo to get atual list of iOS/Android devices
 	public static List<String> getDevicesList(String selenium, String platform) {
-		println("SELENIUM" + selenium)
-		def baseDeviceList = ["DefaultPool", "ANY"]
 
-		def deviceList = []
-		//TODO: reuse selenium host/port/protocol from env jobVars
+		println("SELENIUM" + selenium)
+
+        //TODO: reuse selenium host/port/protocol from env jobVars
 		def proxyInfoUrl = selenium + "/grid/admin/ProxyInfo"
-		//println("ProxyInfo url: ${proxyInfoUrl}")
 
 		try {
-			def json = new JsonSlurper().parse(proxyInfoUrl.toURL())
-			//println(json)
-			json.each {
-				if (platform.equalsIgnoreCase(it.configuration.capabilities.platform)) {
-					println("platform: " + it.configuration.capabilities.platform[0] + "; device: " + it.configuration.capabilities.browserName[0])
-					deviceList.add(it.configuration.capabilities.browserName[0]);
-				}
-			}
+            if (deviceList.length != 0) {
+                def json = new JsonSlurper().parse(proxyInfoUrl.toURL())
+                json.each {
+                    if (platform.equalsIgnoreCase(it.configuration.capabilities.platform)) {
+                        println("platform: " + it.configuration.capabilities.platform[0] + "; device: " + it.configuration.capabilities.browserName[0])
+                        deviceList.add(it.configuration.capabilities.browserName[0]);
+                    }
+                }
+            }
 		} catch (Exception e) {
 			//TODO: find a way to write message in static methods
 			println(e.getMessage())
