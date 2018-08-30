97f56323c1baa2e1810731e201a385bd6447769d
tsvrko <tsvrko@gmail.com>
2018-08-30 15:41:45 +0300

    QUEUE BLOCK

--- src/com/qaprosoft/jenkins/pipeline/Runner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Runner.groovy
@@ -118,6 +118,7 @@ class Runner extends Executor {
 		context.node(nodeName) {
 			// init ZafiraClient to register queued run and abort it at the end of the run pipeline
 			try {
+                context.println "QUEUE BLOCK"
 				zc = new ZafiraClient(context)
                 zc.initZafiraClient()
                 zc.queueZafiraTestRun(uuid)
