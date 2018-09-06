226d299b5755d6e079c37a8e8b30893334736f27
tsvrko <tsvrko@gmail.com>
2018-09-06 21:32:07 +0300

    creator is reverted

--- src/com/qaprosoft/jenkins/jobdsl/Creator.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/Creator.groovy
@@ -13,11 +13,10 @@ def factories = new HashMap(slurper.parseText(factoryDataMap))
 
 factories.each{
 	try {
-		println "IT: " + it.value
 		def factory = Class.forName(it.value.clazz)?.newInstance(this)
-		println("before load: " + it.value.dump())
+		//println("before load: " + it.value.dump())
 		factory.load(it.value)
-		println("factory after load: " + factory.dump())
+		//println("factory after load: " + factory.dump())
 		factory.create()
 	} catch (Exception e) {
 		println(e.dump())
