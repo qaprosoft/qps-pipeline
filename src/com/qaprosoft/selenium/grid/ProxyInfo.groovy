14a0933281378707325c7a41ea12f1dd1c971146
tsvrko <tsvrko@gmail.com>
2018-09-07 15:24:16 +0300

    outputs are deleted from ProxyInfo class

--- src/com/qaprosoft/selenium/grid/ProxyInfo.groovy
+++ src/com/qaprosoft/selenium/grid/ProxyInfo.groovy
@@ -7,7 +7,7 @@ class ProxyInfo {
 
     private def dslFactory
     private String proxyInfoUrl
-    private static def platformDeviceListMap = ["android":[], "ios":[]]
+    private def platformDeviceListMap = ["android":[], "ios":[]]
     private def baseDeviceList = ["DefaultPool", "ANY"]
 
     ProxyInfo(dslFactory) {
@@ -17,23 +17,14 @@ class ProxyInfo {
 
     //TODO: reused grid/admin/ProxyInfo to get atual list of iOS/Android devices
 	public def getDevicesList(String platform) {
-
-        dslFactory.println "MY MAP: " + platformDeviceListMap
-        //TODO: reuse selenium host/port/protocol from env jobVars
         def deviceList = platformDeviceListMap.get(platform.toLowerCase())
-        dslFactory.println "SIZE: " + deviceList.size()
-
 		try {
             if (deviceList.size() == 0) {
                 def json = new JsonSlurper().parse(proxyInfoUrl.toURL())
-                if (json.size() == 0) {
-                    deviceList.add("No " + platform + " devices")
-                }
                 String prettyJson = new JsonBuilder(json).toPrettyString()
-                dslFactory.println "JSON: " + prettyJson
                 json.each {
                     if (platform.equalsIgnoreCase(it.configuration.capabilities.platform)) {
-//                        dslFactory.println "platform: " + it.configuration.capabilities.platform[0] + "; device: " + it.configuration.capabilities.browserName[0]
+                        dslFactory.println "platform: " + it.configuration.capabilities.platform[0] + "; device: " + it.configuration.capabilities.browserName[0]
                         deviceList.add(it.configuration.capabilities.browserName[0])
                     }
                 }
