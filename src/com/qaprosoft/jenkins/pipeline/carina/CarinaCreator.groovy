2b7e7bb11c7d0aeab5b3a2d5e8bc68cabbb89f2e
vdelendik <vadim.delendik@gmail.com>
2018-09-11 15:02:43 +0300

    added caribaCreator to support PR Checker validation

---
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaCreator.groovy
@@ -0,0 +1,44 @@
+package com.qaprosoft.jenkins.pipeline
+
+import com.qaprosoft.jenkins.pipeline.Creator
+import com.qaprosoft.jenkins.pipeline.Scanner
+import com.qaprosoft.scm.github.GitHub;
+
+class CarinaCreator extends Creator {
+
+	public CarinaCreator(context) {
+		super(context)
+		this.context = context
+
+		scmClient = new GitHub(context)
+		scanner = new Scanner(context)
+	}
+
+	//Events
+	@Override
+	protected void onUpdate() {
+		context.println("CarinaCreator->onUpdate")
+		// handle each push/merge operation
+		// execute logic inside this method only if $REPO_HOME/Jenkinsfile was updated
+		context.println("TODO: implement snapshot build generation and emailing build number...")
+	}
+
+	protected void onPullRequest() {
+		context.println("CSarinaCreator->onPullRequest")
+		context.node("master") {
+			scmClient.clonePR()
+			def goals = "-Dcobertura.report.format=xml cobertura:cobertura clean test javadoc:javadoc"
+
+			if (context.isUnix()) {
+				context.sh "'mvn' -B ${goals}"
+			} else {
+				context.bat "mvn -B ${goals}"
+			}
+		}
+		
+		//TODO: publish cobertura report
+		
+		//TODO: send email about unit testing results
+	}
+
+}
