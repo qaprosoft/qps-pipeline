f59442767bae28e4cf46848d36fd824bdc9da59c
vdelendik <vadim.delendik@gmail.com>
2018-09-11 12:46:16 +0300

    simplify structure for repository and scanner classes usage

--- src/com/qaprosoft/jenkins/pipeline/Scanner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Scanner.groovy
@@ -23,38 +23,15 @@ class Scanner extends Executor {
 	
 	protected def creatorTarget = "qps-pipeline/src/com/qaprosoft/jenkins/jobdsl/Creator.groovy"
 	protected def additionalClasspath = "qps-pipeline/src"
-    //TODO: investigate if we need to redefine this psrameter
-    protected boolean ignoreExisting = false
 	
-	protected boolean onlyUpdated = false
-	protected def removedConfigFilesAction = "IGNORE"
-	protected def removedJobAction = "IGNORE"
-	protected def removedViewAction = "IGNORE"
-
     public Scanner(context) {
         super(context)
         this.context = context
         scmClient = new GitHub(context)
-		
-		onlyUpdated = Configurator.get("onlyUpdated").toBoolean()
-		removedConfigFilesAction = Configurator.get("removedConfigFilesAction")
-		removedJobAction = Configurator.get("removedJobAction")
-		removedViewAction = Configurator.get("removedViewAction")
     }
 
-	public Scanner(context, onlyUpdated, removedConfigFilesAction, removedJobAction, removedViewAction) {
-		// do not use this(context) as it require extra job parameters
-        super(context)
-        this.context = context
-        scmClient = new GitHub(context)
-		
-		this.onlyUpdated = onlyUpdated
-		this.removedConfigFilesAction = removedConfigFilesAction
-		this.removedJobAction = removedJobAction
-		this.removedViewAction = removedViewAction
-	}
 	
-    public void scanRepository() {
+    public void updateRepository() {
 		context.node('master') {
 			context.timestamps {
                 this.prepare()
@@ -67,6 +44,16 @@ class Scanner extends Executor {
             }
         }
 	}
+	
+	public void createRepository() {
+		context.node('master') {
+			context.timestamps {
+				this.prepare()
+				this.create()
+				this.clean()
+			}
+		}
+	}
 
 	protected void prepare() {
         scmClient.clone(!onlyUpdated)
@@ -75,31 +62,74 @@ class Scanner extends Executor {
 		scmClient.clone(QPS_PIPELINE_GIT_URL, QPS_PIPELINE_GIT_BRANCH, "qps-pipeline")
 	}
 
-	protected void scan() {
+	protected void create() {
 
 		context.stage("Scan Repository") {
 			def BUILD_NUMBER = Configurator.get(Configurator.Parameter.BUILD_NUMBER)
 			def project = Configurator.get("project")
 			def jobFolder = Configurator.get("project")
-			
+
 			def branch = Configurator.get("branch")
 			context.currentBuild.displayName = "#${BUILD_NUMBER}|${project}|${branch}"
 
 			def workspace = getWorkspace()
 			context.println("WORKSPACE: ${workspace}")
 
-			
 
+
+			// TODO: move folder and main trigger job creation onto the createRepository method
 			registerObject(jobFolder, new FolderFactory(jobFolder, ""))
-			
+
 			// Support DEV related CI workflow
 			registerObject("trigger", new ListViewFactory(jobFolder, 'TRIGGER', '.*trigger.*'))
 			registerObject(project, new TriggerJobFactory(jobFolder, getTriggerScript(), "_trigger-" + project, "trigger project: ${project};", project))
+
+
+			// put into the factories.json all declared jobdsl factories to verify and create/recreate/remove etc
+			context.writeFile file: "factories.json", text: JsonOutput.toJson(dslObjects)
+
+			context.println("creatorTarget: " + creatorTarget)
+
+			context.jobDsl additionalClasspath: additionalClasspath,
+			removedConfigFilesAction: 'DELETE',
+			removedJobAction: 'DELETE',
+			removedViewAction: 'DELETE',
+			targets: creatorTarget,
+			ignoreExisting: false
+
+		}
+	}
 			
+	protected void scan() {
+
+		context.stage("Scan Repository") {
+			def BUILD_NUMBER = Configurator.get(Configurator.Parameter.BUILD_NUMBER)
+			def project = Configurator.get("project")
+			def jobFolder = Configurator.get("project")
+
+			def branch = Configurator.get("branch")
+			context.currentBuild.displayName = "#${BUILD_NUMBER}|${project}|${branch}"
+
+			def workspace = getWorkspace()
+			context.println("WORKSPACE: ${workspace}")
+
+			def onlyUpdated = Configurator.get("onlyUpdated").toBoolean()
+			def removedConfigFilesAction = Configurator.get("removedConfigFilesAction")
+			def removedJobAction = Configurator.get("removedJobAction")
+			def removedViewAction = Configurator.get("removedViewAction")
+
+
+			// TODO: move folder and main trigger job creation onto the createRepository method
+			registerObject(jobFolder, new FolderFactory(jobFolder, ""))
+
+			// Support DEV related CI workflow
+			registerObject("trigger", new ListViewFactory(jobFolder, 'TRIGGER', '.*trigger.*'))
+			registerObject(project, new TriggerJobFactory(jobFolder, getTriggerScript(), "_trigger-" + project, "trigger project: ${project};", project))
+
 			def jenkinsFileOrigin = "Jenkinsfile"
 			if (context.fileExists("${workspace}/${jenkinsFileOrigin}")) {
 				// this is the repo with already available pipeline script in Jenkinsfile
-				// just create a job 
+				// just create a job
 			}
 
 
@@ -111,7 +141,7 @@ class Scanner extends Executor {
 			}
 
 			Object subProjects = this.parseJSON("${workspace}/${jenkinsFile}").sub_projects
-		
+
 			subProjects.each {
 				context.println("sub_project: " + it)
 
@@ -121,9 +151,8 @@ class Scanner extends Executor {
 				if (sub_project.equals(".")) {
 					subProjectFilter = "**"
 				}
-				
+
 				def prChecker = it.pr_checker
-				def prMerger = it.deployable
 				def zafiraFilter = it.zafira_filter
 				def suiteFilter = it.suite_filter
 
@@ -137,17 +166,6 @@ class Scanner extends Executor {
 				}
 				context.println("zafira_project: ${zafira_project}")
 
-				//TODO: restore PR checker functionality after configuring by default sonarqube scanner in jenkins-master:
-				// https://github.com/qaprosoft/jenkins-master/issues/11
-				
-/*				if (prChecker) {
-					//TODO: implement PR_Checker creation
-				}
-*/				
-				if (prMerger) {
-					//TODO: implement auto-deploy artifact job
-				}
-
 				if (suiteFilter.endsWith("/")) {
 					//remove last character if it is slash
 					suiteFilter = suiteFilter[0..-2]
@@ -158,7 +176,7 @@ class Scanner extends Executor {
 				// VIEWS
 				registerObject("cron", new ListViewFactory(jobFolder, 'CRON', '.*cron.*'))
 				//registerObject(project, new ListViewFactory(jobFolder, project.toUpperCase(), ".*${project}.*"))
-				
+
 				//TODO: create default personalized view here
 
 				// find all tetsng suite xml files and launch dsl creator scripts (views, folders, jobs etc)
@@ -185,20 +203,20 @@ class Scanner extends Executor {
 							if (currentSuite.toXml().contains("zafira_project")) {
 								zafira_project = currentSuite.getParameter("zafira_project")
 							}
-							
+
 							// put standard views factory into the map
 							registerObject(zafira_project, new ListViewFactory(jobFolder, zafira_project.toUpperCase(), ".*${zafira_project}.*"))
 							registerObject(suiteOwner, new ListViewFactory(jobFolder, suiteOwner, ".*${suiteOwner}"))
-		
+
 							//pipeline job
 							//TODO: review each argument to TestJobFactory and think about removal
 							//TODO: verify suiteName duplication here and generate email failure to the owner and admin_emails
-                            def jobDesc = "project: ${project}; zafira_project: ${zafira_project}; owner: ${suiteOwner}"
+							def jobDesc = "project: ${project}; zafira_project: ${zafira_project}; owner: ${suiteOwner}"
 							registerObject(suiteName, new TestJobFactory(jobFolder, getPipelineScript(), project, sub_project, zafira_project, getWorkspace() + "/" + suite.path, suiteName, jobDesc))
 
 							//cron job
-							if (!currentSuite.getParameter("jenkinsRegressionPipeline").toString().contains("null") 
-								&& !currentSuite.getParameter("jenkinsRegressionPipeline").toString().isEmpty()) {
+							if (!currentSuite.getParameter("jenkinsRegressionPipeline").toString().contains("null")
+							&& !currentSuite.getParameter("jenkinsRegressionPipeline").toString().isEmpty()) {
 								def cronJobNames = currentSuite.getParameter("jenkinsRegressionPipeline").toString()
 								for (def cronJobName : cronJobNames.split(",")) {
 									cronJobName = cronJobName.trim()
@@ -207,27 +225,27 @@ class Scanner extends Executor {
 								}
 							}
 						}
-						
+
 					} catch (FileNotFoundException e) {
 						context.println("ERROR! Unable to find suite: " + suite.path)
 					} catch (Exception e) {
 						context.println("ERROR! Unable to parse suite: " + suite.path)
 						printStackTrace(e)
 					}
-					
+
 				}
-				
+
 				// put into the factories.json all declared jobdsl factories to verify and create/recreate/remove etc
 				context.writeFile file: "factories.json", text: JsonOutput.toJson(dslObjects)
 
 				context.println("creatorTarget: " + creatorTarget)
 				//TODO: test carefully auto-removal for jobs/views and configs
 				context.jobDsl additionalClasspath: additionalClasspath,
-                        removedConfigFilesAction: removedConfigFilesAction,
-                        removedJobAction: removedJobAction,
-                        removedViewAction: removedViewAction,
-                        targets: creatorTarget,
-                        ignoreExisting: ignoreExisting
+				removedConfigFilesAction: removedConfigFilesAction,
+				removedJobAction: removedJobAction,
+				removedViewAction: removedViewAction,
+				targets: creatorTarget,
+				ignoreExisting: false
 			}
 		}
 	}
