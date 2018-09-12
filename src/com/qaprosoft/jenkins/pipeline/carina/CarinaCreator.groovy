341ac82022be5d2f187fc4244a0959830437a056
tsvrko <tsvrko@gmail.com>
2018-09-12 14:22:01 +0300

    -Dgpg.passphrase= brought back

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaCreator.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaCreator.groovy
@@ -54,7 +54,7 @@ class CarinaCreator extends Creator {
                 context.withCredentials([context.usernamePassword(credentialsId:'gpg_token', usernameVariable:'USERNAME', passwordVariable:'PASSWORD')]) {
                     context.echo "USERNAME: ${context.env.USERNAME}"
                     context.echo "PASSWORD: ${context.env.PASSWORD}"
-                    executeMavenGoals("-Dcobertura.report.format=xml cobertura:cobertura clean deploy javadoc:javadoc")
+                    executeMavenGoals("-Dgpg.passphrase=Qaprosoft2015 -Dcobertura.report.format=xml cobertura:cobertura clean deploy javadoc:javadoc")
                 }
             }
             //email notification
