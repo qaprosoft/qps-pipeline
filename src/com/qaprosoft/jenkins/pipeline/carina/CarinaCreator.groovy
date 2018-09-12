6bc576e6168a6382cdc309d8aea5011e5571b960
tsvrko <tsvrko@gmail.com>
2018-09-12 14:10:57 +0300

    reports publishing temporary commented

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaCreator.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaCreator.groovy
@@ -26,42 +26,38 @@ class CarinaCreator extends Creator {
     @Override
     protected void onPullRequest() {
         context.println("CarinaCreator->onPullRequest")
-        context.withCredentials([context.usernamePassword(credentialsId:'gpg_token', usernameVariable:'USERNAME', passwordVariable:'PASSWORD')]) {
-            context.echo "USERNAME: ${context.env.USERNAME}"
-            context.echo "PASSWORD: ${context.env.PASSWORD}"
-        }
+
         context.node("master") {
             scmClient.clonePR()
-            def goals = "-Dcobertura.report.format=xml cobertura:cobertura clean test javadoc:javadoc"
-            executeMavenGoals(goals)
-            context.junit '**/target/surefire-reports/junitreports/*.xml'
-            context.step([$class: 'CoberturaPublisher',
-                  autoUpdateHealth: false,
-                  autoUpdateStability: false,
-                  coberturaReportFile: '**/target/site/cobertura/coverage.xml',
-                  failUnhealthy: false,
-                  failUnstable: false,
-                  maxNumberOfBuilds: 0,
-                  onlyStable: false,
-                  sourceEncoding: 'ASCII',
-                  zoomCoverageChart: false])
+
+
+//            context.junit '**/target/surefire-reports/junitreports/*.xml'
+//            context.step([$class: 'CoberturaPublisher',
+//                  autoUpdateHealth: false,
+//                  autoUpdateStability: false,
+//                  coberturaReportFile: '**/target/site/cobertura/coverage.xml',
+//                  failUnhealthy: false,
+//                  failUnstable: false,
+//                  maxNumberOfBuilds: 0,
+//                  onlyStable: false,
+//                  sourceEncoding: 'ASCII',
+//                  zoomCoverageChart: false])
 			//TODO: investigate howto use creds functionality in jenkins
 
 
             //TODO: implement below code
 			// produce snapshot build if ghprbPullTitle contains 'build-snapshot'
 			
-
-            //email notification
-        }
-        if (Configuration.get("ghprbPullTitle").contains("build-snapshot")) {
-            executeMavenGoals("versions:set -DnewVersion=${context.env.getEnvironment().get("CARINA_RELEASE")}.${context.env.getEnvironment().get("BUILD_NUMBER")}-SNAPSHOT")
+            if (Configuration.get("ghprbPullTitle").contains("build-snapshot")) {
+				executeMavenGoals("versions:set -DnewVersion=${context.env.getEnvironment().get("CARINA_RELEASE")}.${context.env.getEnvironment().get("BUILD_NUMBER")}-SNAPSHOT")
 //				executeMavenGoals("-Dcobertura.report.format=xml cobertura:cobertura clean deploy javadoc:javadoc")
-            context.withCredentials([context.usernamePassword(credentialsId:'gpg_token', usernameVariable:'USERNAME', passwordVariable:'PASSWORD')]) {
-                context.echo "USERNAME: ${context.env.USERNAME}"
-                context.echo "PASSWORD: ${context.env.PASSWORD}"
-                executeMavenGoals("-Dgpg.passphrase=${context.env.PASSWORD} -Dcobertura.report.format=xml cobertura:cobertura clean deploy javadoc:javadoc")
+                context.withCredentials([context.usernamePassword(credentialsId:'gpg_token', usernameVariable:'USERNAME', passwordVariable:'PASSWORD')]) {
+                    context.echo "USERNAME: ${context.env.USERNAME}"
+                    context.echo "PASSWORD: ${context.env.PASSWORD}"
+                    executeMavenGoals("-Dgpg.passphrase=${context.env.PASSWORD} -Dcobertura.report.format=xml cobertura:cobertura clean deploy javadoc:javadoc")
+                }
             }
+            //email notification
         }
         //TODO: publish cobertura report
         //TODO: send email about unit testing results
