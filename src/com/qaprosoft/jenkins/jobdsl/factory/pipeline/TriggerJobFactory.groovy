b4e8c488b838154da37a5cf9ae0dc421c8c1ca6f
vdelendik <vadim.delendik@gmail.com>
2018-09-12 23:07:50 +0300

    revert trigger job factory to yesterday state

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TriggerJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TriggerJobFactory.groovy
@@ -6,84 +6,73 @@ import groovy.transform.InheritConstructors
 public class TriggerJobFactory extends PipelineFactory {
 	def project
 
-    public TriggerJobFactory(folder, pipelineScript, jobName, jobDesc, project) {
-        this.folder = folder
+	public TriggerJobFactory(folder, pipelineScript, jobName, jobDesc, project) {
+		this.folder = folder
 		this.pipelineScript = pipelineScript
 		this.name = jobName
-        this.description = jobDesc
+		this.description = jobDesc
 		this.project = project
-    }
+	}
 
-    def create() {
+	def create() {
 
-        def pipelineJob = super.create()
+		def pipelineJob = super.create()
 
-        pipelineJob.with {
-			scm {
-                git {
-                    remote {
-                        url("https://github.com/ModiusOpenData/modius-src/")
-                        refspec("+refs/pull/*:refs/remotes/origin/pr/*")
-                    }
-                }
+		pipelineJob.with {
+			properties {
+				pipelineTriggers {
+					triggers {
+						githubPush()
+						ghprbTrigger {
+							gitHubAuthId(getGitHubAuthId(folder))
+							adminlist('')
+							useGitHubHooks(true)
+							triggerPhrase('')
+							autoCloseFailedPullRequests(false)
+							skipBuildPhrase('.*\\[skip\\W+ci\\].*')
+							displayBuildErrorsOnDownstreamBuilds(false)
+							cron('H/5 * * * *')
+							whitelist('')
+							orgslist(getOrganization())
+							blackListLabels('')
+							whiteListLabels('')
+							allowMembersOfWhitelistedOrgsAsAdmin(false)
+							permitAll(true)
+							buildDescTemplate('')
+							blackListCommitAuthor('')
+							includedRegions('')
+							excludedRegions('')
+							onlyTriggerPhrase(false)
+							commentFilePath('')
+							msgSuccess('')
+							msgFailure('')
+							commitStatusContext('')
+						}
+					}
+				}
 			}
 
-            properties {
-                githubProjectUrl('https://github.com/ModiusOpenData/modius-src/')
-                pipelineTriggers {
-                    triggers {
-                        githubPush()
-                        ghprbTrigger {
-                            gitHubAuthId(getGitHubAuthId(folder))
-                            adminlist('')
-                            useGitHubHooks(true)
-                            triggerPhrase('')
-                            autoCloseFailedPullRequests(false)
-                            skipBuildPhrase('.*\\[skip\\W+ci\\].*')
-                            displayBuildErrorsOnDownstreamBuilds(false)
-                            cron('H/5 * * * *')
-                            whitelist('')
-                            orgslist(getOrganization())
-                            blackListLabels('')
-                            whiteListLabels('')
-                            allowMembersOfWhitelistedOrgsAsAdmin(false)
-                            permitAll(true)
-                            buildDescTemplate('')
-                            blackListCommitAuthor('')
-                            includedRegions('')
-                            excludedRegions('')
-                            onlyTriggerPhrase(false)
-                            commentFilePath('')
-                            msgSuccess('')
-                            msgFailure('')
-                            commitStatusContext('')
-                        }
-                    }
-                }
-
-            }
-
 			//TODO: think about other parameters to support DevOps CI operations
-            parameters {
+			parameters {
 				stringParam('project', project, 'Your GitHub repository for scanning')
 				//TODO: analyze howto support several gc_GIT_BRACH basing on project
 				configure addExtensibleChoice('branch', "gc_GIT_BRANCH", "Select a GitHub Testing Repository Branch to run against", "master")
 				booleanParam('onlyUpdated', true, '	If chosen, scan will be performed only in case of any change in *.xml suites.')
-				
+
 				choiceParam('removedConfigFilesAction', ['IGNORE', 'DELETE'], '')
 				choiceParam('removedJobAction', ['IGNORE', 'DELETE'], '')
 				choiceParam('removedViewAction', ['IGNORE', 'DELETE'], '')
-            }
+			}
 
-        }
-        return pipelineJob
-    }
+		}
+		return pipelineJob
+	}
 
-    protected def getOrganization() {
-        return 'ModiusOpenData'
-    }
+	protected def getOrganization() {
+		return 'ModiusOpenData'
+	}
 
-    protected def getGitHubAuthId(project) {
-        return "https://api.github.com : ${project}-token"
-    }
+	protected def getGitHubAuthId(project) {
+		return "https://api.github.com : ${project}-token"
+	}
 }
