aaa96d38e8286aa082dd76a02b5d1d52d4a66bb9
tsvrko <tsvrko@gmail.com>
2018-09-13 18:05:10 +0300

    detailed pushJobDescription added

--- src/com/qaprosoft/jenkins/pipeline/Scanner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Scanner.groovy
@@ -87,12 +87,20 @@ class Scanner extends Executor {
 
 			// Support DEV related CI workflow
             def gitUrl = Configuration.resolveVars("${Configuration.get(Configuration.Parameter.GITHUB_HTML_URL)}/${Configuration.get("project")}")
+
 			registerObject("hooks_view", new ListViewFactory(jobFolder, 'SYSTEM', '.*system.*'))
-            def pullRequestJobDescription = "To finish GitHub WebHook setup please follow the steps below:\nGo to your GitHub repository.\nClick \"Settings\" tab.\nClick \"Webhooks\" menu option.\n" +
-                    "Click \"Add webhook\" button.\nType http://your-jenkins-domain.com/ghprbhook/ into \"Payload URL\" field. Pay attention to the difference with the original web hook URL we obtained above!!!\n" +
-                    "Select x-www-form-urlencoded in \"Content Type\" field.\nTick \"Let me select individual events.\" option and tick Pull request option.\nClick \"Add webhook\" button."
+
+            def pullRequestJobDescription = "To finish GitHub WebHook setup, please, follow the steps below:\n- Go to your GitHub repository\n- Click \"Settings\" tab\n- Click \"Webhooks\" menu option\n" +
+                    "- Click \"Add webhook\" button\n- Type http://your-jenkins-domain.com/ghprbhook/ into \"Payload URL\" field\n" +
+                    "- Select x-www-form-urlencoded in \"Content Type\" field\n- Tick \"Let me select individual events\" option and tick \"Pull request\" option\n- Click \"Add webhook\" button"
+
 			registerObject("pull_request_job", new PullRequestJobFactory(jobFolder, getOnPullRequestScript(), "onPullRequest-" + project, pullRequestJobDescription, project, gitUrl))
-			registerObject("push_job", new PushJobFactory(jobFolder, getOnPushScript(), "onPush-" + project, "system: onPush", project))
+
+            def pushJobDescription = "To finish GitHub WebHook setup, please, follow the steps below:\n- Go to your GitHub repository\n- Click \"Settings\" tab\n- Click \"Webhooks\" menu option\n" +
+                    "- Click \"Add webhook\" button\n- Type http://your-jenkins-domain.com/github-webhook/ into \"Payload URL\" field\n" +
+                    "- Select application/json in \"Content Type\" field\n- Tick \"Just the push event.\" option\n- Click \"Add webhook\" button"
+
+            registerObject("push_job", new PushJobFactory(jobFolder, getOnPushScript(), "onPush-" + project, pushJobDescription, project))
 
 			// put into the factories.json all declared jobdsl factories to verify and create/recreate/remove etc
 			context.writeFile file: "factories.json", text: JsonOutput.toJson(dslObjects)
