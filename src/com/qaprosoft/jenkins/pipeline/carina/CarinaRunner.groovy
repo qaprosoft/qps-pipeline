a05973468f78208294ae74ca10ec2d06c7ab6cd5
vdelendik <vadim.delendik@gmail.com>
2018-09-19 16:21:02 +0300

    removed context usage

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
@@ -9,10 +9,6 @@ class CarinaRunner extends Runner {
     }
 	
 	public void onPush() {
-		context.println("CarinaRunner->onPush")
-		// handle each push/merge operation
-		// execute logic inside this method only if $REPO_HOME/Jenkinsfile was updated
-		context.println("TODO: implement snapshot build generation and emailing build number...")
 	}
 	
 
