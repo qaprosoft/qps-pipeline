94b0a0d32154e81798f7488e11b859c345de95be
vdelendik <vadim.delendik@gmail.com>
2018-09-20 01:23:21 +0300

    transfer context

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
@@ -11,14 +11,12 @@ class CarinaRunner extends Runner {
 
     public CarinaRunner(context) {
         super(context)
-		scanner = new CarinaScanner()
-		//scanner = new CarinaScanner(context)
+		scanner = new CarinaScanner(context)
     }
 
 	@Override
 	public void onPush() {
 		context.println("CarinaRunner->onPush")
-		context.println(scanner.dump())
 		scanner.scanRepository()
 		// handle each push/merge operation
 		// execute logic inside this method only if $REPO_HOME/Jenkinsfile was updated
