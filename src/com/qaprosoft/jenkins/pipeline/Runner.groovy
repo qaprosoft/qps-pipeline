e8a513a03c1e2a2d5ddf266c8a9f2c151938f9f2
vdelendik <vadim.delendik@gmail.com>
2018-09-20 02:33:22 +0300

    removed constructor

--- src/com/qaprosoft/jenkins/pipeline/Runner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Runner.groovy
@@ -1,27 +1,10 @@
 package com.qaprosoft.jenkins.pipeline
 
-import groovy.json.JsonSlurperClassic
-@Grab('org.testng:testng:6.8.8')
-import org.testng.xml.Parser;
-import org.testng.xml.XmlSuite;
-import com.cloudbees.groovy.cps.NonCPS
 
-import com.qaprosoft.scm.ISCM
-
-import java.nio.file.FileSystems
-import java.nio.file.Path
-import java.nio.file.PathMatcher
-import java.nio.file.Paths
-
-import groovy.transform.InheritConstructors
 
 @InheritConstructors
 public class Runner {
-//	protected def context
-	
-	public Runner(context) {
-//		this.context = context
-	}
+	protected def context
 	
 	//Events
 	public void onPush() {
