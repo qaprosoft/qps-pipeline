9e69d29ad9e07a057bf33889f0b48208f517f0f4
vdelendik <vadim.delendik@gmail.com>
2018-09-19 16:46:23 +0300

    added onPush

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
@@ -6,5 +6,11 @@ class CarinaRunner {
     public CarinaRunner(context) {
 		this.context = context
     }
-	
+
+	public void onPush() {
+		context.println("CarinaRunner->onPush")
+		// handle each push/merge operation
+		// execute logic inside this method only if $REPO_HOME/Jenkinsfile was updated
+		context.println("TODO: implement snapshot build generation and emailing build number...")
+	}
 }
