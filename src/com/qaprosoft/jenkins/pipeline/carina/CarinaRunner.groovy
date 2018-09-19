0dcfd0238979417236907fce6c88950b3d09417f
vdelendik <vadim.delendik@gmail.com>
2018-09-19 16:07:31 +0300

    fixed packaging

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
@@ -1,10 +1,7 @@
-package com.qaprosoft.jenkins.pipeline
+package com.qaprosoft.jenkins.pipeline.carina
 
 import com.qaprosoft.jenkins.pipeline.Runner
 
-import groovy.transform.InheritConstructors
-
-@InheritConstructors
 class CarinaRunner extends Runner {
 
     public CarinaRunner(context) {
