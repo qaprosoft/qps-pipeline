caf0be62f6c9e7ac6b01b448c6bf1622d39ac1c7
vdelendik <vadim.delendik@gmail.com>
2018-09-20 00:43:16 +0300

    added scanner declaration

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
@@ -15,52 +15,12 @@ class CarinaRunner extends Runner {
         scanner = new CarinaScanner(context)
     }
 
-    @Override
+	@Override
 	public void onPush() {
 		context.println("CarinaRunner->onPush")
-        scanner.scanRepository()
+		scanner.scanRepository()
 		// handle each push/merge operation
 		// execute logic inside this method only if $REPO_HOME/Jenkinsfile was updated
 		context.println("TODO: implement snapshot build generation and emailing build number...")
 	}
-
-    @Override
-    public void onPullRequest() {
-        context.println("CarinaRunner->onPullRequest")
-
-        context.node("master") {
-            scmClient.clonePR()
-
-            executeMavenGoals("-U clean process-resources process-test-resources -Dcobertura.report.format=xml cobertura:cobertura clean deploy javadoc:javadoc")
-
-            context.junit '**/target/surefire-reports/junitreports/*.xml'
-
-            //TODO: test&fix cobertura report publishing
-            context.step([$class: 'CoberturaPublisher',
-                          autoUpdateHealth: false,
-                          autoUpdateStability: false,
-                          coberturaReportFile: '**/target/site/cobertura/coverage.xml',
-                          failUnhealthy: false,
-                          failUnstable: false,
-                          maxNumberOfBuilds: 0,
-                          onlyStable: false,
-                          sourceEncoding: 'ASCII',
-                          zoomCoverageChart: false])
-
-            //TODO: implement below code
-            // produce snapshot build if ghprbPullTitle contains 'build-snapshot'
-
-            if (Configuration.get("ghprbPullTitle").contains("build-snapshot")) {
-                executeMavenGoals("versions:set -DnewVersion=${context.env.getEnvironment().get("CARINA_RELEASE")}.${context.env.getEnvironment().get("BUILD_NUMBER")}-SNAPSHOT")
-                executeMavenGoals("-Dcobertura.report.format=xml cobertura:cobertura clean deploy javadoc:javadoc")
-/*                context.withCredentials([context.usernamePassword(credentialsId:'gpg_token', usernameVariable:'USERNAME', passwordVariable:'PASSWORD')]) {
-                    context.echo "USERNAME: ${context.env.USERNAME}"
-                    context.echo "PASSWORD: ${context.env.PASSWORD}"
-                    executeMavenGoals("-Dgpg.passphrase=${context.env.PASSWORD} -Dcobertura.report.format=xml cobertura:cobertura clean deploy javadoc:javadoc")
-                }
-*/
-            }
-            //TODO: email notification
-        }
-    }
 }
