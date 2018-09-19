36c1a043b570d5b795eccfd34ad2307321adbbc0
vdelendik <vadim.delendik@gmail.com>
2018-09-19 16:24:42 +0300

    added inheritance

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
@@ -1,7 +1,9 @@
 package com.qaprosoft.jenkins.pipeline.carina
 
 import com.qaprosoft.jenkins.pipeline.Runner
+import groovy.transform.InheritConstructors
 
+@InheritConstructors
 class CarinaRunner extends Runner {
 
     public CarinaRunner(context) {
