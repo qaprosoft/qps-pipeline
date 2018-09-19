6ea3021a53b5c7ebae8bc7963d93a6bd64a5b0f5
tsvrko <tsvrko@gmail.com>
2018-09-19 17:06:17 +0300

    update repository in carina runner

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
@@ -1,17 +1,21 @@
 package com.qaprosoft.jenkins.pipeline.carina
 
+import com.qaprosoft.jenkins.pipeline.Runner
 import groovy.transform.InheritConstructors
 
 @InheritConstructors
-class CarinaRunner {
+class CarinaRunner extends Runner{
 	protected def context
 
     public CarinaRunner(context) {
-		this.context = context
+        super(context)
+        this.context = context
     }
 
+    @Override
 	public void onPush() {
 		context.println("CarinaRunner->onPush")
+        scanner.updateRepository()
 		// handle each push/merge operation
 		// execute logic inside this method only if $REPO_HOME/Jenkinsfile was updated
 		context.println("TODO: implement snapshot build generation and emailing build number...")
