338f05acc4e53f0cfb46e1ba2a85ec2de92d7e2c
vdelendik <vadim.delendik@gmail.com>
2018-09-20 02:05:11 +0300

    empty runner

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
@@ -1,21 +1,21 @@
 package com.qaprosoft.jenkins.pipeline.carina
 
 class CarinaRunner {
-	protected def context
-
-    public CarinaRunner(context) {
-        this.context = context
-    }
+//	protected def context
+//
+//    public CarinaRunner(context) {
+//        this.context = context
+//    }
 
 	public void onPush() {
-		context.println("CarinaRunner->onPush")
+//		context.println("CarinaRunner->onPush")
 		//scanner.scanRepository()
 		// handle each push/merge operation
 		// execute logic inside this method only if $REPO_HOME/Jenkinsfile was updated
-		context.println("TODO: implement snapshot build generation and emailing build number...")
+//		context.println("TODO: implement snapshot build generation and emailing build number...")
 	}
 	
 	public void onPullRequest() {
-		context.println("CarinaRunner->onPullRequest")
+//		context.println("CarinaRunner->onPullRequest")
 	}
 }
