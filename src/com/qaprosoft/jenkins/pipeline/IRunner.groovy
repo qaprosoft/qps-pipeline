30589841190af4e5cc2712350a9d314ddcab70c6
vdelendik <vadim.delendik@gmail.com>
2018-09-20 04:21:38 +0300

    f1

--- src/com/qaprosoft/jenkins/pipeline/IRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/IRunner.groovy
@@ -6,7 +6,7 @@ import com.qaprosoft.scm.ISCM
 trait IRunner {
 	def context
 	ISCM scmClient
-	Configuration configuration
+	Configuration configuration = new Configuration(context)
 
 	abstract void onPush()
 	abstract void onPullRequest()
--- src/com/qaprosoft/jenkins/pipeline/Runner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Runner.groovy
@@ -1,30 +1,28 @@
 package com.qaprosoft.jenkins.pipeline
 
-import com.qaprosoft.jenkins.pipeline.Configuration
-import com.qaprosoft.scm.ISCM
-import com.qaprosoft.scm.github.GitHub;
-
-import com.cloudbees.groovy.cps.NonCPS
+//import com.qaprosoft.jenkins.pipeline.Configuration
+//import com.qaprosoft.scm.ISCM
+//import com.qaprosoft.scm.github.GitHub;
 
 class Runner implements IRunner {
 	public Runner(context) {
 		this.context = context
-		configuration = new Configuration(context)
-		scmClient = new GitHub(context)
+//		configuration = new Configuration(context)
+//		scmClient = new GitHub(context)
 	}
 
 	//Events
 	public void onPush() {
 		context.node("master") {
 			//TODO: incorporate onlyUpdated
-			scmClient.clone()
+//			scmClient.clone()
 			context.println("Runner->onPush")
 		}
 	}
 
 	public void onPullRequest() {
 		context.node("master") {
-			scmClient.clonePR()
+//			scmClient.clonePR()
 			context.println("Runner->onPullRequest")
 		}
 	}
