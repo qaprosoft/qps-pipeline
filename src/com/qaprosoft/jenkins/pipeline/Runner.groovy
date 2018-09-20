31cd6e538cae66baf1faa834e513da307af058c3
vdelendik <vadim.delendik@gmail.com>
2018-09-20 03:57:57 +0300

    remove executor as well

--- src/com/qaprosoft/jenkins/pipeline/Runner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Runner.groovy
@@ -2,9 +2,12 @@ package com.qaprosoft.jenkins.pipeline
 
 public class Runner {
 	protected def context
+	protected ISCM scmClient
+	protected Configuration configuration = new Configuration(context)
 
 	public Runner(context) {
 		this.context = context
+		scmClient = new GitHub(context)
 	}
 
 	//Events
