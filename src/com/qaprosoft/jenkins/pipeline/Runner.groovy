c7573ddca719ea755a3c86497f8541eadb4be9f3
vdelendik <vadim.delendik@gmail.com>
2018-09-20 03:52:20 +0300

    removed all useless content so far

--- src/com/qaprosoft/jenkins/pipeline/Runner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Runner.groovy
@@ -2,25 +2,21 @@ package com.qaprosoft.jenkins.pipeline
 
 public class Runner {
 	protected def context
-	
+
 	public Runner(context) {
 		this.context = context
 	}
-	
+
 	//Events
 	public void onPush() {
 		context.node("master") {
-			context.stage("Runner->onPush") {
-				context.println("Runner->onPush")
-			}
+			context.stage("Runner->onPush") { context.println("Runner->onPush") }
 		}
 	}
 
 	public void onPullRequest() {
 		context.node("master") {
-			context.stage("Runner->onPullRequest") {
-				context.println("Runner->onPullRequest")
-			}
+			context.stage("Runner->onPullRequest") { context.println("Runner->onPullRequest") }
 		}
-    }
+	}
 }
