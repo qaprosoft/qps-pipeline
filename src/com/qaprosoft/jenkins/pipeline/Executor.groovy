090f2c0b59c59701a2bccab930b6653a9e1ea890
tsvrko <tsvrko@gmail.com>
2018-09-20 19:32:21 +0300

    run job moved to QARunner

---
+++ src/com/qaprosoft/jenkins/pipeline/Executor.groovy
@@ -0,0 +1,45 @@
+package com.qaprosoft.jenkins.pipeline
+
+import static java.util.UUID.randomUUID
+
+public class Executor {
+
+    static String getUUID() {
+		def ci_run_id = Configuration.get("ci_run_id")
+		if (ci_run_id == null || ci_run_id.isEmpty()) {
+			ci_run_id = randomUUID() as String
+		}
+		return ci_run_id
+	}
+
+    static boolean isParamEmpty(String value) {
+        return value == null || value.isEmpty() || value.equals("NULL")
+    }
+
+    static def getEmailParams(body, subject, to) {
+        def params = [attachLog: true,
+                      body: body,
+                      recipientProviders: [[$class: 'DevelopersRecipientProvider'],
+                                           [$class: 'RequesterRecipientProvider']],
+                      subject: subject,
+                      to: to]
+        return params
+    }
+
+    static void printStackTrace(context, Exception ex) {
+        context.println "exception: " + ex.getMessage()
+        context.println "exception class: " + ex.getClass().getName()
+        context.println "stacktrace: " + Arrays.toString(ex.getStackTrace())
+    }
+
+    static def getReportParameters(reportDir, reportFiles, reportName) {
+        def reportParameters = [allowMissing: false,
+                                alwaysLinkToLastBuild: false,
+                                keepAll: true,
+                                reportDir: reportDir,
+                                reportFiles: reportFiles,
+                                reportName: reportName]
+        return reportParameters
+    }
+
+}
