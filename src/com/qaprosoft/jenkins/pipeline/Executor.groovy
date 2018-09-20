4533fdbf9557a78de779475ce0dd10c0308253b8
vdelendik <vadim.delendik@gmail.com>
2018-09-20 03:52:20 +0300

    removed all useless content so far

--- src/com/qaprosoft/jenkins/pipeline/Executor.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Executor.groovy
@@ -29,9 +29,7 @@ public abstract class Executor {
 	}
 
 	protected clean() {
-		context.stage('Wipe out Workspace') {
-			context.deleteDir()
-		}
+		context.stage('Wipe out Workspace') { context.deleteDir() }
 	}
 
 	protected void printStackTrace(Exception ex) {
@@ -58,120 +56,118 @@ public abstract class Executor {
 		return content
 	}
 
-    public void publishUnitTestResults() {
-        //publish junit/cobertura reports
-        context.junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
-        context.step([$class: 'CoberturaPublisher',
-                      autoUpdateHealth: false,
-                      autoUpdateStability: false,
-                      coberturaReportFile: '**/target/site/cobertura/coverage.xml',
-                      failUnhealthy: false,
-                      failUnstable: false,
-                      maxNumberOfBuilds: 0,
-                      onlyStable: false,
-                      sourceEncoding: 'ASCII',
-                      zoomCoverageChart: false])
-    }
+	public void publishUnitTestResults() {
+		//publish junit/cobertura reports
+		context.junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
+		context.step([$class: 'CoberturaPublisher',
+			autoUpdateHealth: false,
+			autoUpdateStability: false,
+			coberturaReportFile: '**/target/site/cobertura/coverage.xml',
+			failUnhealthy: false,
+			failUnstable: false,
+			maxNumberOfBuilds: 0,
+			onlyStable: false,
+			sourceEncoding: 'ASCII',
+			zoomCoverageChart: false])
+	}
 
-    protected void publishReports(String pattern, String reportName) {
-        def reports = context.findFiles(glob: pattern)
-        for (int i = 0; i < reports.length; i++) {
-            def parentFile = new File(reports[i].path).getParentFile()
-            if (parentFile == null) {
-                context.println "ERROR! Parent report is null! for " + reports[i].path
-                continue
-            }
-            def reportDir = parentFile.getPath()
-            context.println "Report File Found, Publishing " + reports[i].path
-            if (i > 0){
-                def reportIndex = "_" + i
-                reportName = reportName + reportIndex
-            }
-            context.publishHTML getReportParameters(reportDir, reports[i].name, reportName )
-        }
-    }
+	protected void publishReports(String pattern, String reportName) {
+		def reports = context.findFiles(glob: pattern)
+		for (int i = 0; i < reports.length; i++) {
+			def parentFile = new File(reports[i].path).getParentFile()
+			if (parentFile == null) {
+				context.println "ERROR! Parent report is null! for " + reports[i].path
+				continue
+			}
+			def reportDir = parentFile.getPath()
+			context.println "Report File Found, Publishing " + reports[i].path
+			if (i > 0){
+				def reportIndex = "_" + i
+				reportName = reportName + reportIndex
+			}
+			context.publishHTML getReportParameters(reportDir, reports[i].name, reportName )
+		}
+	}
 
-    protected def getReportParameters(reportDir, reportFiles, reportName) {
-        def reportParameters = [allowMissing: false,
-                                alwaysLinkToLastBuild: false,
-                                keepAll: true,
-                                reportDir: reportDir,
-                                reportFiles: reportFiles,
-                                reportName: reportName]
-        return reportParameters
-    }
+	protected def getReportParameters(reportDir, reportFiles, reportName) {
+		def reportParameters = [allowMissing: false,
+			alwaysLinkToLastBuild: false,
+			keepAll: true,
+			reportDir: reportDir,
+			reportFiles: reportFiles,
+			reportName: reportName]
+		return reportParameters
+	}
 
-    /** Detects if any changes are present in files matching patterns  */
-    @NonCPS
-    protected boolean isUpdated(String patterns) {
-        def isUpdated = false
-        def changeLogSets = context.currentBuild.rawBuild.changeSets
-        changeLogSets.each { changeLogSet ->
-            /* Extracts GitChangeLogs from changeLogSet */
-            for (entry in changeLogSet.getItems()) {
-                /* Extracts paths to changed files */
-                for (path in entry.getPaths()) {
-                    context.println("UPDATED: " + path.getPath())
-                    Path pathObject = Paths.get(path.getPath())
-                    /* Checks whether any changed file matches one of patterns */
-                    for (pattern in patterns.split(",")){
-                        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern)
-                        /* As only match is found stop search*/
-                        if (matcher.matches(pathObject)){
-                            isUpdated = true
-                            return
-                        }
-                    }
-                }
-            }
-        }
-        return isUpdated
-    }
+	/** Detects if any changes are present in files matching patterns  */
+	@NonCPS
+	protected boolean isUpdated(String patterns) {
+		def isUpdated = false
+		def changeLogSets = context.currentBuild.rawBuild.changeSets
+		changeLogSets.each { changeLogSet ->
+			/* Extracts GitChangeLogs from changeLogSet */
+			for (entry in changeLogSet.getItems()) {
+				/* Extracts paths to changed files */
+				for (path in entry.getPaths()) {
+					context.println("UPDATED: " + path.getPath())
+					Path pathObject = Paths.get(path.getPath())
+					/* Checks whether any changed file matches one of patterns */
+					for (pattern in patterns.split(",")){
+						PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern)
+						/* As only match is found stop search*/
+						if (matcher.matches(pathObject)){
+							isUpdated = true
+							return
+						}
+					}
+				}
+			}
+		}
+		return isUpdated
+	}
 
-    /** Checks if current job started as rebuild */
-    protected Boolean isRebuild(String jobName) {
-        Boolean isRebuild = false
-        /* Gets CauseActions of the job */
-        context.currentBuild.rawBuild.getActions(hudson.model.CauseAction.class).each {
-            action ->
-                /* Search UpstreamCause among CauseActions */
-                if (action.findCause(hudson.model.Cause.UpstreamCause.class) != null)
-                /* If UpstreamCause exists and has the same name as current job, rebuild was called */
-                    isRebuild = (jobName == action.findCause(hudson.model.Cause.UpstreamCause.class).getUpstreamProject())
-        }
-        return isRebuild
-    }
+	/** Checks if current job started as rebuild */
+	protected Boolean isRebuild(String jobName) {
+		Boolean isRebuild = false
+		/* Gets CauseActions of the job */
+		context.currentBuild.rawBuild.getActions(hudson.model.CauseAction.class).each { action ->
+			/* Search UpstreamCause among CauseActions */
+			if (action.findCause(hudson.model.Cause.UpstreamCause.class) != null)
+			/* If UpstreamCause exists and has the same name as current job, rebuild was called */
+			isRebuild = (jobName == action.findCause(hudson.model.Cause.UpstreamCause.class).getUpstreamProject())
+		}
+		return isRebuild
+	}
 
-    /** Determines BuildCause */
-    protected String getBuildCause(String jobName) {
-        String buildCause = null
-        /* Gets CauseActions of the job */
-        context.currentBuild.rawBuild.getActions(hudson.model.CauseAction.class).each {
-            action ->
-//                context.println "DUMP" + action.dump()
-                /* Searches UpstreamCause among CauseActions and checks if it is not the same job as current(the other way it was rebuild) */
-                if (action.findCause(hudson.model.Cause.UpstreamCause.class)
-                        && (jobName != action.findCause(hudson.model.Cause.UpstreamCause.class).getUpstreamProject())) {
-                    buildCause = "UPSTREAMTRIGGER"
-                }
-                /* Searches TimerTriggerCause among CauseActions */
-                else if (action.findCause(hudson.triggers.TimerTrigger$TimerTriggerCause.class)) {
-                    buildCause = "TIMERTRIGGER"
-                }
-                /* Searches GitHubPushCause among CauseActions */
-                else if (action.findCause(com.cloudbees.jenkins.GitHubPushCause.class)) {
-                    buildCause = "SCMPUSHTRIGGER"
-                }
-                else if (action.findCause(org.jenkinsci.plugins.ghprb.GhprbCause.class)) {
-                    buildCause = "SCMGHPRBTRIGGER"
-                }
-                else {
-                    buildCause = "MANUALTRIGGER"
-                }
+	/** Determines BuildCause */
+	protected String getBuildCause(String jobName) {
+		String buildCause = null
+		/* Gets CauseActions of the job */
+		context.currentBuild.rawBuild.getActions(hudson.model.CauseAction.class).each { action ->
+			//                context.println "DUMP" + action.dump()
+			/* Searches UpstreamCause among CauseActions and checks if it is not the same job as current(the other way it was rebuild) */
+			if (action.findCause(hudson.model.Cause.UpstreamCause.class)
+			&& (jobName != action.findCause(hudson.model.Cause.UpstreamCause.class).getUpstreamProject())) {
+				buildCause = "UPSTREAMTRIGGER"
+			}
+			/* Searches TimerTriggerCause among CauseActions */
+			else if (action.findCause(hudson.triggers.TimerTrigger$TimerTriggerCause.class)) {
+				buildCause = "TIMERTRIGGER"
+			}
+			/* Searches GitHubPushCause among CauseActions */
+			else if (action.findCause(com.cloudbees.jenkins.GitHubPushCause.class)) {
+				buildCause = "SCMPUSHTRIGGER"
+			}
+			else if (action.findCause(org.jenkinsci.plugins.ghprb.GhprbCause.class)) {
+				buildCause = "SCMGHPRBTRIGGER"
+			}
+			else {
+				buildCause = "MANUALTRIGGER"
+			}
 
-        }
-        return buildCause
-    }
+		}
+		return buildCause
+	}
 
 	XmlSuite parseSuite(String path) {
 		def xmlFile = new Parser(path)
@@ -189,7 +185,7 @@ public abstract class Executor {
 			context.bat "mvn -B ${goals}"
 		}
 	}
-	
+
 	protected void performSonarQubeScan(){
 		def sonarQubeEnv = ''
 		Jenkins.getInstance().getDescriptorByType(SonarGlobalConfiguration.class).getInstallations().each { installation ->
@@ -216,7 +212,7 @@ public abstract class Executor {
 				 -Dsonar.inclusions=**/src/main/java/** \
 				 -Dsonar.test.inclusions=**/src/test/java/** \
 				 -Dsonar.java.source=1.8"
-			}
 		}
 	}
+}
 }
