a7bdde40e9874e99725978007e4a1d69aabf83c7
vdelendik <vadim.delendik@gmail.com>
2018-09-20 05:08:10 +0300

    added default QARunner implementation with scanner

--- src/com/qaprosoft/jenkins/pipeline/AbstractRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/AbstractRunner.groovy
@@ -2,11 +2,14 @@ package com.qaprosoft.jenkins.pipeline
 
 import com.qaprosoft.jenkins.pipeline.Configuration
 import com.qaprosoft.scm.ISCM
-import com.qaprosoft.scm.github.GitHub;
 
 public abstract class AbstractRunner {
 	protected def context
 	protected ISCM scmClient
+
+	protected final def FACTORY_TARGET = "qps-pipeline/src/com/qaprosoft/jenkins/jobdsl/Factory.groovy"
+	protected final def EXTRA_CLASSPATH = "qps-pipeline/src"
+
 	
 	//this is very important line which should be declared only as a class member!
 	protected Configuration configuration = new Configuration(context)
