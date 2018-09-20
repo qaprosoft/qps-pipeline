9e383780f0a58db48309c2fed941e0becae8b3c3
vdelendik <vadim.delendik@gmail.com>
2018-09-20 03:06:27 +0300

    protected Configuration configuration = new Configuration(context)

--- src/com/qaprosoft/jenkins/pipeline/Runner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Runner.groovy
@@ -2,6 +2,7 @@ package com.qaprosoft.jenkins.pipeline
 
 public class Runner {
 	protected def context
+	protected Configuration configuration = new Configuration(context)
 	
 	public Runner(context) {
 		this.context = context
