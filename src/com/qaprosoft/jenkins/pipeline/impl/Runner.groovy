49eac41d8fe55e27d665da819bcdf9d25f3e46aa
vdelendik <vadim.delendik@gmail.com>
2018-09-20 04:39:20 +0300

    declared AbstractRunner class

--- src/com/qaprosoft/jenkins/pipeline/impl/Runner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/impl/Runner.groovy
@@ -3,14 +3,10 @@ package com.qaprosoft.jenkins.pipeline
 import com.qaprosoft.jenkins.pipeline.Configuration
 import com.qaprosoft.scm.ISCM
 import com.qaprosoft.scm.github.GitHub;
+import com.qaprosoft.jenkins.pipeline.AbstractRunner
 
-public class Runner {
-	protected def context
-	protected ISCM scmClient
+public class Runner extends AbstractRunner {
 	
-	//this is very important line which should be declared only as a class member!
-	protected Configuration configuration = new Configuration(context)
-
 	public Runner(context) {
 		this.context = context
 		scmClient = new GitHub(context)
