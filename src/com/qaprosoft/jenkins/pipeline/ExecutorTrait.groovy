193c3c42db200bc06f5adb55daa8129ac7e357ea
tsvrko <tsvrko@gmail.com>
2018-09-21 15:26:27 +0300

    empty trait usage

--- src/com/qaprosoft/jenkins/pipeline/ExecutorTrait.groovy
+++ src/com/qaprosoft/jenkins/pipeline/ExecutorTrait.groovy
@@ -2,9 +2,10 @@ package com.qaprosoft.jenkins.pipeline
 
 trait ExecutorTrait {
 
-    public void printStackTrace(Exception e) {
-        println "exception: " + e.getMessage()
-        println "exception class: " + e.getClass().getName()
-        println "stacktrace: " + Arrays.toString(e.getStackTrace())
-    }
+    def context
+//    public void printStackTrace(Exception e) {
+//        println "exception: " + e.getMessage()
+//        println "exception class: " + e.getClass().getName()
+//        println "stacktrace: " + Arrays.toString(e.getStackTrace())
+//    }
 }
