d471e4a700f3472076a42f8e27001ad1be97ae20
vdelendik <vadim.delendik@gmail.com>
2018-09-23 16:56:15 +0300

    support custom pom.xml file location

--- src/com/qaprosoft/jenkins/pipeline/impl/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/impl/QARunner.groovy
@@ -100,16 +100,8 @@ public class QARunner extends AbstractRunner {
             context.println("QARunner->onPullRequest")
             scmClient.clonePR()
 
-            context.stage('Maven Compile') {
-                def goals = "clean compile test-compile \
-						 -f pom.xml -Dmaven.test.failure.ignore=true \
-						-Dcom.qaprosoft.carina-core.version=${Configuration.get(Configuration.Parameter.CARINA_CORE_VERSION)}"
-
-                executeMavenGoals(goals)
-            }
-            context.stage('Sonar Scanner') {
-                performSonarQubeScan()
-            }
+			compile()
+            performSonarQubeScan()
 
             //TODO: investigate whether we need this piece of code
             //            if (Configuration.get("ghprbPullTitle").contains("automerge")) {
@@ -117,6 +109,21 @@ public class QARunner extends AbstractRunner {
             //            }
         }
     }
+	
+	protected void compile() {
+		compile("pom.xml")
+	}
+	
+	protected void compile(pomFile) {
+		context.stage('Maven Compile') {
+			def goals = "clean compile test-compile \
+					-f ${pomFile} \
+					-Dmaven.test.failure.ignore=true \
+					-Dcom.qaprosoft.carina-core.version=${Configuration.get(Configuration.Parameter.CARINA_CORE_VERSION)}"
+
+			executeMavenGoals(goals)
+		}
+	}
 
     protected void prepare() {
         scmClient.clone(!onlyUpdated)
@@ -134,19 +141,24 @@ public class QARunner extends AbstractRunner {
         }
     }
 
-    protected void performSonarQubeScan(){
-        def sonarQubeEnv = ''
-        Jenkins.getInstance().getDescriptorByType(SonarGlobalConfiguration.class).getInstallations().each { installation ->
-            sonarQubeEnv = installation.getName()
-        }
-        if(sonarQubeEnv.isEmpty()){
-            context.println "There is no SonarQube server configured. Please, configure Jenkins for performing SonarQube scan."
-            return
-        }
-        //TODO: find a way to get somehow 2 below hardcoded string values
-        context.stage('SonarQube analysis') {
+	protected void performSonarQubeScan(){
+		performSonarQubeScan("pom.xml")
+	}
+	
+    protected void performSonarQubeScan(pomFile){
+		context.stage('Sonar Scanner') {
+	        def sonarQubeEnv = ''
+	        Jenkins.getInstance().getDescriptorByType(SonarGlobalConfiguration.class).getInstallations().each { installation ->
+	            sonarQubeEnv = installation.getName()
+	        }
+	        if(sonarQubeEnv.isEmpty()){
+	            context.println "There is no SonarQube server configured. Please, configure Jenkins for performing SonarQube scan."
+	            return
+	        }
             context.withSonarQubeEnv(sonarQubeEnv) {
-                context.sh "mvn clean package sonar:sonar -DskipTests \
+                context.sh "mvn \
+				 -f ${pomFile} \
+				 clean package sonar:sonar -DskipTests \
 				 -Dsonar.github.endpoint=${Configuration.resolveVars("${Configuration.get(Configuration.Parameter.GITHUB_API_URL)}")} \
 				 -Dsonar.analysis.mode=preview  \
 				 -Dsonar.github.pullRequest=${Configuration.get("ghprbPullId")} \
@@ -161,7 +173,7 @@ public class QARunner extends AbstractRunner {
 				 -Dsonar.test.inclusions=**/src/test/java/** \
 				 -Dsonar.java.source=1.8"
             }
-        }
+		}
     }
     /** **/
 
