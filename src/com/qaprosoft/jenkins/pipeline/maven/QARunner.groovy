6b994ba09e91bb25f6cae6d979bfad104b041f57
tsvrko <tsvrko@gmail.com>
2018-10-02 16:41:47 +0300

    var to uncommented

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -753,7 +753,7 @@ public class QARunner extends AbstractRunner {
 				Console: ${jobBuildUrl}/console"""
 
         //        def to = Configuration.get("email_list") + "," + Configuration.get(Configuration.Parameter.ADMIN_EMAILS)
-        //def to = Configuration.get(Configuration.Parameter.ADMIN_EMAILS)
+        def to = Configuration.get(Configuration.Parameter.ADMIN_EMAILS)
         //TODO: enable emailing but seems like it should be moved to the notification code
         context.emailext Executor.getEmailParams(body, subject, to)
         return failureReason
