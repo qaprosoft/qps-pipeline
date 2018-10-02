c3f12e73c9e263b87297a6974d197b4057e04e9f
tsvrko <tsvrko@gmail.com>
2018-10-02 14:42:02 +0300

    rawBuild.getLog(10)

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -726,8 +726,7 @@ public class QARunner extends AbstractRunner {
         def subject = Executor.getFailureSubject("UNRECOGNIZED FAILURE", jobName, env, buildNumber)
 
         if (currentBuild.rawBuild.log.contains("COMPILATION ERROR : ")) {
-            context.println "DUMP: " + context.currentBuild.dump()
-            context.println "DUMP RAWBUIlD: " + context.currentBuild.rawBuild.dump()
+            context.println "DUMP: " + context.currentBuild.rawBuild.getLog(10)
             failureReason = "COMPILATION ERROR"
             bodyHeader = "<p>Unable to execute tests due to the compilation failure. ${jobBuildUrl}</p>"
             subject = Executor.getFailureSubject("COMPILATION FAILURE", jobName, env, buildNumber)
