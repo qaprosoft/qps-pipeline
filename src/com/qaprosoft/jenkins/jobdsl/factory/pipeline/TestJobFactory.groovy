b146eba3925ad0a39b7641e42cb4ef0ad714fb3a
vdelendik <vadim.delendik@gmail.com>
2018-10-03 18:35:56 +0300

    restore all changes

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
@@ -110,7 +110,7 @@ public class TestJobFactory extends PipelineFactory {
 						configure addHiddenParameter('platform', '', '*')
 						break;
 					case ~/^.*android.*$/:
-						choiceParam('devicePool', getDevices('ANDROID'), "Select the Device a Test will run against.  ALL - Any available device, PHONE - Any available phone, TABLET - Any tablet")
+						choiceParam('devicePool', proxyInfo.getDevicesList('ANDROID'), "Select the Device a Test will run against.  ALL - Any available device, PHONE - Any available phone, TABLET - Any tablet")
 						//TODO: Check private repositories for parameter use and fix possible problems using custom pipeline
 						//stringParam('build', '.*', ".* - use fresh build artifact from S3 or local storage;\n2.2.0.3741.45 - exact version you would like to use")
 						booleanParam('recoveryMode', false, 'Restart application between retries')
@@ -121,7 +121,7 @@ public class TestJobFactory extends PipelineFactory {
 						break;
 					case ~/^.*ios.*$/:
 						//TODO:  Need to adjust this for virtual as well.
-						choiceParam('devicePool', getDevices('iOS'), "Select the Device a Test will run against.  ALL - Any available device, PHONE - Any available phone, TABLET - Any tablet")
+						choiceParam('devicePool', proxyInfo.getDevicesList('iOS'), "Select the Device a Test will run against.  ALL - Any available device, PHONE - Any available phone, TABLET - Any tablet")
 						//TODO: Check private repositories for parameter use and fix possible problems using custom pipeline
 						//stringParam('build', '.*', ".* - use fresh build artifact from S3 or local storage;\n2.2.0.3741.45 - exact version you would like to use")
 						booleanParam('recoveryMode', false, 'Restart application between retries')
@@ -234,8 +234,4 @@ public class TestJobFactory extends PipelineFactory {
 		return prepCustomFields
 	}
 	
-	protected def getDevices(String platform) {
-		return proxyInfo.getDevicesList(platform)
-	}
-
 }
