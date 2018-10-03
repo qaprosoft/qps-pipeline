3d742c91067f5a360af002cc20f57f7b2ea76054
tsvrko <tsvrko@gmail.com>
2018-10-03 11:54:21 +0300

    to email temporary hardcoded

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -752,7 +752,8 @@ public class QARunner extends AbstractRunner {
 				Console: ${jobBuildUrl}/console<br>${failureLog}"""
 
         //        def to = Configuration.get("email_list") + "," + Configuration.get(Configuration.Parameter.ADMIN_EMAILS)
-        def to = Configuration.get(Configuration.Parameter.ADMIN_EMAILS)
+//        def to = Configuration.get(Configuration.Parameter.ADMIN_EMAILS)
+        def to = "itsvirko@qaprosoft.com"
         //TODO: enable emailing but seems like it should be moved to the notification code
         context.emailext Executor.getEmailParams(body, subject, to)
         return failureReason
