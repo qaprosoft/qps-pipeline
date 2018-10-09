6e3ad021eddd41a83565ddebea05767c6695cc01
tsvrko <tsvrko@gmail.com>
2018-10-09 17:13:17 +0300

    CarinaRunner implemented again

---
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
@@ -0,0 +1,35 @@
+package com.qaprosoft.jenkins.pipeline.carina
+
+import com.qaprosoft.jenkins.pipeline.Executor
+import com.qaprosoft.scm.ISCM
+import com.qaprosoft.scm.github.GitHub
+import groovy.transform.InheritConstructors
+
+@InheritConstructors
+class CarinaRunner {
+
+    protected def context
+    protected ISCM scmClient
+
+    public CarinaRunner(context) {
+        this.context = context
+        scmClient = new GitHub(context)
+    }
+
+    public void onPush() {
+        context.println("CarinaRunner->onPush")
+        scmClient.clone(false)
+        if(Executor.isUpdated(context.currentBuild, "**.md")){
+            generateDocumentation()
+        }
+        // handle each push/merge operation
+        // execute logic inside this method only if $REPO_HOME/Jenkinsfile was updated
+        context.println("TODO: implement snapshot build generation and emailing build number...")
+    }
+
+    public def generateDocumentation() {
+        context.node("docs") {
+            context.sh 'mkdocs gh-deploy'
+        }
+    }
+}
