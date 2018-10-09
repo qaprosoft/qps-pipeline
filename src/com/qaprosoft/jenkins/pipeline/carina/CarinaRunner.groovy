d5aa0df65553136a168538633a19cba97dc588d7
tsvrko <tsvrko@gmail.com>
2018-10-09 17:27:02 +0300

    node name mistype fixed

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
@@ -17,7 +17,7 @@ class CarinaRunner {
     }
 
     public void onPush() {
-        context.node("docs") {
+        context.node("master") {
             context.println("CarinaRunner->onPush")
             scmClient.clone(false)
             if(Executor.isUpdated(context.currentBuild, "**.md")){
