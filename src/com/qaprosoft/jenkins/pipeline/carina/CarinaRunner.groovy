681c760efe5ab5d4dbd4f2e236b94eee08104df0
tsvrko <tsvrko@gmail.com>
2018-10-10 12:19:08 +0300

    update git refs before docs deploy

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
@@ -21,8 +21,8 @@ class CarinaRunner {
             context.println("CarinaRunner->onPush")
             scmClient.clonePush()
             if(Executor.isUpdated(context.currentBuild, "**.md")){
-                context.sh 'mkdocs gh-deploy'
                 context.sh 'git update-ref -d'
+                context.sh 'mkdocs gh-deploy'
             }
             context.deleteDir()
             // handle each push/merge operation
