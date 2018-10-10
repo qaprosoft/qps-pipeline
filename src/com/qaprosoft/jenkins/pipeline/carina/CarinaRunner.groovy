cb8ba3554bfca2246ec97907f74084a53aed569c
tsvrko <tsvrko@gmail.com>
2018-10-10 14:40:34 +0300

    executeMavenGoals logic brought back

--- src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/carina/CarinaRunner.groovy
@@ -24,8 +24,15 @@ class CarinaRunner {
                 context.sh 'mkdocs gh-deploy'
             }
             context.deleteDir()
-            executeMavenGoals()
-
+            context.println "CARINA_RELEASE: " + context.env.getEnvironment().get("CARINA_RELEASE")
+            executeMavenGoals("versions:set -DnewVersion=${context.env.getEnvironment().get("CARINA_RELEASE")}.${context.env.getEnvironment().get("BUILD_NUMBER")}-SNAPSHOT")
+            executeMavenGoals("-Dgpg.passphrase=Qaprosoft2015 -Dcobertura.report.format=xml cobertura:cobertura clean deploy javadoc:javadoc")
+            /*                context.withCredentials([context.usernamePassword(credentialsId:'gpg_token', usernameVariable:'USERNAME', passwordVariable:'PASSWORD')]) {
+                            context.echo "USERNAME: ${context.env.USERNAME}"
+                            context.echo "PASSWORD: ${context.env.PASSWORD}"
+                            executeMavenGoals("-Dgpg.passphrase=${context.env.PASSWORD} -Dcobertura.report.format=xml cobertura:cobertura clean deploy javadoc:javadoc")
+                        }
+        */
             // handle each push/merge operation
             // execute logic inside this method only if $REPO_HOME/Jenkinsfile was updated
             context.println("TODO: implement snapshot build generation and emailing build number...")
@@ -39,4 +46,6 @@ class CarinaRunner {
             context.bat "mvn -B ${goals}"
         }
     }
+
+
 }
