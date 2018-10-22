09c868e897b9560045b903ee147e624cbda90e77
tsvrko <tsvrko@gmail.com>
2018-10-22 19:18:28 +0300

    Logger class implementation

---
+++ src/com/qaprosoft/jenkins/Logger.groovy
@@ -0,0 +1,35 @@
+package com.qaprosoft.jenkins
+
+class Logger {
+
+    private static final LEVEL_MAP = ["DEBUG": 1, "INFO": 2, "WARN": 3, "ERROR": 4]
+
+    public static def debug(logLevel, message){
+        def logMessage = ""
+        if(LEVEL_MAP.get(logLevel) < 2){
+            logMessage = message
+        }
+        return logMessage
+    }
+
+    public static def info(logLevel, message){
+        def logMessage = ""
+        if(LEVEL_MAP.get(logLevel) < 3){
+            logMessage = message
+        }
+        return logMessage
+    }
+
+    public static def warn(logLevel, message){
+        def logMessage = ""
+        if(LEVEL_MAP.get(logLevel) < 4){
+            logMessage = message
+        }
+        return logMessage
+    }
+
+    public static def error(logLevel, message){
+        return message
+    }
+
+}
