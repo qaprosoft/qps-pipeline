11a39ddec467f8b3ef4672f8c769ec06138ecc25
tsvrko <tsvrko@gmail.com>
2018-10-23 17:09:53 +0300

    logger test in Factory script and DscFactory class

--- src/com/qaprosoft/jenkins/jobdsl/Factory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/Factory.groovy
@@ -1,25 +1,23 @@
 package com.qaprosoft.jenkins.jobdsl
 
+import com.qaprosoft.jenkins.Logger
 import com.qaprosoft.jenkins.Utils
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
 
 // groovy script for initialization and execution all kind of jobdsl factories which are transfered from pipeline scanner script
 
-import groovy.json.*
-
-println "LGLVL: " + binding.variables.PIPELINE_LOG_LEVEL
+def logLevel = binding.variables.PIPELINE_LOG_LEVEL
 def slurper = new JsonSlurper()
-
 String factoryDataMap = readFileFromWorkspace("factories.json")
-def prettyPrint = JsonOutput.prettyPrint(factoryDataMap)
-println "factoryDataMap: " + prettyPrint
+printf Logger.info(logLevel,"FactoryDataMap: ${JsonOutput.prettyPrint(factoryDataMap)}")
 def factories = new HashMap(slurper.parseText(factoryDataMap))
-
 factories.each{
 	try {
 		def factory = Class.forName(it.value.clazz)?.newInstance(this)
-		//println "before load: " + it.value.dump()
+        printf Logger.info(logLevel, "Factory before load: ${it.value.dump()}")
 		factory.load(it.value)
-		//println "factory after load: " + factory.dump()
+        printf Logger.info(logLevel, "Factory after load: ${factory.dump()}")
 		factory.create()
 	} catch (Exception e) {
 		println Utils.printStackTrace(e)
