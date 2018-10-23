d9e08214a2dc7858253f87ce16b7ec90e685d5bf
tsvrko <tsvrko@gmail.com>
2018-10-23 13:48:23 +0300

    define log level in DslFactory class

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
@@ -15,7 +15,7 @@ public class TestJobFactory extends PipelineFactory {
 	def zafira_project
 	def suitePath
 	def suiteName
-    def logLevel
+
 
 	public TestJobFactory(folder, pipelineScript, project, sub_project, zafira_project, suitePath, suiteName, jobDesc) {
 		this.folder = folder
@@ -26,7 +26,6 @@ public class TestJobFactory extends PipelineFactory {
 		this.zafira_project = zafira_project
 		this.suitePath = suitePath
 		this.suiteName = suiteName
-        logLevel = super._dslFactory.binding.variables.PIPELINE_LOG_LEVEL
 	}
 
 	def create() {
