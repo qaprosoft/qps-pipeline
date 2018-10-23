cc0cdfdaddd63a83586b3227e2d87f0952259df4
tsvrko <tsvrko@gmail.com>
2018-10-23 17:12:10 +0300

    test values

--- src/com/qaprosoft/jenkins/jobdsl/Factory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/Factory.groovy
@@ -16,8 +16,10 @@ factories.each{
 	try {
 		def factory = Class.forName(it.value.clazz)?.newInstance(this)
         printf Logger.info(logLevel, "Factory before load: ${it.value.dump()}")
+        println "111"
 		factory.load(it.value)
         printf Logger.info(logLevel, "Factory after load: ${factory.dump()}")
+        println "222"
 		factory.create()
 	} catch (Exception e) {
 		println Utils.printStackTrace(e)
