b30554b9a0c01e8d356c4afc8d680da3a58c3888
tsvrko <tsvrko@gmail.com>
2018-10-24 17:46:42 +0300

    log methods completely refactored

--- src/com/qaprosoft/Utils.groovy
+++ src/com/qaprosoft/Utils.groovy
@@ -1,36 +1,6 @@
 package com.qaprosoft
 
 class Utils {
-
-    private static final LEVEL_MAP = ["DEBUG": 1, "INFO": 2, "WARN": 3, "ERROR": 4]
-
-    static def debug(logLevel, String message){
-        def logMessage = ""
-        if(LEVEL_MAP.get(logLevel) < 2){
-            logMessage = "${message}\n"
-        }
-        return logMessage
-    }
-
-    static def info(logLevel, String message){
-        def logMessage = ""
-        if(LEVEL_MAP.get(logLevel) < 3){
-            logMessage = "${message}\n"
-        }
-        return logMessage
-    }
-
-    static def warn(logLevel, String message){
-        def logMessage = ""
-        if(LEVEL_MAP.get(logLevel) < 4){
-            logMessage = "${message}\n"
-        }
-        return logMessage
-    }
-
-    static def error(logLevel, message){
-        return "${message}\n"
-    }
     
     static def printStackTrace(Exception e) {
         def stringStacktrace = ""
