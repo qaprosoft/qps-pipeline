ce70ee9481394810c0e84f092092dae670e93e69
tsvrko <tsvrko@gmail.com>
2018-10-25 13:51:11 +0300

    binding variables dump

--- src/com/qaprosoft/scm/github/GitHub.groovy
+++ src/com/qaprosoft/scm/github/GitHub.groovy
@@ -13,7 +13,7 @@ class GitHub implements ISCM {
 
 	public GitHub(context) {
 		this.context = context
-        context.println "PPLNDUMP: " + context.binding.dump()
+        context.println "PPLNDUMP: " + context.binding.variables.dump()
         logger = new Logger(context)
         gitHtmlUrl = Configuration.resolveVars("${Configuration.get(Configuration.Parameter.GITHUB_HTML_URL)}/${Configuration.get("project")}")
         gitSshUrl = Configuration.resolveVars("${Configuration.get(Configuration.Parameter.GITHUB_SSH_URL)}/${Configuration.get("project")}")
