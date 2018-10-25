fe430077dcbd141f7bd1719ac521937fefb88979
tsvrko <tsvrko@gmail.com>
2018-10-25 16:26:36 +0300

    AbstractRunner formatting 2

--- src/com/qaprosoft/jenkins/pipeline/AbstractRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/AbstractRunner.groovy
@@ -7,21 +7,21 @@ public abstract class AbstractRunner {
     protected def context
     protected ISCM scmClient
     protected Logger logger
-	protected final def FACTORY_TARGET = "qps-pipeline/src/com/qaprosoft/jenkins/jobdsl/Factory.groovy"
-	protected def additionalClasspath = "qps-pipeline/src"
+    protected final def FACTORY_TARGET = "qps-pipeline/src/com/qaprosoft/jenkins/jobdsl/Factory.groovy"
+    protected def additionalClasspath = "qps-pipeline/src"
 
-	//this is very important line which should be declared only as a class member!
-	protected Configuration configuration = new Configuration(context)
-	
-	public AbstractRunner(context) {
-		this.context = context
+    //this is very important line which should be declared only as a class member!
+    protected Configuration configuration = new Configuration(context)
+
+    public AbstractRunner(context) {
+        this.context = context
         this.logger = new Logger(context)
-	}
+    }
 
-	//Methods
-	abstract public void build()
-	
-	//Events
-	abstract public void onPush()
-	abstract public void onPullRequest()
+    //Methods
+    abstract public void build()
+
+    //Events
+    abstract public void onPush()
+    abstract public void onPullRequest()
 }
