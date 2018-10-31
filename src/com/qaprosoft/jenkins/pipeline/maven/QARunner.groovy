e9a8ead68ac232beef7044f9da410e0548de66d0
tsvrko <tsvrko@gmail.com>
2018-10-31 18:51:40 +0300

    possibly correct queue registration value extracting

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -829,7 +829,7 @@ public class QARunner extends AbstractRunner {
         if(!Executor.isParamEmpty(queueRegistration)){
             context.println "QUEUE: " + queueRegistration
             Configuration.set("QUEUE_REGISTRATION", queueRegistration)
-            context.println "QUEUE_REGISTRATION1:" + Configuration.Parameter.QUEUE_REGISTRATION.getValue()
+            context.println "QUEUE_REGISTRATION1:" + Configuration.get(Configuration.Parameter.QUEUE_REGISTRATION)
         }
 
         def jenkinsMultipleLanguages = currentSuite.getParameter("jenkinsMultipleLanguages")
@@ -932,8 +932,8 @@ public class QARunner extends AbstractRunner {
                             pipelineMap.put("env", supportedEnv)
                             pipelineMap.put("order", orderNum)
                             pipelineMap.put("BuildPriority", priorityNum)
-                            context.println "QUEUE_REGISTRATION2:" + Configuration.Parameter.QUEUE_REGISTRATION.getValue()
-                            pipelineMap.put("QUEUE_REGISTRATION", Configuration.Parameter.QUEUE_REGISTRATION.getValue())
+                            context.println "QUEUE_REGISTRATION2:" + Configuration.get(Configuration.Parameter.QUEUE_REGISTRATION)
+                            pipelineMap.put("QUEUE_REGISTRATION", Configuration.get(Configuration.Parameter.QUEUE_REGISTRATION))
                             Executor.putNotNullWithSplit(pipelineMap, "emailList", emailList)
                             Executor.putNotNullWithSplit(pipelineMap, "executionMode", executionMode)
                             Executor.putNotNull(pipelineMap, "overrideFields", overrideFields)
