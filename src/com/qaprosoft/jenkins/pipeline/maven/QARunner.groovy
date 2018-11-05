2c2de57bb1b09791fe1f7793aa92d8f7f37b2108
tsvrko <tsvrko@gmail.com>
2018-11-05 19:21:17 +0300

    RSLT 2

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -708,7 +708,7 @@ public class QARunner extends AbstractRunner {
             zc.sendEmail(uuid, emailList, "all")
         }
         context.println "ISFLR: " + isFailure(currentBuild.rawBuild)
-        context.println "RSLT: " + currentBuild.result.dump()
+        context.println "RSLT: " + currentBuild.result
         if (isFailure(currentBuild.rawBuild) && failureEmailList != null && !failureEmailList.isEmpty()) {
             zc.sendEmail(uuid, failureEmailList, "failures")
         }
