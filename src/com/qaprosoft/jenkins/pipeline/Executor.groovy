28d01d262b53d07f3648033bb3749565cf114454
tsvrko <tsvrko@gmail.com>
2018-11-05 18:27:17 +0300

    equals replaced with operator

--- src/com/qaprosoft/jenkins/pipeline/Executor.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Executor.groovy
@@ -124,7 +124,7 @@ public class Executor {
      static boolean isFailure(currentBuild) {
         boolean failure = false
         if (currentBuild.result) {
-            failure = "FAILURE".equals(currentBuild.result.name)
+            failure = BuildResult.FAILURE == currentBuild.result.name
         }
         return failure
     }
