1338db3a5d0bd88d84ad13ed5a76ee7fe88b9243
tsvrko <tsvrko@gmail.com>
2018-11-05 13:37:46 +0300

    return added into runCron() method

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -779,6 +779,9 @@ public class QARunner extends AbstractRunner {
                     logger.info("Number of Test Suites to Scan Through: " + files.length)
                     for (int i = 0; i < files.length; i++) {
                         def currentSuite = parsePipeline(workspace + "/p" + files[i].path)
+                        if(!currentSuite){
+                            return
+                        }
                         def supportedLanguages = getPipelineLanguages(currentSuite)
                         if (supportedLanguages.size() > 0){
                             supportedLanguages.each { language ->
@@ -823,11 +826,9 @@ public class QARunner extends AbstractRunner {
         } catch (FileNotFoundException e) {
             logger.error("ERROR! Unable to find suite: " + filePath)
             logger.error(Utils.printStackTrace(e))
-            return
         } catch (Exception e) {
             logger.error("ERROR! Unable to parse suite: " + filePath)
             logger.error(Utils.printStackTrace(e))
-            return
         }
         return currentSuite
     }
