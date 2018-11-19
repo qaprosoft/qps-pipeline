07b906bcb80eb393d865473f2056e85d5170d9de
tsvrko <tsvrko@gmail.com>
2018-11-19 19:10:11 +0300

    deleteTestRun from api

--- src/com/qaprosoft/testrail/TestRailClient.groovy
+++ src/com/qaprosoft/testrail/TestRailClient.groovy
@@ -51,6 +51,17 @@ class TestRailClient {
         }
     }
 
+    public def deleteTestRun(testRunId) {
+        context.withCredentials([context.usernamePassword(credentialsId:'testrail_creds', usernameVariable:'USERNAME', passwordVariable:'PASSWORD')]) {
+            def parameters = [customHeaders: [[name: 'Authorization', value: "Basic ${encodeToBase64("${context.env.USERNAME}:${context.env.PASSWORD}")}"]],
+                              contentType: 'APPLICATION_JSON',
+                              httpMode: 'POST',
+                              validResponseCodes: "200",
+                              url: this.serviceURL + "delete_run/${testRunId}"]
+            return sendRequest(parameters)
+        }
+    }
+
     public def getUserIdByEmail(userEmail) {
         context.withCredentials([context.usernamePassword(credentialsId:'testrail_creds', usernameVariable:'USERNAME', passwordVariable:'PASSWORD')]) {
             def parameters = [customHeaders: [[name: 'Authorization', value: "Basic ${encodeToBase64("${context.env.USERNAME}:${context.env.PASSWORD}")}"]],
