72edda80a7fe1d722b62dfbfdcd8c4389c660b22
vdelendik <vadim.delendik@gmail.com>
2018-11-21 20:22:22 +0300

    more logging for github client

--- src/com/qaprosoft/scm/github/GitHub.groovy
+++ src/com/qaprosoft/scm/github/GitHub.groovy
@@ -42,7 +42,14 @@ class GitHub implements ISCM {
             def branch = Configuration.get("branch")
             def project = Configuration.get("project")
             def userId = Configuration.get("BUILD_USER_ID")
+			
+			logger.info("GITHUB_HOST: " + Configuration.get("GITHUB_HOST"))
+			logger.info("GITHUB_ORGANIZATION: " + Configuration.get("GITHUB_ORGANIZATION"))
+			
+			logger.info("gitSshUrl: " + gitSshUrl)
             def gitUrl = Configuration.resolveVars(gitSshUrl)
+			
+			logger.info("gitUrl: " + gitUrl)
             def scmVars = [:]
 
             logger.info("GIT_URL: " + gitUrl)
