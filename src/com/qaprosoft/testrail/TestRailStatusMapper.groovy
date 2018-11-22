f47377a1ca39b2cdd84a6bd5e9dc2e160bed8790
tsvrko <tsvrko@gmail.com>
2018-11-22 18:50:07 +0300

    value called as property

--- src/com/qaprosoft/testrail/TestRailStatusMapper.groovy
+++ src/com/qaprosoft/testrail/TestRailStatusMapper.groovy
@@ -30,7 +30,7 @@ class TestRailStatusMapper {
         ZafiraStatus zafiraStatus = ZafiraStatus.valueOf(zafiraStringStatus)
         for(testRailStatus in TestRailStatus.values()){
             if(testRailStatus.value == zafiraStatus.value) {
-                return testRailStatus.value()
+                return testRailStatus.value
             }
         }
     }
