8164e088845196571800bfdcd90271fc7dc2aeed
tsvrko <tsvrko@gmail.com>
2018-11-23 17:16:23 +0300

    REQUEST:

--- src/com/qaprosoft/testrail/TestRailClient.groovy
+++ src/com/qaprosoft/testrail/TestRailClient.groovy
@@ -118,7 +118,7 @@ class TestRailClient {
     public def addResultsForCases(testRunId, results) {
         JsonBuilder jsonBuilder = new JsonBuilder()
         jsonBuilder results: results
-        logger.info("REQUEST:\n" + formatJson(jsonBuilder.toString()))
+        logger.info("REQUEST:\n" + formatJson(jsonBuilder))
         context.withCredentials([context.usernamePassword(credentialsId:'testrail_creds', usernameVariable:'USERNAME', passwordVariable:'PASSWORD')]) {
             def parameters = [customHeaders: [[name: 'Authorization', value: "Basic ${encodeToBase64("${context.env.USERNAME}:${context.env.PASSWORD}")}"]],
                               contentType: 'APPLICATION_JSON',
