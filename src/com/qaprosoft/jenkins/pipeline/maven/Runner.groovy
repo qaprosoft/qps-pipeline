130bf5edcd16f7bdafbbb03c19f4b3db7879297b
tsvrko <tsvrko@gmail.com>
2018-11-28 17:37:47 +0300

    import paths refactored

--- src/com/qaprosoft/jenkins/pipeline/maven/Runner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/Runner.groovy
@@ -1,7 +1,7 @@
 package com.qaprosoft.jenkins.pipeline.maven
 
 import com.qaprosoft.Logger
-import com.qaprosoft.scm.github.GitHub;
+import com.qaprosoft.scm.github.GitHub
 import com.qaprosoft.jenkins.pipeline.AbstractRunner
 
 //[VD] do not remove this important import!
