eb786688ef0d62cd5510f0d9864991be2781c4d7
tsvrko <tsvrko@gmail.com>
2018-11-30 16:36:20 +0300

    uuid hardcoded

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -367,6 +367,7 @@ public class QARunner extends AbstractRunner {
     protected void runJob() {
         logger.info("QARunner->runJob")
         uuid = getUUID()
+        uuid = "0652b374-274b-4181-8f2c-fe1e103f06e4"
         logger.info("UUID: " + uuid)
         def isRerun = isRerun()
         logger.info("SEARCH: " + isRerun)
