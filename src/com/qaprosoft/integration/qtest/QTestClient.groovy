a0a8efafa32fb98de0399d36211274f6d5a7f947
tsvrko <tsvrko@gmail.com>
2018-11-30 13:47:49 +0300

    Merge branches 'demo' and 'master' of https://github.com/qaprosoft/qps-pipeline into demo
    # Conflicts:
    #	src/com/qaprosoft/zafira/ZafiraClient.groovy

--- src/com/qaprosoft/integration/qtest/QTestClient.groovy
+++ src/com/qaprosoft/integration/qtest/QTestClient.groovy
@@ -49,4 +49,15 @@ class QTestClient extends HttpClient{
             return sendRequestFormatted(parameters)
         }
     }
+
+    public def getToken() {
+        context.withCredentials([context.usernamePassword(credentialsId:'qtest_creds', usernameVariable:'USERNAME', passwordVariable:'PASSWORD')]) {
+            def parameters = [customHeaders: [[name: 'Authorization', value: "Basic ${encodeToBase64("${Configuration.get("SITE_NAME")}:")}"]],
+                              contentType: 'APPLICATION_FORM',
+                              httpMode: 'POST',
+                              validResponseCodes: "200:401",
+                              url: "https://${Configuration.get("SITE_NAME")}/#/oath/token?grant_type=password&username=${context.env.USERNAME}&password=${context.env.PASSWORD}"]
+            return sendRequestFormatted(parameters)
+        }
+    }
 }
