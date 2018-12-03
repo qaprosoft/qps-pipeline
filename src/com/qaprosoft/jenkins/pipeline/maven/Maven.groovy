fd36a40e14b0c2d08e20bb3be2d915a9d7c8112d
vdelendik <vadim.delendik@gmail.com>
2018-12-03 22:23:48 +0300

    wrapped parameter in quotas

--- src/com/qaprosoft/jenkins/pipeline/maven/Maven.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/Maven.groovy
@@ -29,7 +29,7 @@ public class Maven {
 			maven: 'M3',
 			// Maven settings.xml file defined with the Jenkins Config File Provider Plugin
 			// Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
-			mavenSettingsConfig: ${mavenSettingsConfig},
+			mavenSettingsConfig: "${mavenSettingsConfig}",
 			mavenLocalRepo: '.repository') {
 	 
 			// Run the maven build
