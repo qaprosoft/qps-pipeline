483977c3f07c9cdcd141f84f78815f1b928993da
vdelendik <vadim.delendik@gmail.com>
2018-12-03 18:47:22 +0300

    migrate all maven commands to use new Mixin class declaration

--- src/com/qaprosoft/jenkins/pipeline/maven/Maven.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/Maven.groovy
@@ -2,8 +2,8 @@ package com.qaprosoft.jenkins.pipeline.maven
 
 public class Maven {
 	
-	public void executeMavenGoals2(goals) {
-		logger.info("Maven mixing->executeMavenGoals2")
+	public void executeMavenGoals(goals) {
+		logger.debug("Maven mixing->executeMavenGoals")
 		context.withMaven(
 			// Maven installation declared in the Jenkins "Global Tool Configuration"
 			maven: 'M3',
@@ -19,9 +19,27 @@ public class Maven {
 			} else {
 				context.bat "mvn -B ${goals}"
 			}
-
-	 
-		} // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe & FindBugs reports...
-
+		}
 	}
+	
+	//TODO: 
+/*	public void executeMavenGoals(goals, maven, mavenLocalRepo, mavenSettingsConfig) {
+		logger.info("Maven mixing->executeMavenGoals")
+		context.withMaven(
+			// Maven installation declared in the Jenkins "Global Tool Configuration"
+			maven: 'M3',
+			// Maven settings.xml file defined with the Jenkins Config File Provider Plugin
+			// Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
+			//mavenSettingsConfig: 'settings',
+			mavenLocalRepo: '.repository') {
+	 
+			// Run the maven build
+			
+			if (context.isUnix()) {
+				context.sh "'mvn' -B ${goals}"
+			} else {
+				context.bat "mvn -B ${goals}"
+			}
+		}
+	}*/
 }
