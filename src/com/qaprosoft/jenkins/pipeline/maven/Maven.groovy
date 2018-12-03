f8700a64fbeb7079c146b147d5ffe9a650d1726c
vdelendik <vadim.delendik@gmail.com>
2018-12-03 23:51:36 +0300

    added fully parametrized executeMavenGoals

--- src/com/qaprosoft/jenkins/pipeline/maven/Maven.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/Maven.groovy
@@ -2,7 +2,7 @@ package com.qaprosoft.jenkins.pipeline.maven
 
 public class Maven {
 	
-	public void executeMavenGoals(String goals) {
+	public void executeMavenGoals(goals) {
 		logger.debug("Maven mixing->executeMavenGoals")
 		context.withMaven(
 			// Maven installation declared in the Jenkins "Global Tool Configuration"
@@ -31,6 +31,26 @@ public class Maven {
 			// Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
 			mavenSettingsConfig: "${mavenSettingsConfig}",
 			mavenLocalRepo: '.repository') {
+	 
+			// Run the maven build
+			
+			if (context.isUnix()) {
+				context.sh "'mvn' -B ${goals}"
+			} else {
+				context.bat "mvn -B ${goals}"
+			}
+		}
+	}
+	
+	public void executeMavenGoals(goals, mavenTool, mavenSettingsConfig, mavenLocalRepo) {
+		logger.info("Maven mixing->executeMavenGoals")
+		context.withMaven(
+			// Maven installation declared in the Jenkins "Global Tool Configuration"
+			maven: "${mavenTool}",
+			// Maven settings.xml file defined with the Jenkins Config File Provider Plugin
+			// Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
+			mavenSettingsConfig: "${mavenSettingsConfig}",
+			mavenLocalRepo: "${mavenLocalRepo}") {
 	 
 			// Run the maven build
 			
