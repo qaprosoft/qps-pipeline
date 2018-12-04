8666bebefde94354da0a02837f3741bf2d60d212
vdelendik <vadim.delendik@gmail.com>
2018-12-04 14:27:13 +0300

    added TODO comment

--- src/com/qaprosoft/jenkins/pipeline/maven/Maven.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/Maven.groovy
@@ -1,6 +1,8 @@
 package com.qaprosoft.jenkins.pipeline.maven
 
 public class Maven {
+	//TODO: migreate to traits as only it is supported in pipelines
+	// https://issues.jenkins-ci.org/browse/JENKINS-46145
 	
 	public void executeMavenGoals(goals) {
 		logger.debug("Maven mixing->executeMavenGoals")
