bd48441e6fd7afa7b8d146d3c5940e26e4cd56f9
tsvrko <tsvrko@gmail.com>
2018-12-14 17:22:06 +0300

    else

--- src/com/qaprosoft/integration/zafira/ZafiraUpdater.groovy
+++ src/com/qaprosoft/integration/zafira/ZafiraUpdater.groovy
@@ -42,8 +42,7 @@ class ZafiraUpdater {
 
     public def smartRerun() {
         def response = zc.smartRerun()
-        logger.info(response.dump())
-        if(isParamEmpty(response) || response.size() == 0){
+        if(response.size() == 0){
             "No tests for rerun!"
         } else {
             logger.info("Results : " + response.size())
