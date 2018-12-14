ed28c9b08ae9e824f9c0a276492ac11830630ba8
tsvrko <tsvrko@gmail.com>
2018-12-14 16:19:24 +0300

    call smart rerun from another job

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -374,9 +374,9 @@ public class QARunner extends AbstractRunner {
         logger.info("SEARCH: " + isRerun)
         String nodeName = "master"
 
-//        context.stage('Rerun Tests'){
-//            zc.smartRerun()
-//        }
+        context.stage('Rerun Tests'){
+            zc.smartRerun()
+        }
         context.node(nodeName) {
             zafiraUpdater.queueZafiraTestRun(uuid)
             nodeName = chooseNode()
