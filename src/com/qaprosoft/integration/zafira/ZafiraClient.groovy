651589d1790d1310a3a8d53f1c799c5a3162fbf2
tsvrko <tsvrko@gmail.com>
2018-12-14 15:49:03 +0300

    no formatting

--- src/com/qaprosoft/integration/zafira/ZafiraClient.groovy
+++ src/com/qaprosoft/integration/zafira/ZafiraClient.groovy
@@ -164,7 +164,7 @@ class ZafiraClient extends HttpClient{
 						  httpMode: 'POST',
                           requestBody: "${jsonBuilder}",
 						  url: this.serviceURL + "/api/auth/refresh"]
-        Map properties = (Map)sendRequestFormatted(parameters)
+        Map properties = (Map)sendRequest(parameters)
 
 		authToken = properties.type + " " + properties.accessToken
 		tokenExpTime = System.currentTimeMillis() + 290 * 60 * 1000
