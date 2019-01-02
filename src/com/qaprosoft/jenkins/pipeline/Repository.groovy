368d5c3074e2a4433173f48f3f41946cee9d8484
tsvrko <tsvrko@gmail.com>
2019-01-02 13:51:33 +0300

    rejister jobs in nested folder

--- src/com/qaprosoft/jenkins/pipeline/Repository.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Repository.groovy
@@ -97,17 +97,17 @@ class Repository {
 			//TODO: analyze do we need system jobs for QA repo... maybe prametrize CreateRepository call
 			def gitUrl = Configuration.resolveVars("${Configuration.get(Configuration.Parameter.GITHUB_HTML_URL)}/${Configuration.get("repo")}")
 
-			registerObject("hooks_view", new ListViewFactory(repoFolder, 'SYSTEM', null, ".*onPush.*|.*onPullRequest.*"))
+			registerObject("hooks_view", new ListViewFactory("${organization}/${repoFolder}", 'SYSTEM', null, ".*onPush.*|.*onPullRequest.*"))
 
 			def pullRequestJobDescription = "Customized pull request verification checker"
 
-			registerObject("pull_request_job", new PullRequestJobFactory(repoFolder, getOnPullRequestScript(), "onPullRequest-" + repo, pullRequestJobDescription, repo, gitUrl))
+			registerObject("pull_request_job", new PullRequestJobFactory("${organization}/${repoFolder}", getOnPullRequestScript(), "onPullRequest-" + repo, pullRequestJobDescription, repo, gitUrl))
 
 			def pushJobDescription = "To finish GitHub WebHook setup, please, follow the steps below:\n- Go to your GitHub repository\n- Click \"Settings\" tab\n- Click \"Webhooks\" menu option\n" +
 					"- Click \"Add webhook\" button\n- Type http://your-jenkins-domain.com/github-webhook/ into \"Payload URL\" field\n" +
 					"- Select application/json in \"Content Type\" field\n- Tick \"Send me everything.\" option\n- Click \"Add webhook\" button"
 
-			registerObject("push_job", new PushJobFactory(repoFolder, getOnPushScript(), "onPush-" + repo, pushJobDescription, repo, gitUrl))
+			registerObject("push_job", new PushJobFactory("${organization}/${repoFolder}", getOnPushScript(), "onPush-" + repo, pushJobDescription, repo, gitUrl))
 
 			// put into the factories.json all declared jobdsl factories to verify and create/recreate/remove etc
 			context.writeFile file: "factories.json", text: JsonOutput.toJson(dslObjects)
