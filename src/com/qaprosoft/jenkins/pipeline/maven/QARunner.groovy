48fe292c290aacc83051564a11b927fcffbf8b1d
tsvrko <tsvrko@gmail.com>
2019-01-03 14:31:29 +0300

    get InterruptedBuildAction

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -373,7 +373,7 @@ public class QARunner extends AbstractRunner {
                     zafiraUpdater.abortTestRun(uuid, currentBuild)
                     throw e
                 } finally {
-					logger.info("BUILD_DUMP: " + currentBuild.rawBuild.getActions().dump())
+					logger.info("BUILD_DUMP: " + currentBuild.rawBuild.getActions(jenkins.model.InterruptedBuildAction.class).dump())
                     qTestUpdater.updateTestRun(uuid,  isRerun)
                     testRailUpdater.updateTestRun(uuid, isRerun, true)
                     zafiraUpdater.exportZafiraReport(uuid, getWorkspace())
