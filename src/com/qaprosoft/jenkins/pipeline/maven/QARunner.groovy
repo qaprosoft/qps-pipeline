58b15aa89a481a52d8d1c6de4772f106133e7100
tsvrko <tsvrko@gmail.com>
2019-01-03 14:29:05 +0300

    getActions

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -373,7 +373,7 @@ public class QARunner extends AbstractRunner {
                     zafiraUpdater.abortTestRun(uuid, currentBuild)
                     throw e
                 } finally {
-					logger.info("BUILD_DUMP: " + currentBuild.rawBuild.dump())
+					logger.info("BUILD_DUMP: " + currentBuild.rawBuild.getActions())
                     qTestUpdater.updateTestRun(uuid,  isRerun)
                     testRailUpdater.updateTestRun(uuid, isRerun, true)
                     zafiraUpdater.exportZafiraReport(uuid, getWorkspace())
