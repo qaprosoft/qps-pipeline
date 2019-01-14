18177b14449c8c9e93db0ef27baf49ec64d0f4c4
tsvrko <tsvrko@gmail.com>
2019-01-14 13:40:11 +0300

    skipped marked as failed in testing purposes

--- src/com/qaprosoft/integration/zafira/StatusMapper.groovy
+++ src/com/qaprosoft/integration/zafira/StatusMapper.groovy
@@ -17,7 +17,7 @@ class StatusMapper {
     enum ZafiraStatus {
         PASSED(1),
         FAILED(5),
-        SKIPPED(3),
+        SKIPPED(5),
         ABORTED(3),
         QUEUED(3),
         final int value
