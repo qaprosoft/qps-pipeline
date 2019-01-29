ac67d511a7b81c7f6ae91ce51e599bfb5fcc7989
tsvrko <tsvrko@gmail.com>
2019-01-29 14:58:06 +0300

    GlobalTextareaChoiceListProvider in jobdsl

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
@@ -6,6 +6,7 @@ import org.testng.xml.Parser;
 import org.testng.xml.XmlSuite;
 import com.qaprosoft.selenium.grid.ProxyInfo
 import groovy.transform.InheritConstructors
+import jp.ikedam.jenkins.plugins.extensible_choice_parameter.GlobalTextareaChoiceListProvider
 
 @InheritConstructors
 public class TestJobFactory extends PipelineFactory {
@@ -39,7 +40,8 @@ public class TestJobFactory extends PipelineFactory {
 
 		this.name = currentSuite.getParameter("jenkinsJobName").toString()
 		logger.info("JenkinsJobName: ${name}")
-
+		def mavenDesc = jenkins.model.Jenkins.instance.getExtensionList(jp.ikedam.jenkins.plugins.extensible_choice_parameter.GlobalTextareaChoiceListProvider.DescriptorImpl.class)[0]
+		logger.info(mavenDesc.dump())
 		def pipelineJob = super.create()
 		pipelineJob.with {
 			def scheduling = currentSuite.getParameter("scheduling")
