2a5f3bcc501bd87b460fcdd1e21e1a8821610520
tsvrko <tsvrko@gmail.com>
2019-01-30 12:12:12 +0300

    testng used in launcher

--- src/com/qaprosoft/jenkins/pipeline/maven/Launcher.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/Launcher.groovy
@@ -41,7 +41,7 @@ class Launcher {
     }
 
     protected void prepare() {
-        scmClient.clone(true)
+        scmClient.clone()
         String QPS_PIPELINE_GIT_URL = Configuration.get(Configuration.Parameter.QPS_PIPELINE_GIT_URL)
         String QPS_PIPELINE_GIT_BRANCH = Configuration.get(Configuration.Parameter.QPS_PIPELINE_GIT_BRANCH)
         scmClient.clone(QPS_PIPELINE_GIT_URL, QPS_PIPELINE_GIT_BRANCH, "qps-pipeline")
