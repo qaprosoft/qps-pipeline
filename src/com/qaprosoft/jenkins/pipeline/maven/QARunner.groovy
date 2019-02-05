a9824ba5b5a1a50c5234e6d4b4aa79bfb50ae466
tsvrko <tsvrko@gmail.com>
2019-02-05 18:14:18 +0300

    context.readFile pomFile

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -192,19 +192,21 @@ public class QARunner extends AbstractRunner {
                 }
                 zafiraProject = getZafiraProject(subProjectFilter)
 
-                def pom = context.readMavenPom file: pomFile
-                pom.build.plugins.each { plugin ->
-                    logger.info(plugin.getConfiguration().dump())
-                    logger.info(plugin.configuration.dump())
-                    def suiteXmlFiles = plugin.configuration.getChild("suiteXmlFiles")
-//                    def config = new XmlParser().parseText(plugin.configuration.toString)
-                    logger.info(suiteXmlFiles)
-                    if(plugin.artifactId.contains("surefire")) {
-                        plugin.configuration.suiteXmlFiles.each { suiteXmlFile ->
-                            logger.info(suiteXmlFile)
-                        }
-                    }
-                }
+                def pomContent  = context.readFile pomFile
+                logger.info(pomContent)
+//                def pom = context.readMavenPom file: pomFile
+//                pom.build.plugins.each { plugin ->
+//                    logger.info(plugin.getConfiguration().dump())
+//                    logger.info(plugin.configuration.dump())
+//                    def suiteXmlFiles = plugin.configuration.getChild("suiteXmlFiles")
+////                    def config = new XmlParser().parseText(plugin.configuration.toString)
+//                    logger.info(suiteXmlFiles)
+//                    if(plugin.artifactId.contains("surefire")) {
+//                        plugin.configuration.suiteXmlFiles.each { suiteXmlFile ->
+//                            logger.info(suiteXmlFile)
+//                        }
+//                    }
+//                }
 
 
 
