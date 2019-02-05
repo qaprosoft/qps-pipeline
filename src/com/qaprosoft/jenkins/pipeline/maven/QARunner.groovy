42d34622e8131d36dc83a5a0570a740fa254f272
tsvrko <tsvrko@gmail.com>
2019-02-05 16:16:02 +0300

    value dump toString

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -179,7 +179,7 @@ public class QARunner extends AbstractRunner {
             pomFiles.each { pomFile ->
 
                 logger.info("POM: " + pomFile.dump())
-                logger.info("POM: " + pomFile.value.dump())
+                logger.info("POM: " + pomFile.value.toString().dump())
                 logger.info("SUB_PROJECT: " + Paths.get(pomFile.value).getParent())
             }
             subProjects.each {
