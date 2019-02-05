a95921ad4141dc931af20c2ebc36b185acd0a178
tsvrko <tsvrko@gmail.com>
2019-02-05 17:54:39 +0300

    config parsing

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -194,7 +194,8 @@ public class QARunner extends AbstractRunner {
 
                 def pom = context.readMavenPom file: pomFile
                 pom.build.plugins.each { plugin ->
-                    logger.info(plugin.configuration.suiteXmlFiles)
+                    def config = new XmlParser().parseText(plugin.configuration.suiteXmlFiles)
+                    logger.info(config)
                     if(plugin.artifactId.contains("surefire")) {
                         plugin.configuration.suiteXmlFiles.each { suiteXmlFile ->
                             logger.info(suiteXmlFile)
