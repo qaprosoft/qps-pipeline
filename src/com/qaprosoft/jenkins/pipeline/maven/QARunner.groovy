79ea78b5af0ec2a06743d10c9b5728904c0cd5b7
tsvrko <tsvrko@gmail.com>
2019-02-06 14:32:44 +0300

    suite.dump()

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -209,7 +209,8 @@ public class QARunner extends AbstractRunner {
                     def suiteName = objectSuitePath.getFileName()
                     def currentSuitePath = workspace + "/" + suitePath
                     logger.info("CONCAT_PATH: " + currentSuitePath)
-                    logger.info("OBJECT_PATH: " + suite.getAbsoluteFile())
+                    logger.info(suite.dump())
+                    logger.info("OBJECT_PATH: " + objectSuitePath.toAbsolutePath())
                     XmlSuite currentSuite = parsePipeline(currentSuitePath)
                     if (!isParamEmpty(currentSuite.getParameter("jenkinsJobCreation")) && currentSuite.getParameter("jenkinsJobCreation").toBoolean()) {
 
