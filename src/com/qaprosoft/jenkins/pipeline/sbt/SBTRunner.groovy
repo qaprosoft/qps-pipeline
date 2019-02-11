8d3ae2897a92fef7cce4ec7c00b8e1e89512b843
tamikana <tkantarovich@qaprosoft.com>
2019-02-11 16:59:25 +0300

    add email notificarion 7

--- src/com/qaprosoft/jenkins/pipeline/sbt/SBTRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/sbt/SBTRunner.groovy
@@ -75,7 +75,7 @@ class SBTRunner extends AbstractRunner {
             context.zip archive: true, dir: 'target/gatling/', glob: '', zipFile: randomArchiveName
             //    context.s3CopyArtifact buildSelector: context.lastCompleted(), excludeFilter: '', filter: '*.zip', flatten: false, optional: false, projectName: 'loadTesting/Gatling-load-testing', target: 'jenkins-mobile-artifacts'
            // context.emailext to: "${mails}"
-            context.emailext body: 'Test Text', subject: 'Test', to: 'anazarenko@underarmour.com'
+            context.emailext body: 'Test Text', subject: 'Test', to: 'anazarenko@consultant.underarmour.com'
 
         }
     }
