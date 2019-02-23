820fec879abb46986e4ee661f3e5094b111be73c
tamikana <tkantarovich@qaprosoft.com>
2019-02-23 13:55:20 +0300

    add wait=false

--- src/com/qaprosoft/jenkins/pipeline/sbt/SBTRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/sbt/SBTRunner.groovy
@@ -86,7 +86,7 @@ class SBTRunner extends AbstractRunner {
     protected void uploadResultsToS3() {
         def needToUpload = Configuration.get("needToUpload").toString().toBoolean()
         if (needToUpload) {
-            context.build job: 'loadTesting/Upload-Results-To-S3'
+            context.build job: 'loadTesting/Upload-Results-To-S3', wait: false
         }
     }
 }
