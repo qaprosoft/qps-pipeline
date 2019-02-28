3b7dafd000a452416a9960a6c0f70c463b9f5cd3
tsvrko <tsvrko@gmail.com>
2019-02-28 14:53:59 +0300

    string supported browsers brought back

--- src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/maven/QARunner.groovy
@@ -854,6 +854,9 @@ public class QARunner extends AbstractRunner {
             return
         }
 
+//        def overrideFields = Configuration.get("overrideFields")
+//
+//        def supportedBrowsers = currentSuite.getParameter("jenkinsPipelineBrowsers")
 //        def currentBrowser = !isParamEmpty(Configuration.get("browser"))?Configuration.get("browser"):"NULL"
 //
 //        def logLine = "regressionPipelines: ${regressionPipelines};\n	jobName: ${jobName};\n	" +
@@ -870,7 +873,8 @@ public class QARunner extends AbstractRunner {
         def queueRegistration = !isParamEmpty(currentSuite.getParameter("jenkinsQueueRegistration"))?currentSuite.getParameter("jenkinsQueueRegistration"):Configuration.get("queue_registration")
         def emailList = !isParamEmpty(Configuration.get("email_list"))?Configuration.get("email_list"):currentSuite.getParameter("jenkinsEmail")
         def priorityNum = !isParamEmpty(Configuration.get("BuildPriority"))?Configuration.get("BuildPriority"):"5"
-        def supportedBrowsers = currentSuite.getParameter("jenkinsPipelineBrowsers")
+
+        String supportedBrowsers = currentSuite.getParameter("jenkinsPipelineBrowsers")
         String logLine = "regressionPipelines: ${regressionPipelines};\n	jobName: ${jobName};\n	orderNum: ${orderNum};\n	email_list: ${emailList};\n	supportedEnv(s): ${supportedEnvs};\n	currentEnv(s): ${currentEnvs};\n	supportedBrowsers: ${supportedBrowsers};\n"
 
         def currentBrowser = Configuration.get("browser")
