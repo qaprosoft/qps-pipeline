6347b4a9f9b11571f85c6ed26f7f7a79d7a4a569
tsvrko <tsvrko@gmail.com>
2019-03-04 14:28:29 +0300

    test output is removed

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
@@ -159,7 +159,6 @@ public class TestJobFactory extends PipelineFactory {
 					logger.debug("Parameter: ${param}")
 					def delimiter = "::"
 					if (param.key.contains(delimiter)) {
-						logger.info("PARAMETR: " + param)
 						def (type, name, desc) = param.key.split(delimiter)
 						switch(type.toLowerCase()) {
 							case "hiddenparam":
