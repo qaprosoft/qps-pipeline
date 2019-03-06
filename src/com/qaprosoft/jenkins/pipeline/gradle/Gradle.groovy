46f220f533cd15d7e5ce16923c4f99def52f7774
tsvrko <tsvrko@gmail.com>
2019-03-06 16:05:19 +0300

    build gradle version

--- src/com/qaprosoft/jenkins/pipeline/gradle/Gradle.groovy
+++ src/com/qaprosoft/jenkins/pipeline/gradle/Gradle.groovy
@@ -2,13 +2,13 @@ package com.qaprosoft.jenkins.pipeline.gradle
 
 class Gradle {
 
-    public void buildGradle(){
+    public void buildGradle(version){
         context.stage('Gradle Build') {
             logger.debug("Gradle mixin->buildGradle")
             if (context.isUnix()) {
-                context.sh 'cp config/gradle.properties .'
                 context.sh 'chmod a+x gradlew'
-                context.sh './gradlew build'
+                context.sh 'cp ./config/gradle.$environment.properties ./gradle.properties'
+                context.sh "./gradlew clean build -P version=${version}"
             } else {
                 context.bat 'gradlew.bat build'
             }
