47a12f7ba9f35b761b96c9d9ed1f7860bb16cf7b
tsvrko <tsvrko@gmail.com>
2019-03-20 19:59:13 +0300

    jobdsl results output

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
@@ -186,12 +186,14 @@ public class QARunner extends AbstractRunner {
                 context.writeFile file: "factories.json", text: JsonOutput.toJson(dslObjects)
                 logger.info("factoryTarget: " + FACTORY_TARGET)
                 //TODO: test carefully auto-removal for jobs/views and configs
-                context.jobDsl additionalClasspath: additionalClasspath,
+                def res = context.jobDsl additionalClasspath: additionalClasspath,
                         removedConfigFilesAction: Configuration.get("removedConfigFilesAction"),
                         removedJobAction: Configuration.get("removedJobAction"),
                         removedViewAction: Configuration.get("removedViewAction"),
                         targets: FACTORY_TARGET,
                         ignoreExisting: false
+
+                logger.info("RESLTS: " + res)
 
             }
         }
