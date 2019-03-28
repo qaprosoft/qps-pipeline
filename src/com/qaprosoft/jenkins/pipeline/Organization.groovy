83cca764d65da49d1e35b2fcb25f6626e81db3c2
tsvrko <tsvrko@gmail.com>
2019-03-28 15:27:59 +0300

    created user dump

--- src/com/qaprosoft/jenkins/pipeline/Organization.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Organization.groovy
@@ -105,7 +105,7 @@ class Organization {
 //        logger.info("USER_GET: " + User.getById(userName, false))
 //        logger.info("USER_CREATE: " + Jenkins.instance.securityRealm.createAccount(userName, password))
         def user =  User.getById(userName, true)
-        Jenkins.instance.save()
+        logger.info(user.dump())
         return user
     }
 
