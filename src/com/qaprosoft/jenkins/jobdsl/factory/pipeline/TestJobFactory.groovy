3da2f320d9df8aead6cf04349a4ffa90d7229a12
vdelendik <vadim.delendik@gmail.com>
2019-04-01 19:23:11 +0300

    added mobile support for jobs

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
@@ -97,6 +97,9 @@ public class TestJobFactory extends PipelineFactory {
 						break
 					case ~/^.*android.*$/:
 						choiceParam('devicePool', getDevices('ANDROID'), "Select the Device a Test will run against.  ALL - Any available device, PHONE - Any available phone, TABLET - Any tablet")
+                        if (getSuiteParameter("false", "jenkinsMobileWeb", currentSuite).toBoolean()) {
+                                choiceParam('deviceBrowser', "'chrome", "Select the mobile browser a Test will run against.")
+                        }
 						//TODO: Check private repositories for parameter use and fix possible problems using custom pipeline
 						//stringParam('build', '.*', ".* - use fresh build artifact from S3 or local storage\n2.2.0.3741.45 - exact version you would like to use")
 						booleanParam('recoveryMode', false, 'Restart application between retries')
@@ -108,6 +111,9 @@ public class TestJobFactory extends PipelineFactory {
 					case ~/^.*ios.*$/:
 						//TODO:  Need to adjust this for virtual as well.
 						choiceParam('devicePool', getDevices('iOS'), "Select the Device a Test will run against.  ALL - Any available device, PHONE - Any available phone, TABLET - Any tablet")
+                        if (getSuiteParameter("false", "jenkinsMobileWeb", currentSuite).toBoolean()) {
+                            choiceParam('deviceBrowser', "'safari", "Select the mobile browser a Test will run against.")
+                        }
 						//TODO: Check private repositories for parameter use and fix possible problems using custom pipeline
 						//stringParam('build', '.*', ".* - use fresh build artifact from S3 or local storage\n2.2.0.3741.45 - exact version you would like to use")
 						booleanParam('recoveryMode', false, 'Restart application between retries')
