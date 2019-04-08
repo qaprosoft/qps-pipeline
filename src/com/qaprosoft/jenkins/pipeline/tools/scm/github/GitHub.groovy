6f91a7263a5a586c0c7d315d126290e8799dc8f1
tsvrko <tsvrko@gmail.com>
2019-04-08 17:15:50 +0300

    fork logic refactoring

--- src/com/qaprosoft/jenkins/pipeline/tools/scm/github/GitHub.groovy
+++ src/com/qaprosoft/jenkins/pipeline/tools/scm/github/GitHub.groovy
@@ -4,6 +4,7 @@ import com.qaprosoft.jenkins.Logger
 import com.qaprosoft.jenkins.pipeline.tools.scm.ISCM
 import com.qaprosoft.jenkins.pipeline.Configuration
 import static com.qaprosoft.jenkins.pipeline.Executor.*
+import static com.qaprosoft.jenkins.Utils.*
 
 class GitHub implements ISCM {
 
@@ -12,11 +13,12 @@ class GitHub implements ISCM {
     private def gitSshUrl
 	private def credentialsId
 	private Logger logger
+    private def scmHost
 
 	public GitHub(context) {
 		this.context = context
         logger = new Logger(context)
-        def scmHost = Configuration.get(Configuration.Parameter.GITHUB_HOST)
+        scmHost = Configuration.get(Configuration.Parameter.GITHUB_HOST)
         if(scmHost.contains("github")){
             gitHtmlUrl = "https://\${GITHUB_HOST}/\${GITHUB_ORGANIZATION}/${Configuration.get("repo")}"
         } else if(scmHost.contains("bitbucket")) {
@@ -36,47 +38,30 @@ class GitHub implements ISCM {
 	public def clone(isShallow) {
 		context.stage('Checkout GitHub Repository') {
             logger.info("GitHub->clone")
-
-            def fork = parseFork(Configuration.get("fork"))
+            def fork = !isParamEmpty(Configuration.get("fork")) ? Configuration.get("fork").toBoolean() : false
             def branch = Configuration.get("branch")
             def repo = Configuration.get("repo")
             def userId = Configuration.get("BUILD_USER_ID")
-			logger.info("GITHUB_HOST: " + Configuration.get("GITHUB_HOST"))
-			logger.info("GITHUB_ORGANIZATION: " + Configuration.get("GITHUB_ORGANIZATION"))
-			logger.info("gitHtmlUrl: " + gitHtmlUrl)
-
             def gitUrl = Configuration.resolveVars(gitHtmlUrl)
-
+            def credentialsId = credentialsId
+            logger.info("GITHUB_HOST: " + Configuration.get("GITHUB_HOST"))
+            logger.info("GITHUB_ORGANIZATION: " + Configuration.get("GITHUB_ORGANIZATION"))
             logger.info("GIT_URL: " + gitUrl)
-            logger.debug("forked_repo: " + fork)
+            logger.info("CREDENTIALS_ID: " + credentialsId)
+            if(fork) {
+                def tokenName = 'token_' + "${userId}"
+                def userCredentials = getCredentials(tokenName)
+                if (userCredentials) {
+                    context.withCredentials([context.usernamePassword(credentialsId:tokenName, usernameVariable:'USERNAME', passwordVariable:'PASSWORD')]) {
+                        gitUrl = "https://${scmHost}/${context.env.USERNAME}/${repo}"
+                        credentialsId = tokenName
+                    }
+                } else {
+                    throw new RuntimeException("Unable to run from fork repo as ${tokenName} token is not registered on CI!")
+                }
 
-			Map scmVars
-			if (!fork) {
-                scmVars = context.checkout getCheckoutParams(gitUrl, branch, null, isShallow, true, '', credentialsId)
-			} else {
-				def token_name = 'token_' + "${userId}"
-                logger.debug("token_name: " + token_name)
-
-				//register into the Configuration.vars personal token of the current user
-				def token_value = context.env.getEnvironment().get(token_name)
-
-				//if token_value contains ":" as delimiter then redefine build_user_id using the 1st part
-				if (token_value != null && token_value.contains(":")) {
-					def (tempUserId, tempToken) = token_value.tokenize( ':' )
-					userId = tempUserId
-					token_value =  tempToken
-				}
-                if (token_value != null) {
-                    def GITHUB_HOST = Configuration.get(Configuration.Parameter.GITHUB_HOST)
-					gitUrl = "https://${token_value}@${GITHUB_HOST}/${userId}/${repo}"
-                    logger.info("fork repo url: ${gitUrl}")
-                    scmVars = context.checkout getCheckoutParams(gitUrl, branch, null, isShallow, true, '', credentialsId)
-				} else {
-					throw new RuntimeException("Unable to run from fork repo as ${token_name} token is not registered on CI!")
-				}
-			}
-
-            //TODO: remove git_branch after update ZafiraListener: https://github.com/qaprosoft/zafira/issues/760
+            }
+            Map scmVars = context.checkout getCheckoutParams(gitUrl, branch, null, isShallow, true, '', credentialsId)
             Configuration.set("scm_url", scmVars.GIT_URL)
             Configuration.set("scm_branch", branch)
             Configuration.set("scm_commit", scmVars.GIT_COMMIT)
@@ -145,13 +130,5 @@ class GitHub implements ISCM {
     
     public def setUrl(url) {
         gitHtmlUrl = url
-    }
-
-    private boolean parseFork(fork) {
-        boolean booleanFork = false
-        if (fork != null && !fork.isEmpty()) {
-            booleanFork = fork.toBoolean()
-        }
-        return booleanFork
     }
 }
