a8ba1bdc42827984ec202437b4544b49e83c12c3
tsvrko <tsvrko@gmail.com>
2019-04-17 16:37:36 +0300

    formatting

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
@@ -149,7 +149,6 @@ public class QARunner extends AbstractRunner {
         String QPS_PIPELINE_GIT_URL = Configuration.get(Configuration.Parameter.QPS_PIPELINE_GIT_URL)
         String QPS_PIPELINE_GIT_BRANCH = Configuration.get(Configuration.Parameter.QPS_PIPELINE_GIT_BRANCH)
         scmClient.clone(QPS_PIPELINE_GIT_URL, QPS_PIPELINE_GIT_BRANCH, "qps-pipeline")
-
     }
 
     protected void scan() {
