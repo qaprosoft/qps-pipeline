9c19d059b9c5f19c2d7c0789ada2926030bd5e21
tsvrko <tsvrko@gmail.com>
2019-04-30 16:14:58 +0300

    dump output

--- src/com/qaprosoft/jenkins/pipeline/integration/HttpClient.groovy
+++ src/com/qaprosoft/jenkins/pipeline/integration/HttpClient.groovy
@@ -30,7 +30,7 @@ abstract class HttpClient {
         /** Catches exceptions in every http call */
         try {
             response = context.httpRequest requestParams
-            logger.debug("response: " + response.dump())
+            logger.debug(response.dump())
         } catch (Exception e) {
             logger.error(printStackTrace(e))
         }
