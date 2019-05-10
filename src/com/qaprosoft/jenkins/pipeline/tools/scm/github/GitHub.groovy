f6faaba1d191b663abd65650482fb209364f38aa
rzubkouski <c-rzubkouski@lendingclub.com>
2019-05-10 13:34:34 +0300

    included git config command to mergeBranch logic

--- src/com/qaprosoft/jenkins/pipeline/tools/scm/github/GitHub.groovy
+++ src/com/qaprosoft/jenkins/pipeline/tools/scm/github/GitHub.groovy
@@ -137,6 +137,7 @@ class GitHub implements ISCM {
         context.withCredentials([context.usernamePassword(credentialsId: "${credentialsId}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
             logger.debug("USERNAME: ${context.env.USERNAME}")
             logger.debug("PASSWORD: ${context.env.PASSWORD}")
+            context.sh "git config --global ${context.env.USERNAME}"
             context.sh "git checkout -B ${source}"
             context.sh "git gc"
             // context.sh "git pull -v --progress origin"
