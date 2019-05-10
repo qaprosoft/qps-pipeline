820cec5494370578ef6176e9598ee25513b2c634
rzubkouski <c-rzubkouski@lendingclub.com>
2019-05-10 14:20:00 +0300

    returned git pull to mergeBranch method

--- src/com/qaprosoft/jenkins/pipeline/tools/scm/github/GitHub.groovy
+++ src/com/qaprosoft/jenkins/pipeline/tools/scm/github/GitHub.groovy
@@ -139,12 +139,12 @@ class GitHub implements ISCM {
             logger.debug("PASSWORD: ${context.env.PASSWORD}")
             context.sh "git checkout -B ${source}"
             context.sh "git gc"
-            // context.sh "git pull -v --progress origin"
+            context.sh "git pull https://${context.env.USERNAME}:${context.env.PASSWORD}@github.com/${Configuration.get("GITHUB_ORGANIZATION")}/${Configuration.get("repo")} -v --progress origin"
             if (isForce) {
-//                context.sh "git push --force --progress origin ${source}:${target}"
+//              context.sh "git push --force --progress origin ${source}:${target}"
                 context.sh("git push https://${context.env.USERNAME}:${context.env.PASSWORD}@github.com/${Configuration.get("GITHUB_ORGANIZATION")}/${Configuration.get("repo")} --force --progress origin ${source}:${target}")
             } else {
-//                context.sh "git push --progress origin ${source}:${target}"
+//              context.sh "git push --progress origin ${source}:${target}"
                 context.sh("git push https://${context.env.USERNAME}:${context.env.PASSWORD}@github.com/${Configuration.get("GITHUB_ORGANIZATION")}/${Configuration.get("repo")} --progress origin ${source}:${target}")
             }
         }
