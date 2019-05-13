6a9f18a571c8ad3557a3b38b34af35cf46c1fb10
tsvrko <tsvrko@gmail.com>
2019-05-13 14:33:50 +0300

    GIT_URL corrected

--- src/com/qaprosoft/jenkins/pipeline/tools/scm/github/GitHub.groovy
+++ src/com/qaprosoft/jenkins/pipeline/tools/scm/github/GitHub.groovy
@@ -138,7 +138,7 @@ class GitHub implements ISCM {
             logger.debug("USERNAME: ${context.env.USERNAME}")
             logger.debug("PASSWORD: ${context.env.PASSWORD}")
             context.sh "git config --global user.name ${context.env.USERNAME}"
-            context.sh "git remote add " + gitHtmlUrl
+            context.sh "git remote add " + Configuration.get("GIT_URL")
             context.sh "git checkout -B ${source}"
             context.sh "git gc"
             context.sh "git pull -v --progress origin"
