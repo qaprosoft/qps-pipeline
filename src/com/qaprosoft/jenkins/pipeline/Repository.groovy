9e9d60ff2b241d1717190b6ca4c451f66749bd77
tsvrko <tsvrko@gmail.com>
2019-06-13 17:19:28 +0300

    FLDR

--- src/com/qaprosoft/jenkins/pipeline/Repository.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Repository.groovy
@@ -98,6 +98,7 @@ class Repository {
             // Folder from which RegisterRepository job was started
             def registerRepositoryFolder = Paths.get(Configuration.get(Configuration.Parameter.JOB_NAME)).getName(0).toString()
 
+            logger.info("FLDR: " + registerRepositoryFolder)
             // If started from custom location, rootFolder is already created
             if (!"Management_Jobs".equals(registerRepositoryFolder)) {
                 rootFolder = registerRepositoryFolder
