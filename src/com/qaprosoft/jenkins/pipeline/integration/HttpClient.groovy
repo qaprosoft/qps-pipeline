27684ab335211929ba29ba9758d41af807dfa977
tsvrko <tsvrko@gmail.com>
2019-07-31 17:39:34 +0300

    test output

--- src/com/qaprosoft/jenkins/pipeline/integration/HttpClient.groovy
+++ src/com/qaprosoft/jenkins/pipeline/integration/HttpClient.groovy
@@ -19,7 +19,7 @@ abstract class HttpClient {
     /** Sends httpRequest using passed parameters */
     protected def sendRequestFormatted(requestParams) {
         def response = sendRequest(requestParams)
-        logger.info("response: " + response.dump())
+        logger.info("response: " + response)
         if (response){
             return getObjectResponse(response)
         }
