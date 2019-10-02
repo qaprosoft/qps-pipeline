cea74ecc82beede9cd3c89460f7aba47185c547e
ilya-varchenya <ilya.varchenya@gmail.com>
2019-10-02 15:31:03 +0300

    add TestRailJobFactory, QTestJobFactory

---
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestRailJobFactory.groovy
@@ -0,0 +1,29 @@
+package com.qaprosoft.jenkins.jobdsl.factory.pipeline
+
+@Grab('org.testng:testng:6.8.8')
+import groovy.transform.InheritConstructors
+
+@InheritConstructors
+public class TestRailJobFactory extends PipelineFactory {
+
+    public TestRailJobFactory(folder, pipelineScript, name, jobDesc) {
+        this.folder = folder
+        this.description = jobDesc
+        this.pipelineScript = pipelineScript
+        this.name = name
+    }
+
+    def create() {
+        logger.info("TestRailJobFactory->create")
+        def pipelineJob = super.create()
+        pipelineJob.with {
+            parameters {
+                configure stringParam('uuid', '', "uuid")
+                configure booleanParam('isRerun', false,'isRerun')
+                configure booleanParam('testrail_enabled','','testrail_enabled')
+            }
+        }
+        return pipelineJob
+    }
+
+}
