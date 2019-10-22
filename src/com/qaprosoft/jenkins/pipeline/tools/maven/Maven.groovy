f90fe565fe1a268970b35386d9bb8001b6eceef8
ilya-varchenya <ilya.varchenya@gmail.com>
2019-10-22 15:42:08 +0300

    delete log

--- src/com/qaprosoft/jenkins/pipeline/tools/maven/Maven.groovy
+++ src/com/qaprosoft/jenkins/pipeline/tools/maven/Maven.groovy
@@ -60,7 +60,6 @@ public class Maven {
         def arrayOfParmeters = goals.split()
         context.println("111111111111")
         for (parameter in arrayOfParmeters) {
-            context.println(parameter)
             if (parameter.contains("token")) {
                 def arrayOfString = parameter.split("=")
                 context.println(arrayOfParmeters)
