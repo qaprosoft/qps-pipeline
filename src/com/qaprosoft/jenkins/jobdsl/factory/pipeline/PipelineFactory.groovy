49b8ad740883d61b0913a7324a9a318ee95b9a3e
ilya-varchenya <ilya.varchenya@gmail.com>
2019-10-25 15:52:16 +0300

    more logs

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/PipelineFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/PipelineFactory.groovy
@@ -1,5 +1,6 @@
 package com.qaprosoft.jenkins.jobdsl.factory.pipeline
 
+import com.qaprosoft.jenkins.Logger
 import com.qaprosoft.jenkins.jobdsl.factory.job.JobFactory
 import groovy.transform.InheritConstructors
 
@@ -43,6 +44,7 @@ public class PipelineFactory extends JobFactory {
             }
 
             /** Git Stuff **/
+            Logger.info("MEMEMEMEMEME")
             context.println("??????????? MEW-MEW-MEW-MEW-MEW-MEW-MEW")
             if (context.fileExists('Jenkinsfile')) {
                 context.println("MEW-MEW-MEW-MEW-MEW-MEW-MEW")
