5a57879753452fce4d91f01262db2fe4e99241a1
ilya-varchenya <ilya.varchenya@gmail.com>
2019-11-15 11:27:46 +0300

    update TODO

--- src/com/qaprosoft/jenkins/pipeline/Repository.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Repository.groovy
@@ -88,7 +88,7 @@ class Repository {
 
         context.stage("Create Repository") {
             def buildNumber = Configuration.get(Configuration.Parameter.BUILD_NUMBER)
-            //TODO: check if repoFolder created correctly
+            //TODO: check if repoFolder is created correctly for not carina-demo repository
             def repoFolder = repo
 
             // Folder from which RegisterRepository job was started
