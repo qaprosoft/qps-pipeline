170630f2d052f022a245ba5bb1103f6f2f89dc42
ilya-varchenya <ilya.varchenya@gmail.com>
2019-11-20 10:43:55 +0300

    add build step configuration

--- src/com/qaprosoft/jenkins/jobdsl/factory/job/hook/PullRequestJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/job/hook/PullRequestJobFactory.groovy
@@ -87,37 +87,92 @@ public class PullRequestJobFactory extends FreestyleJobFactory {
                 }
             }
 
-            triggers {
-                githubPullRequest {
-                    admin('')
-                    userWhitelist('')
-                    orgWhitelist(organization)
-                    cron('H/5 * * * *')
-                    triggerPhrase('')
-                    onlyTriggerPhrase(false)
-                    useGitHubHooks(true)
-                    permitAll(true)
-                    autoCloseFailedPullRequests(false)
-                    displayBuildErrorsOnDownstreamBuilds(false)
-                    allowMembersOfWhitelistedOrgsAsAdmin(false)
-                    extensions {
-                        commitStatus {
-                            context('')
-                            triggeredStatus('starting deployment...')
-                            startedStatus('deploying...')
-                            addTestResults(true)
-                            statusUrl('http://mystatussite.com/prs')
-                            completedStatus('SUCCESS', 'All is well')
-                            completedStatus('FAILURE', 'Something went wrong. Investigate!')
-                            completedStatus('PENDING', 'still in progress...')
-                            completedStatus('ERROR', 'Something went really wrong. Investigate!')
-                        }
-                        buildStatus {
-                            completedStatus('SUCCESS', '')
-                            completedStatus('FAILURE', '')
-                            completedStatus('ERROR', '')
-                        }
-                    }
+//            triggers {
+//                githubPullRequest {
+//                    admin('')
+//                    userWhitelist('')
+//                    orgWhitelist(organization)
+//                    cron('H/5 * * * *')
+//                    triggerPhrase('')
+//                    onlyTriggerPhrase(false)
+//                    useGitHubHooks(true)
+//                    permitAll(true)
+//                    autoCloseFailedPullRequests(false)
+//                    displayBuildErrorsOnDownstreamBuilds(false)
+//                    allowMembersOfWhitelistedOrgsAsAdmin(false)
+//                    extensions {
+//                        commitStatus {
+//                            context('')
+//                            triggeredStatus('starting deployment...')
+//                            startedStatus('deploying...')
+//                            addTestResults(true)
+//                            statusUrl('http://mystatussite.com/prs')
+//                            completedStatus('SUCCESS', 'All is well')
+//                            completedStatus('FAILURE', 'Something went wrong. Investigate!')
+//                            completedStatus('PENDING', 'still in progress...')
+//                            completedStatus('ERROR', 'Something went really wrong. Investigate!')
+//                        }
+//                        buildStatus {
+//                            completedStatus('SUCCESS', '')
+//                            completedStatus('FAILURE', '')
+//                            completedStatus('ERROR', '')
+//                        }
+//                    }
+//                }
+//            }
+
+            steps {
+                remoteTrigger('onPullRequest-carina-demo-trigger', 'onPullRequest-carina-demo-trigger') {
+                    parameter('GITHUB_HOST', host)
+                    parameter('GITHUB_ORGANIZATION', organization)
+                    parameter('repo', repo)
+                    parameter('sha1', '')
+                    parameter('ghprbActualCommit', '')
+                    parameter('ghprbActualCommitAutho', '')
+                    parameter('ghprbActualCommitAuthorEmail', '')
+                    parameter('ghprbAuthorRepoGitUrl', '')
+                    parameter('ghprbTriggerAuthor', '')
+                    parameter('ghprbTriggerAuthorEmai', '')
+                    parameter('ghprbTriggerAuthorLogi', '')
+                    parameter('ghprbTriggerAuthorLoginMention', '')
+                    parameter('ghprbPullI', '')
+                    parameter('ghprbTargetBranch', '')
+                    parameter('ghprbSourceBranch', '')
+                    parameter('GIT_BRANCH', '')
+                    parameter('ghprbPullAuthorEmail', '')
+                    parameter('ghprbPullAuthorLogin', '')
+                    parameter('ghprbPullAuthorLoginMentio', '')
+                    parameter('ghprbPullDescription', '')
+                    parameter('ghprbPullTitle', '')
+                    parameter('ghprbPullLink', '')
+                    parameter('ghprbPullLongDescription', '')
+                    parameter('ghprbCommentBody', '')
+                    parameter('ghprbGhRepository', '')
+                    parameter('ghprbCredentialsId', '')
+
+                    parameter('gitHubAuthId', getGitHubAuthId(folder))
+                    parameter('adminlist', '')
+                    parameter('useGitHubHooks', true)
+                    parameter('triggerPhrase', '')
+                    parameter('autoCloseFailedPullRequests', false)
+                    parameter('skipBuildPhrase', '.*\\[skip\\W+ci\\].*')
+                    parameter('displayBuildErrorsOnDownstreamBuilds', false)
+                    parameter('cron', 'H/5 * * * *')
+                    parameter('whitelist', '')
+                    parameter('orgslist', organization)
+                    parameter('blackListLabels', '')
+                    parameter('whiteListLabels', '')
+                    parameter('allowMembersOfWhitelistedOrgsAsAdmin', false)
+                    parameter('permitAll', true)
+                    parameter('buildDescTemplate', '')
+                    parameter('blackListCommitAuthor', '')
+                    parameter('includedRegions', '')
+                    parameter('excludedRegions', '')
+                    parameter('onlyTriggerPhrase', false)
+                    parameter('commentFilePath', '')
+                    parameter('msgSuccess', '')
+                    parameter('msgFailure', '')
+                    parameter('commitStatusContext', '')
                 }
             }
         }
