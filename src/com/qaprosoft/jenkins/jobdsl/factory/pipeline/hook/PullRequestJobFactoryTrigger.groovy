54a76b1830bf66f2c3e68c15ac58b873159ffa5a
ilya-varchenya <ilya.varchenya@gmail.com>
2019-11-20 16:12:17 +0300

    hotfix

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/hook/PullRequestJobFactoryTrigger.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/hook/PullRequestJobFactoryTrigger.groovy
@@ -69,7 +69,7 @@ public class PullRequestJobFactoryTrigger extends PipelineFactory {
 //                        }
 //                    }
 //                }
-            }
+//            }
 
         }
         return pipelineJob
