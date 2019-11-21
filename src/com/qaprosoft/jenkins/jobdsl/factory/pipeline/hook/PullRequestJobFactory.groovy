7e090f3f49f9030b5d900460bab3cdb930f33028
ilya-varchenya <ilya.varchenya@gmail.com>
2019-11-21 12:36:46 +0300

    replace ghprbTrigger

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/hook/PullRequestJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/hook/PullRequestJobFactory.groovy
@@ -39,36 +39,36 @@ public class PullRequestJobFactory extends PipelineFactory {
             }
             properties {
                 githubProjectUrl(scmRepoUrl)
-                //TODO: test with removed "cron('H/5 * * * *')"
-                pipelineTriggers {
-                    triggers {
-                        ghprbTrigger {
-                            gitHubAuthId(getGitHubAuthId(folder))
-                            adminlist('')
-                            useGitHubHooks(true)
-                            triggerPhrase('')
-                            autoCloseFailedPullRequests(false)
-                            skipBuildPhrase('.*\\[skip\\W+ci\\].*')
-                            displayBuildErrorsOnDownstreamBuilds(false)
-                            cron('H/5 * * * *')
-                            whitelist('')
-                            orgslist(organization)
-                            blackListLabels('')
-                            whiteListLabels('')
-                            allowMembersOfWhitelistedOrgsAsAdmin(false)
-                            permitAll(true)
-                            buildDescTemplate('')
-                            blackListCommitAuthor('')
-                            includedRegions('')
-                            excludedRegions('')
-                            onlyTriggerPhrase(false)
-                            commentFilePath('')
-                            msgSuccess('')
-                            msgFailure('')
-                            commitStatusContext('')
-                        }
-                    }
-                }
+//                //TODO: test with removed "cron('H/5 * * * *')"
+//                pipelineTriggers {
+//                    triggers {
+//                        ghprbTrigger {
+//                            gitHubAuthId(getGitHubAuthId(folder))
+//                            adminlist('')
+//                            useGitHubHooks(true)
+//                            triggerPhrase('')
+//                            autoCloseFailedPullRequests(false)
+//                            skipBuildPhrase('.*\\[skip\\W+ci\\].*')
+//                            displayBuildErrorsOnDownstreamBuilds(false)
+//                            cron('H/5 * * * *')
+//                            whitelist('')
+//                            orgslist(organization)
+//                            blackListLabels('')
+//                            whiteListLabels('')
+//                            allowMembersOfWhitelistedOrgsAsAdmin(false)
+//                            permitAll(true)
+//                            buildDescTemplate('')
+//                            blackListCommitAuthor('')
+//                            includedRegions('')
+//                            excludedRegions('')
+//                            onlyTriggerPhrase(false)
+//                            commentFilePath('')
+//                            msgSuccess('')
+//                            msgFailure('')
+//                            commitStatusContext('')
+//                        }
+//                    }
+//                }
             }
 
         }
