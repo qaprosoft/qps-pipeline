d05a2c5eab630d289aebb2f3607a9ea7df9d9a24
ilya-varchenya <ilya.varchenya@gmail.com>
2019-11-22 14:13:44 +0300

    add failure status to checking construction

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
@@ -113,6 +113,7 @@ public class QARunner extends AbstractRunner {
                 } catch (Exception e) {
                     logger.error("Scan failed.\n" + e.getMessage())
                     getJenkinsJobsScanResult(null)
+                    currentBuild.result = BuildResult.FAILURE
                 }
                 clean()
             }
