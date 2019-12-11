a9413ddaa13ca273a666f9e1223358b9f7262ced
vdelendik <vadim.delendik@gmail.com>
2019-12-12 02:07:15 +0300

    removed useless import

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
@@ -1,7 +1,5 @@
 package com.qaprosoft.jenkins.jobdsl.factory.pipeline
 
-import com.qaprosoft.jenkins.pipeline.Configuration
-
 @Grab('org.testng:testng:6.8.8')
 
 import static com.qaprosoft.jenkins.Utils.*
