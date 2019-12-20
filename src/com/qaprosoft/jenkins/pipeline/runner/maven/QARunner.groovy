e1a8ede56caebec7042fa88c903d7fb3ad27d463
ilya-varchenya <ilya.varchenya@gmail.com>
2019-12-20 15:28:08 +0300

    some logs

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
@@ -853,10 +853,10 @@ public class QARunner extends AbstractRunner {
         ]
 
         def goals = ''
-        for (def i = 0; i < params.size(); i++) {
-            if (!(params[i] in necessaryMavenParams)) {
-                context.println("MEW_MEW_MEW" + params[i])
-                goals = goals + " -D${params[i].getKey()}=\'${params[i].getValue()}\'"
+        for (params p : params.values()) {
+            if (!(p in necessaryMavenParams)) {
+                context.println("MEW_MEW_MEW" + p)
+                // goals = goals + " -D${params[i].getKey()}=\'${params[i].getValue()}\'"
             }
         }
     }
