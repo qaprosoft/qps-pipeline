5e9c53a314e502580ec591d7bde19b375357ece0
vdelendik <vadim.delendik@gmail.com>
2020-01-18 16:32:33 +0300

    comment some lines

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
@@ -379,8 +379,8 @@ public class QARunner extends AbstractRunner {
 			xmlFile.setLoadClasses(false)
 			logger.info(xmlFile.dump())
 			logger.info(xmlFile.TESTNG_DTD_URL)
-			xmlFile.TESTNG_DTD_URL="https://testng.org/testng-1.0.dtd"
-			logger.info(xmlFile.TESTNG_DTD_URL)
+			//xmlFile.TESTNG_DTD_URL="https://testng.org/testng-1.0.dtd"
+			//logger.info(xmlFile.TESTNG_DTD_URL)
 			List<XmlSuite> suiteXml = xmlFile.parseToList()
 			XmlSuite currentSuite2 = suiteXml.get(0)
 			logger.error(currentSuite2.toXml())
