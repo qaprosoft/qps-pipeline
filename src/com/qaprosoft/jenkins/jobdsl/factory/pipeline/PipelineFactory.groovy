d6f800dbb84bd6f81295ede3fbc9c7573d1a7489
ilya-varchenya <ilya.varchenya@gmail.com>
2020-03-02 12:01:37 +0300

    hotfix

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/PipelineFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/PipelineFactory.groovy
@@ -114,7 +114,7 @@ public class PipelineFactory extends JobFactory {
                     'bla-bla-bla'
                     choice
                     flase
-                    WhenToAdd 'Triggered'
+                    AddEditedChoiceListProvider.WhenToAdd 'Triggered'
 
                     //whenToAdd 'Triggered'
                     //name globalName
