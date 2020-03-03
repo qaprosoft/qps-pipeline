9cdc236a605b3c026756ca4ee62392aa6620f7d2
ilya-varchenya <ilya.varchenya@gmail.com>
2020-03-03 14:52:41 +0300

    fix import error

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/PipelineFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/PipelineFactory.groovy
@@ -2,7 +2,7 @@ package com.qaprosoft.jenkins.jobdsl.factory.pipeline
 
 import com.qaprosoft.jenkins.jobdsl.factory.job.JobFactory
 import groovy.transform.InheritConstructors
-import com.qaprosoft.jenkins.Utils.isParamEmpty
+import com.qaprosoft.jenkins.Utils
 import org.apache.tools.ant.types.resources.selectors.None
 
 @InheritConstructors
@@ -57,7 +57,7 @@ public class PipelineFactory extends JobFactory {
 
     protected List<String> getEnvironments(currentSuite) {
         def env = currentSuite.getParameter("jenkinsEnvironments")
-        if (isParamEmpty(env)) {
+        if (Utils.isParamEmpty(env)) {
             env += "DEMO\nSTAG"
         }
         return env
