93360c791affbdab15c6393d9641ebb88c87c863
ilya-varchenya <ilya.varchenya@gmail.com>
2020-03-12 13:49:18 +0300

    organization -> repoFolder

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
@@ -345,7 +345,7 @@ public class QARunner extends AbstractRunner {
                     break
             }
 
-            def nameOrgRepoScheduling = (organization + "-" + repo + "-scheduling")
+            def nameOrgRepoScheduling = (repoFolder + "-" + repo + "-scheduling")
             def orgRepoScheduling = true
             if (!isParamEmpty(configuration.getGlobalProperty(nameOrgRepoScheduling)) && configuration.getGlobalProperty(nameOrgRepoScheduling).toBoolean() == false) {
                 orgRepoScheduling = false
