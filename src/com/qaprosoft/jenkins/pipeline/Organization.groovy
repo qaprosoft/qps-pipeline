47371ab4dca4efcef0c21b1bf7cbfd4c5e2ac8a6
ilya-varchenya <ilya.varchenya@gmail.com>
2020-04-04 14:04:51 +0300

    delete logger

--- src/com/qaprosoft/jenkins/pipeline/Organization.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Organization.groovy
@@ -310,12 +310,10 @@ class Organization {
 	public static void registerZafiraCredentials(orgFolderName, zafiraServiceURL, zafiraRefreshToken){
         if (isParamEmpty(orgFolderName)) {
             orgFolderName = 'qaprosoft'
-            logger.info("1111111111 " + orgFolderName)
         }
 
         if (!isParamEmpty(Configuration.get('SCM_ORGANIZATION')) || Configuration.get('SCM_ORGANIZATION').toBoolean() == true) {
             orgFolderName = Configuration.get('SCM_ORGANIZATION')
-            logger.info("1111111111 " + orgFolderName)
         }
 
 		if (isParamEmpty(orgFolderName) || isParamEmpty(zafiraServiceURL) || isParamEmpty(zafiraRefreshToken)){
