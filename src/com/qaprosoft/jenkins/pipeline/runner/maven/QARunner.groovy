3d655d2737a78f19ee78dafea1f697c30063c45e
vdelendik <vadim.delendik@gmail.com>
2020-04-11 15:08:08 +0300

    added message to log

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
@@ -816,6 +816,7 @@ public class QARunner extends AbstractRunner {
 	
 	protected void setSeleniumUrl() {
 		def seleniumUrl = Configuration.get(Configuration.Parameter.SELENIUM_URL)
+		logger.info("seleniumUrl: ${seleniumUrl}")
 		if (!isParamEmpty(seleniumUrl)) {
 			// do not override from creds as looks like external service or user overrided this value
 			return
