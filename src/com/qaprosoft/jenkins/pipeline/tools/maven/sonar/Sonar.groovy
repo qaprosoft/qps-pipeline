4c7b021391270019673a060da0e0945f07ba2eed
sandinosanchez <sandino320@gmail.com>
2020-04-30 16:11:47 -0300

    sonar.java.binaries change #2

--- src/com/qaprosoft/jenkins/pipeline/tools/maven/sonar/Sonar.groovy
+++ src/com/qaprosoft/jenkins/pipeline/tools/maven/sonar/Sonar.groovy
@@ -31,20 +31,20 @@ public class Sonar {
         context.stage('Sonar Scanner') {
             context.withSonarQubeEnv(sonarQubeEnv) {
                 def goals = "-U -f ${pomFile} \
-					clean compile test-compile package sonar:sonar -DskipTests=true \
-					-Dsonar.github.endpoint=${Configuration.resolveVars("${Configuration.get(Configuration.Parameter.GITHUB_API_URL)}")} \
-					-Dsonar.github.pullRequest=${Configuration.get("ghprbPullId")} \
-					-Dsonar.github.repository=${Configuration.get("ghprbGhRepository")} \
-					-Dsonar.projectKey=${Configuration.get("repo")} \
-					-Dsonar.projectName=${Configuration.get("repo")} \
-					-Dsonar.projectVersion=1.${Configuration.get(Configuration.Parameter.BUILD_NUMBER)} \
-					-Dsonar.github.oauth=${Configuration.get(Configuration.Parameter.GITHUB_OAUTH_TOKEN)} \
-					-Dsonar.sources=. \
-					-Dsonar.tests=. \
-					-Dsonar.inclusions=**/src/main/java/** \
-					-Dsonar.test.inclusions=**/src/test/java/** \
-					-Dsonar.java.source=1.8"
-                /** **/
+        					clean compile test-compile package sonar:sonar -DskipTests=true \
+        					-Dsonar.github.endpoint=${Configuration.resolveVars("${Configuration.get(Configuration.Parameter.GITHUB_API_URL)}")} \
+        					-Dsonar.github.pullRequest=${Configuration.get("ghprbPullId")} \
+        					-Dsonar.github.repository=${Configuration.get("ghprbGhRepository")} \
+        					-Dsonar.projectKey=${Configuration.get("repo")} \
+        					-Dsonar.projectName=${Configuration.get("repo")} \
+        					-Dsonar.projectVersion=1.${Configuration.get(Configuration.Parameter.BUILD_NUMBER)} \
+        					-Dsonar.github.oauth=${Configuration.get(Configuration.Parameter.GITHUB_OAUTH_TOKEN)} \
+        					-Dsonar.sources=. \
+        					-Dsonar.tests=. \
+        					-Dsonar.inclusions=**/src/main/java/** \
+        					-Dsonar.test.inclusions=**/src/test/java/** \
+        					-Dsonar.java.source=1.8"
+                        /** **/
                 if (mavenSettingsConfig != null) {
                     executeMavenGoals(goals, mavenSettingsConfig)
                 } else {
@@ -72,7 +72,6 @@ public class Sonar {
                 return
             }
 
-
             //download combined integration testing coverage report: jacoco-it.exec
             def jacocoEnable = Configuration.get(Configuration.Parameter.JACOCO_ENABLE).toBoolean()
             def jacocoItExec = 'jacoco-it.exec'
@@ -85,29 +84,27 @@ public class Sonar {
                 }
             }
 
-
             context.env.sonarHome = context.tool name: 'sonar-ci-scanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
-            context.withSonarQubeEnv('sonar-ci')
+            context.withSonarQubeEnv('sonar-ci') {
                 //TODO: [VD] find a way for easier env getter. how about making Configuration syncable with current env as well...
                 def sonarHome = context.env.getEnvironment().get("sonarHome")
                 def BUILD_NUMBER = Configuration.get("BUILD_NUMBER")
 
                 // execute sonar scanner
                 context.sh "${sonarHome}/bin/sonar-scanner \
-					-Dsonar.projectBaseDir=${projectBaseDir} \
-					-Dsonar.projectName=${projectName} \
-					-Dsonar.projectKey=${projectKey} \
-					-Dsonar.projectVersion=1.${BUILD_NUMBER} \
-					-Dsonar.java.source=1.8 \
-					-Dsonar.sources='src/main' \
-					-Dsonar.tests='src/test' \
-					-Dsonar.java.binaries=target/classes \
-					-Dsonar.junit.reportsPath='target/surefire-reports' \
-					-Dsonar.modules=${modules} \
-					-Dsonar.jacoco.ReportPath='target/jacoco.exec' \
-					-Dsonar.jacoco.itReportPath='/tmp/${jacocoItExec}'"
+        					-Dsonar.projectBaseDir=${projectBaseDir} \
+        					-Dsonar.projectName=${projectName} \
+        					-Dsonar.projectKey=${projectKey} \
+        					-Dsonar.projectVersion=1.${BUILD_NUMBER} \
+        					-Dsonar.java.source=1.8 \
+        					-Dsonar.sources='src/main' \
+        					-Dsonar.tests='src/test' \
+        					-Dsonar.java.binaries=target/classes \
+        					-Dsonar.junit.reportsPath='target/surefire-reports' \
+        					-Dsonar.modules=${modules} \
+        					-Dsonar.jacoco.ReportPath='target/jacoco.exec' \
+        					-Dsonar.jacoco.itReportPath='/tmp/${jacocoItExec}'"
             }
         }
     }
-
 }
