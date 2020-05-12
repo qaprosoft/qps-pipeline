bb5773806db9c1104906e3338ca2c2e6abfadcca
sandinosanchez <sandino320@gmail.com>
2020-05-12 13:16:26 -0300

    small change

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
@@ -129,7 +129,7 @@ public class QARunner extends Runner {
 				this.currentBuild.result = BuildResult.FAILURE
 			}
 			clean()
-      //            }
+			//            }
 		}
         context.node("master") {
             jenkinsFileScan()
