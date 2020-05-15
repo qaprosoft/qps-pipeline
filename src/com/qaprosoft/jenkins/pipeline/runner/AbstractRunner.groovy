05f0ca082974fae5966d1fdfa07950754bea08f9
vdelendik <vadim.delendik@gmail.com>
2020-05-16 02:49:27 +0300

    trying to set org statically at all

--- src/com/qaprosoft/jenkins/pipeline/runner/AbstractRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/AbstractRunner.groovy
@@ -3,6 +3,7 @@ package com.qaprosoft.jenkins.pipeline.runner
 import com.qaprosoft.jenkins.BaseObject
 
 public abstract class AbstractRunner extends BaseObject {
+	private static final String organization = getOrganization()
 	
     public AbstractRunner(context) {
 		super(context)
@@ -27,5 +28,32 @@ public abstract class AbstractRunner extends BaseObject {
             }
         }
     }
+	
+	
+	@NonCPS
+	private void getOrganization() {
+		String jobName = Configuration.get(Configuration.Parameter.JOB_NAME)
+		int nameCount = Paths.get(jobName).getNameCount()
+
+		def orgFolderName = ""
+		if (nameCount == 1 && (jobName.contains("qtest-updater") || jobName.contains("testrail-updater"))) {
+			// testrail-updater - i.e. stage
+			orgFolderName = ""
+		} else if (nameCount == 2 && (jobName.contains("qtest-updater") || jobName.contains("testrail-updater"))) {
+			// stage/testrail-updater - i.e. stage
+			orgFolderName = Paths.get(jobName).getName(0).toString()
+		} else if (nameCount == 2) {
+			// carina-demo/API_Demo_Test - i.e. empty orgFolderName
+			orgFolderName = ""
+		} else if (nameCount == 3) { //TODO: need to test use-case with views!
+			// qaprosoft/carina-demo/API_Demo_Test - i.e. orgFolderName=qaprosoft
+			orgFolderName = Paths.get(jobName).getName(0).toString()
+		} else {
+			throw new RuntimeException("Invalid job organization structure: '${jobName}'!" )
+		}
+		
+		Configuration.set(Configuration.Parameter.ORG_FOLDER, orgFolderName)
+		
+	}
 
 }
