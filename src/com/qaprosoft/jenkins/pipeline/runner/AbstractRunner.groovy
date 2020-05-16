a9ac3e921dd07ddd548c441ec3b12902ce3ff23b
vdelendik <vadim.delendik@gmail.com>
2020-05-16 16:47:57 +0300

    format AbstractRunner completely

--- src/com/qaprosoft/jenkins/pipeline/runner/AbstractRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/AbstractRunner.groovy
@@ -7,12 +7,12 @@ import static com.qaprosoft.jenkins.Utils.*
 import static com.qaprosoft.jenkins.pipeline.Executor.*
 
 public abstract class AbstractRunner extends BaseObject {
-	// organization folder name of the current job/runner
-	protected String organization = ""
-	
+    // organization folder name of the current job/runner
+    protected String organization = ""
+
     public AbstractRunner(context) {
-		super(context)
-		initOrganization()
+        super(context)
+        initOrganization()
     }
 
     //Methods
@@ -23,63 +23,63 @@ public abstract class AbstractRunner extends BaseObject {
     abstract public void onPullRequest()
 
     protected void jenkinsFileScan() {
-		if (!context.fileExists('Jenkinsfile')) {
-			// do nothing
-			return
-		}
-		
+        if (!context.fileExists('Jenkinsfile')) {
+            // do nothing
+            return
+        }
+
         context.stage('Jenkinsfile Stage') {
-            context.script { 
+            context.script {
                 context.jobDsl targets: 'Jenkinsfile'
             }
         }
     }
-	
-	protected def getOrgFolder() {
-		return this.organization
-	}	
-	
-	@NonCPS
-	protected void initOrganization() {
-		String jobName = context.env.getEnvironment().get("JOB_NAME") //Configuration.get(Configuration.Parameter.JOB_NAME)
-		int nameCount = Paths.get(jobName).getNameCount()
 
-		def orgFolderName = ""
-		if (nameCount == 1 && (jobName.contains("qtest-updater") || jobName.contains("testrail-updater"))) {
-			// testrail-updater - i.e. stage
-			orgFolderName = ""
-		} else if (nameCount == 2 && (jobName.contains("qtest-updater") || jobName.contains("testrail-updater"))) {
-			// stage/testrail-updater - i.e. stage
-			orgFolderName = Paths.get(jobName).getName(0).toString()
-		} else if (nameCount == 2) {
-			// carina-demo/API_Demo_Test - i.e. empty orgFolderName
-			orgFolderName = ""
-		} else if (nameCount == 3) { //TODO: need to test use-case with views!
-			// qaprosoft/carina-demo/API_Demo_Test - i.e. orgFolderName=qaprosoft
-			orgFolderName = Paths.get(jobName).getName(0).toString()
-		} else {
-			throw new RuntimeException("Invalid job organization structure: '${jobName}'!" )
-		}
-		
-		this.organization = orgFolderName
-	}
-	
-	protected def getToken(tokenName) {
-		def tokenValue = ""
+    protected def getOrgFolder() {
+        return this.organization
+    }
 
-		if (!isParamEmpty(this.organization)) {
-			tokenName = "${this.organization}" + "-" + tokenName
-		}
-		
-		if (getCredentials(tokenName)){
-			context.withCredentials([context.usernamePassword(credentialsId:tokenName, usernameVariable:'KEY', passwordVariable:'VALUE')]) {
-				tokenValue=context.env.VALUE
-			}
-		}
-		logger.debug("tokenName: ${tokenName}; tokenValue: ${tokenValue}")
-		return tokenValue
-	}
-    
+    @NonCPS
+    protected void initOrganization() {
+        String jobName = context.env.getEnvironment().get("JOB_NAME") //Configuration.get(Configuration.Parameter.JOB_NAME)
+        int nameCount = Paths.get(jobName).getNameCount()
+
+        def orgFolderName = ""
+        if (nameCount == 1 && (jobName.contains("qtest-updater") || jobName.contains("testrail-updater"))) {
+            // testrail-updater - i.e. stage
+            orgFolderName = ""
+        } else if (nameCount == 2 && (jobName.contains("qtest-updater") || jobName.contains("testrail-updater"))) {
+            // stage/testrail-updater - i.e. stage
+            orgFolderName = Paths.get(jobName).getName(0).toString()
+        } else if (nameCount == 2) {
+            // carina-demo/API_Demo_Test - i.e. empty orgFolderName
+            orgFolderName = ""
+        } else if (nameCount == 3) { //TODO: need to test use-case with views!
+            // qaprosoft/carina-demo/API_Demo_Test - i.e. orgFolderName=qaprosoft
+            orgFolderName = Paths.get(jobName).getName(0).toString()
+        } else {
+            throw new RuntimeException("Invalid job organization structure: '${jobName}'!" )
+        }
+
+        this.organization = orgFolderName
+    }
+
+    protected def getToken(tokenName) {
+        def tokenValue = ""
+
+        if (!isParamEmpty(this.organization)) {
+            tokenName = "${this.organization}" + "-" + tokenName
+        }
+
+        if (getCredentials(tokenName)){
+            context.withCredentials([context.usernamePassword(credentialsId:tokenName, usernameVariable:'KEY', passwordVariable:'VALUE')]) {
+                tokenValue=context.env.VALUE
+            }
+        }
+        logger.debug("tokenName: ${tokenName}; tokenValue: ${tokenValue}")
+        return tokenValue
+    }
+
     protected def getUserCreds(tokenName) {
         def name = ""
         def password = ""
