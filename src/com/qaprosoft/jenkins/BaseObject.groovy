7797c67b768f7233adb23b817b03d263be4038ec
vdelendik <vadim.delendik@gmail.com>
2020-05-16 18:13:00 +0300

    formatting baseObject using spaces

--- src/com/qaprosoft/jenkins/BaseObject.groovy
+++ src/com/qaprosoft/jenkins/BaseObject.groovy
@@ -11,32 +11,32 @@ import com.qaprosoft.jenkins.pipeline.tools.scm.github.ssh.SshGitHub
  * BaseObject to operate with pipeline context, loggers and runners
  */
 public abstract class BaseObject {
-	protected def context
-	protected Logger logger
-	protected FactoryRunner factoryRunner // object to be able to start JobDSL anytime we need
-	
-	protected ISCM scmClient
-	protected ISCM scmSshClient
-	
-	protected def currentBuild
-	
-	//this is very important line which should be declared only as a class member!
-	protected Configuration configuration = new Configuration(context)
+    protected def context
+    protected Logger logger
+    protected FactoryRunner factoryRunner // object to be able to start JobDSL anytime we need
 
-	public BaseObject(context) {
-		this.context = context
-		this.logger = new Logger(context)
-		this.scmClient = new GitHub(context)
-		this.scmSshClient = new SshGitHub(context)
-		
-		this.factoryRunner = new FactoryRunner(context)
-		
-		currentBuild = context.currentBuild
-	}
+    protected ISCM scmClient
+    protected ISCM scmSshClient
 
-	//TODO: [VD] think about moving into AbstractRunner!
-	protected void setDslClasspath(additionalClasspath) {
-		factoryRunner.setDslClasspath(additionalClasspath)
-	}
-	
+    protected def currentBuild
+
+    //this is very important line which should be declared only as a class member!
+    protected Configuration configuration = new Configuration(context)
+
+    public BaseObject(context) {
+        this.context = context
+        this.logger = new Logger(context)
+        this.scmClient = new GitHub(context)
+        this.scmSshClient = new SshGitHub(context)
+
+        this.factoryRunner = new FactoryRunner(context)
+
+        currentBuild = context.currentBuild
+    }
+
+    //TODO: [VD] think about moving into AbstractRunner!
+    protected void setDslClasspath(additionalClasspath) {
+        factoryRunner.setDslClasspath(additionalClasspath)
+    }
+
 }
