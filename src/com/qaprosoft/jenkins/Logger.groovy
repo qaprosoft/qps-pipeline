069bc351866a996f2073c33fe30140ea02928775
vdelendik <vadim.delendik@gmail.com>
2020-05-22 03:30:06 +0300

    removed ansi colored part at all

--- src/com/qaprosoft/jenkins/Logger.groovy
+++ src/com/qaprosoft/jenkins/Logger.groovy
@@ -1,15 +1,6 @@
 package com.qaprosoft.jenkins
 
 class Logger {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
 
     public static enum LogLevel {
         DEBUG(1),
@@ -49,10 +40,7 @@ class Logger {
 
     private def log(logLevel, message, ansiCode){
         if (logLevel.value >= pipelineLogLevel.value){
-            context.ansiColor('xterm') {
-                context.printf "${ansiCode}[${logLevel}] ${message}${ANSI_RESET}"
-            }
+            context.printf "${ansiCode}[${logLevel}] ${message}${ANSI_RESET}"
         }
     }
-
 }
