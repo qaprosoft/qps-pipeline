5068ffa42ff43ebcbc6dd4e14fde282930a40d29
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-05 13:39:06 +0300

    some fixes

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/Runner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/Runner.groovy
@@ -33,8 +33,8 @@ public class Runner extends AbstractRunner {
     }
 
 	public void onPullRequest() {
-        logger.info("Runner->onPullRequest"),
-        sonar.setToken(getToken(Configuration.CREDS_SONAR_GITHUB_OAUTH_TOKEN)),
+        logger.info("Runner->onPullRequest")
+        sonar.setToken(getToken(Configuration.CREDS_SONAR_GITHUB_OAUTH_TOKEN))
         Maven([
             node:"master",
             methods:[
