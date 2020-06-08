3667520482851c10e65f957f4852373b9dc21489
vdelendik <vadim.delendik@gmail.com>
2020-06-09 01:51:12 +0300

    added new file

---
+++ vars/getPomFiles.groovy
@@ -0,0 +1,25 @@
+    //TODO: reuse global logInfo and logDebug
+def call() {
+    def pomFiles = []
+
+    def files = findFiles(glob: "**/pom.xml")
+    if (files.length > 0) {
+        println("Number of pom.xml files to analyze: " + files.length)
+
+        int curLevel = 5 //do not analyze projects where highest pom.xml level is lower or equal 5
+        for (pomFile in files) {
+            def path = pomFile.path
+            int level = path.count("/")
+            // logger.debug("file: " + path + "; level: " + level + "; curLevel: " + curLevel)
+            if (level < curLevel) {
+                curLevel = level
+                pomFiles.clear()
+                pomFiles.add(pomFile.path)
+            } else if (level == curLevel) {
+                pomFiles.add(pomFile.path)
+            }
+        }
+        println("PROJECT POMS: " + pomFiles)
+    }
+    return pomFiles
+}
