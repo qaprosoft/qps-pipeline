13cd96e7f04269dcf6631e462ce90a975b0e99a6
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-16 12:25:20 +0300

    setBuildNumberTemplate in QARunner

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
@@ -4,9 +4,11 @@ public class QARunner extends TestNG {
 
     public QARunner(context) {
         super(context)
+        setBuildNameTemplate("${BUILD_NUMBER}${suite}${branch}${env}${browser}${browserVersion}${locale}${language}")
     }
 
     public QARunner(context, jobType) {
         super(context, jobType)
+        setBuildNameTemplate("${BUILD_NUMBER}${suite}${branch}${env}${browser}${browserVersion}${locale}${language}")
     }
 }
