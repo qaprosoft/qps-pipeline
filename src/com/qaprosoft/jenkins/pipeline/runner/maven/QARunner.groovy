01950ff187d982b6bf0a1ec7c8216ad9b333613b
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-16 15:19:14 +0300

    setBuildName in QARunner

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
@@ -4,9 +4,11 @@ public class QARunner extends TestNG {
 
     public QARunner(context) {
         super(context)
+        setBuildName("BUILD_NUMBER|suite|branch|env|browser|browserVersion|locale|language")
     }
 
     public QARunner(context, jobType) {
         super(context, jobType)
+        setBuildName("BUILD_NUMBER|suite|branch|env|browser|browserVersion|locale|language")
     }
 }
