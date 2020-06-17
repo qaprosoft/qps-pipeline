0c09b1da80647bf0c7feafa37107acfff7de3bc8
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-17 12:18:40 +0300

    get BUILD_NUMBER from configuration file in AbstractRunner

--- src/com/qaprosoft/jenkins/pipeline/runner/AbstractRunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/AbstractRunner.groovy
@@ -10,7 +10,7 @@ import static com.qaprosoft.jenkins.pipeline.Executor.*
 public abstract class AbstractRunner extends BaseObject {
     // organization folder name of the current job/runner
     protected String organization = ""
-    protected String displayNameTemplate = "#${BUILD_NUMBER}|${branch}"
+    protected String displayNameTemplate = "#${Configuration.Parameter.BUILD_NUMBER}|${branch}"
     protected final String DISPLAY_NAME_SEPARATOR = "|"
 
     public AbstractRunner(context) {
