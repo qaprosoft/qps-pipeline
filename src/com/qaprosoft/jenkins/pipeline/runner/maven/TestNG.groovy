471de5daba452fbb8132f76f2d7e26ab66d1ead5
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-22 11:43:32 +0300

    set value by parameter

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/TestNG.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/TestNG.groovy
@@ -825,8 +825,7 @@ public class TestNG extends Runner {
         addCapability("ci_build_cause", getBuildCause((Configuration.get(Configuration.Parameter.JOB_NAME)), currentBuild))
         addCapability("suite", suiteName)
         addCapabilityIfPresent("rerun_failures", "zafira_rerun_failures")
-        Configuration.set(Configuration.Parameter.ENABLE_VIDEO, getSuiteParameter("true", "enableVideo", currentSuite).toBoolean())
-        addOptionalCapability("enableVideo", "Video recording was enabled.", "capabilities.enableVideo", "true")
+        addOptionalCapability("enableVideo", "Video recording was enabled.", "capabilities.enableVideo", getSuiteParameter("true", "enableVideo", currentSuite).toBoolean())
         // [VD] getting debug host works only on specific nodes which are detecetd by chooseNode.
         // on this stage this method is not fucntion properly!
         //TODO: move 8000 port into the global var
