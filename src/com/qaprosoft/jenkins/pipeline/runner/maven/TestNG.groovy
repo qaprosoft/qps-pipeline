2136345121d01314e1165386473c16557032300d
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-22 15:15:48 +0300

    logger #6

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/TestNG.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/TestNG.groovy
@@ -920,7 +920,7 @@ public class TestNG extends Runner {
         logger.info("Configuration.get(parameterName): " + Configuration.get(parameterName))
         logger.info("333333")
         logger.info("Configuration.get(parameterName)?.toBoolean(): " + Configuration.get(parameterName)?.toBoolean())
-        if (Configuration.get(parameterName)?.toBoolean()) {
+        if (Configuration.get(parameterName)?.toBoolean() && Configuration.get(parameterName) != null) {
             logger.info(message)
             Configuration.set(capabilityName, capabilityValue)
         }
