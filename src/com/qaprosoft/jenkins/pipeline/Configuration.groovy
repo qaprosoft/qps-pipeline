332009ba8bb6b9b1f9bd1bd7a96a991763e0a737
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-24 09:49:03 +0300

    delete QTEST_ENABLE

--- src/com/qaprosoft/jenkins/pipeline/Configuration.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Configuration.groovy
@@ -114,7 +114,6 @@ public class Configuration {
 
         QTEST_SERVICE_URL("QTEST_SERVICE_URL", ""), // "https://<CHANGE_ME>/api/v3/"
         QTEST_ACCESS_TOKEN("QTEST_ACCESS_TOKEN", "", true),
-        QTEST_ENABLE("qtest_enabled", "false"),
 
         //TODO:  reorganize logic to operate with tokens without Configuration enum!
         SONAR_GITHUB_OAUTH_TOKEN("SONAR_GITHUB_OAUTH_TOKEN", "", true)
