9894bb5e2c6659cd15b824049aa2fd8f0e2eadf0
sandinosanchez <sandino320@gmail.com>
2020-06-29 15:17:04 -0300

    small fix

--- src/com/qaprosoft/jenkins/pipeline/tools/maven/sonar/Sonar.groovy
+++ src/com/qaprosoft/jenkins/pipeline/tools/maven/sonar/Sonar.groovy
@@ -26,13 +26,14 @@ class Sonar extends BaseObject {
 
                 def jacocoEnable = configuration.get(Configuration.Parameter.JACOCO_ENABLE).toBoolean()
                 def (jacocoReportPath, jacocoReportPaths) = getJacocoReportPaths(jacocoEnable)
-                // [VD] don't remove -U otherwise latest dependencies are not downloaded
-                def goals = "-U clean compile test -f ${pomFile} sonar:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.log.level=${LOG_LEVEL} ${jacocoReportPaths} ${jacocoReportPath}"
-                def extraGoals = jacocoEnable ? 'jacoco:report-aggregate' : ''
-
+                
+            
                 for (pomFile in context.getPomFiles()) {
                     logger.debug("pomFile: " + pomFile)
                     //do compile and scanner for all high level pom.xml file
+                    // [VD] don't remove -U otherwise latest dependencies are not downloaded
+                    def goals = "-U clean compile test -f ${pomFile} sonar:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.log.level=${LOG_LEVEL} ${jacocoReportPaths} ${jacocoReportPath}"
+                    def extraGoals = jacocoEnable ? 'jacoco:report-aggregate' : ''
                     if (isPullRequest) {
                         // no need to run unit tests for PR analysis
                         extraGoals += " -DskipTests \
