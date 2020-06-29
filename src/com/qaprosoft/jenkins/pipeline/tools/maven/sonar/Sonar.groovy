9ccc0a5d60c5414a9994fe32ce836982a8b46add
sandinosanchez <sandino320@gmail.com>
2020-06-29 11:05:40 -0300

    smallf fix

--- src/com/qaprosoft/jenkins/pipeline/tools/maven/sonar/Sonar.groovy
+++ src/com/qaprosoft/jenkins/pipeline/tools/maven/sonar/Sonar.groovy
@@ -51,7 +51,7 @@ public class Sonar extends BaseObject {
                     logger.debug("pomFile: " + pomFile)
                     //do compile and scanner for all high level pom.xml files
                     // [VD] don't remove -U otherwise latest dependencies are not downloaded
-                    def jacocoEnable = configuration.getGlobalProperty(Configuration.Parameter.JACOCO_ENABLE).toBoolean()
+                    def jacocoEnable = configuration.get(Configuration.Parameter.JACOCO_ENABLE).toBoolean()
                     def goals = "-U clean compile test -f ${pomFile} sonar:sonar"
                     def extraGoals = jacocoEnable ? 'jacoco:report-aggregate' : ''
                     def (jacocoReportPath, jacocoReportPaths) = getJacocoReportPaths(jacocoEnable)
