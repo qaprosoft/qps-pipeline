47dd06925c0fbf5c07780827a109612a0a824b84
sandinosanchez <sandino320@gmail.com>
2020-07-01 20:51:08 -0300

    removing verify maven goal

--- src/com/qaprosoft/jenkins/pipeline/tools/maven/sonar/Sonar.groovy
+++ src/com/qaprosoft/jenkins/pipeline/tools/maven/sonar/Sonar.groovy
@@ -32,7 +32,7 @@ class Sonar extends BaseObject {
                     logger.debug("pomFile: " + pomFile)
                     //do compile and scanner for all high level pom.xml file
                     // [VD] don't remove -U otherwise latest dependencies are not downloaded
-                    def goals = "-U clean compile test verify -f ${pomFile} sonar:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.log.level=${LOG_LEVEL}"
+                    def goals = "-U clean compile test -f ${pomFile} sonar:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.log.level=${LOG_LEVEL}"
                     def extraGoals = jacocoEnable ? 'jacoco:report-aggregate ${jacocoReportPaths} ${jacocoReportPath}' : ''
 
                     if (isPullRequest) {
