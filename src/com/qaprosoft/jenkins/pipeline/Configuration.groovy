50366817aed187a740ed6de1353f4b53eedcd039
sandinosanchez <sandino320@gmail.com>
2020-07-03 12:47:36 -0300

    removing fail attempt to get sonar url

--- src/com/qaprosoft/jenkins/pipeline/Configuration.groovy
+++ src/com/qaprosoft/jenkins/pipeline/Configuration.groovy
@@ -112,9 +112,7 @@ public class Configuration {
         TESTRAIL_PASSWORD("TESTRAIL_PASSWORD", "", true),
 
         QTEST_SERVICE_URL("QTEST_SERVICE_URL", ""), // "https://<CHANGE_ME>/api/v3/"
-        QTEST_ACCESS_TOKEN("QTEST_ACCESS_TOKEN", "", true),
-
-        SONAR_SERVICE_URL("SONAR_URL", "")
+        QTEST_ACCESS_TOKEN("QTEST_ACCESS_TOKEN", "", true)
 
         private final String key
         private final String value
