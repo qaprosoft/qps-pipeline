a1eeb782c2d9d9195c24823166f0109fa26a07d4
vdelendik <vadim.delendik@gmail.com>
2020-07-07 03:51:39 +0300

    misprint fix

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/TestNG.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/TestNG.groovy
@@ -443,7 +443,7 @@ public class TestNG extends Runner {
         } else {
             jenkinsJob.type = 'cron'
         }
-    }
+
         parameters.remove("job_type")
         jenkinsJob.url = jobUrl
         jenkinsJob.parameters  = new JsonBuilder(parameters).toPrettyString()
