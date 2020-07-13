15379156ddc80cd729dc614265ca60ee7c93e726
ilya-varchenya <ilya.varchenya@gmail.com>
2020-07-13 12:51:33 +0300

    choiceparam delete defining as array

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
@@ -218,7 +218,7 @@ public class TestJobFactory extends PipelineFactory {
                                 break
                             case "choiceparam":
                                 logger.info("1111" + value)
-                                choiceParam(name, Arrays.asList(value), desc)
+                                choiceParam(name, value, desc)
                                 break
                             case "booleanparam":
                                 booleanParam(name, value.toBoolean(), desc)
