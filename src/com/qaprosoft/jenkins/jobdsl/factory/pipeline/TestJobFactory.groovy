8f2f7ef8977cefbed63d335f0f593a80b00d3f56
ilya-varchenya <ilya.varchenya@gmail.com>
2020-07-14 14:03:17 +0300

    change defining of retryCountList

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/TestJobFactory.groovy
@@ -235,7 +235,7 @@ public class TestJobFactory extends PipelineFactory {
 
     protected def getRetryCountArray(currentSuite) {
         def retryCount = getSuiteParameter(0, "jenkinsDefaultRetryCount", currentSuite).toInteger()
-        List retryCountList = new ArrayList(Arrays.asList(0, 1, 2, 3))
+        def retryCountList = [0, 1, 2, 3]
         if (retryCount != 0) {
             retryCountList.add(0, retryCount)
         }
