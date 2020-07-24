798cc873b7f4f8a33810dbc9881ef5b88ffe2fc8
sandinosanchez <sandino320@gmail.com>
2020-07-23 23:41:44 -0300

    initial logic to habdle docker build onPush event

--- src/com/qaprosoft/jenkins/pipeline/runner/docker/Runner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/docker/Runner.groovy
@@ -1,13 +1,34 @@
 package com.qaprosoft.jenkins.pipeline.runner.docker
 
 import com.qaprosoft.jenkins.pipeline.runner.AbstractRunner
+import com.qaprosoft.jenkins.pipeline.Configuration
+
+import com.qaprosoft.jenkins.Utils
 
 class Runner extends AbstractRunner {
+
+	private def registry
+	private def registryCreds
+	private def releaseName
 	
+	public Runner(context) {
+		super(context)
+		registry = "${Configuration.get(Configuration.Parameter.GITHUB_ORGANIZATION)}/${Configuration.get("repo")}"
+		registryCreds = ""
+		releaseName = "1.${Configuration.get(Configuration.Parameter.BUILD_NUMBER)}-SNAPSHOT"
+		context.currentBuild.setDisplayName(releaseName)
+	}
 
 	@Override
 	public void onPush() {
-
+		context.node('docker') {
+			logger.info('DockerRunner->onPush')
+			context.timestamps {
+				getScm().clonePush()
+				context.dockerBuild(releaseName, registry, registryCreds, "Dockerfile-jdk11")
+				clean()
+			}
+		}
 	}
 
 	@Override
@@ -17,6 +38,6 @@ class Runner extends AbstractRunner {
 
 	@Override
 	public void build() {
-		
+
 	}
 }
