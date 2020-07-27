65f59fa09d7aea79a1e8744b1804bfef13413e02
sandinosanchez <sandino320@gmail.com>
2020-07-27 10:03:33 -0300

    added missing import, small fix

--- src/com/qaprosoft/jenkins/pipeline/runner/docker/Runner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/docker/Runner.groovy
@@ -16,8 +16,8 @@ class Runner extends AbstractRunner {
 	public Runner(context) {
 		super(context)
 		registry = "${Configuration.get(Configuration.Parameter.GITHUB_ORGANIZATION)}/${Configuration.get("repo")}"
-		registryCreds = ""
-		releaseName = Configuration.get('RELEASE_VERSION')
+		registryCreds = "sandino1995-docker"
+		releaseName = "${}"
 		context.currentBuild.setDisplayName(releaseName)
 	}
 
@@ -27,7 +27,7 @@ class Runner extends AbstractRunner {
 			context.timestamps {
 				logger.info('DockerRunner->onPush')
 				getScm().clonePush()
-				context.dockerDeploy(releaseName, registry, registryCreds, "Dockerfile-jdk11")
+				context.dockerDeploy(releaseName, registry, registryCreds)
 				clean()
 			}
 		}
@@ -39,9 +39,10 @@ class Runner extends AbstractRunner {
 			context.timestamps {
 				logger.info('DockerRunner->onPullRequest')
 				getScm.clonePR()
+				
 				try {
 					def image = dockerDeploy.build(releaseName, registry)
-					dockerDeploy.clean(image)
+					context.dockerDeploy.clean(image)
 				} catch (Exception e) {
 					logger.error("Something went wrong while building the docker image. \n" + e.getMessage())
 					context.currentBuild.result = BuildResult.FAILURE
