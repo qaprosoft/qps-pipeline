4b0ef79f9debf346ebe1efedd4c6aa46e3bdf2c2
sandinosanchez <sandino320@gmail.com>
2020-09-07 14:52:26 -0300

    3

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/hook/PushJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/hook/PushJobFactory.groovy
@@ -61,38 +61,36 @@ public class PushJobFactory extends PipelineFactory {
                 configure addHiddenParameter('zafiraFields', '', zafiraFields)
             }
 
-            pipelineTriggers {
-                triggers {
-                  genericTrigger {
-                   genericVariables {
-                    genericVariable {
-                     key("ref")
-                     value("\$.ref")
-                     expressionType("JSONPath") //Optional, defaults to JSONPath
-                     regexpFilter("") //Optional, defaults to empty string
-                     defaultValue("") //Optional, defaults to empty string
-                    }
-                   }
-                   // genericRequestVariables {
-                   //  genericRequestVariable {
-                   //   key("requestParameterName")
-                   //   regexpFilter("")
-                   //  }
-                   // }
-                   // genericHeaderVariables {
-                   //  genericHeaderVariable {
-                   //   key("requestHeaderName")
-                   //   regexpFilter("")
-                   //  }
-                   // }
-                   token('abc123')
-                   printContributedVariables(true)
-                   printPostContent(true)
-                   silentResponse(false)
-                   regexpFilterText("\$.ref")
-                   regexpFilterExpression("ref")
-                  }
+            triggers {
+              genericTrigger {
+               genericVariables {
+                genericVariable {
+                 key("ref")
+                 value("\$.ref")
+                 expressionType("JSONPath") //Optional, defaults to JSONPath
+                 regexpFilter("") //Optional, defaults to empty string
+                 defaultValue("") //Optional, defaults to empty string
                 }
+               }
+               // genericRequestVariables {
+               //  genericRequestVariable {
+               //   key("requestParameterName")
+               //   regexpFilter("")
+               //  }
+               // }
+               // genericHeaderVariables {
+               //  genericHeaderVariable {
+               //   key("requestHeaderName")
+               //   regexpFilter("")
+               //  }
+               // }
+               token('abc123')
+               printContributedVariables(true)
+               printPostContent(true)
+               silentResponse(false)
+               regexpFilterText("\$.ref")
+               regexpFilterExpression("ref")
+              }
             }
 
             /** Git Stuff **/
