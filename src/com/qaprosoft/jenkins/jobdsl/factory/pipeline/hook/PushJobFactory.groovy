5219a12bdaf9ae53d595f1bf0a0ebe6610542240
sandinosanchez <sandino320@gmail.com>
2020-09-07 20:05:58 -0300

    3

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/hook/PushJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/hook/PushJobFactory.groovy
@@ -61,38 +61,42 @@ public class PushJobFactory extends PipelineFactory {
                 configure addHiddenParameter('userId', 'Identifier of the user who triggered the process', userId)
                 configure addHiddenParameter('zafiraFields', '', zafiraFields)
             }
-
-            triggers {
-              genericTrigger {
-               genericVariables {
-                genericVariable {
-                 key("ref")
-                 value("\$.ref")
-                 expressionType("JSONPath") //Optional, defaults to JSONPath
-                 regexpFilter("") //Optional, defaults to empty string
-                 defaultValue("") //Optional, defaults to empty string
+            
+            properties {
+                triggers {
+                  genericTrigger {
+                   genericVariables {
+                    genericVariable {
+                     key("ref")
+                     value("\$.ref")
+                     expressionType("JSONPath") //Optional, defaults to JSONPath
+                     regexpFilter("") //Optional, defaults to empty string
+                     defaultValue("") //Optional, defaults to empty string
+                    }
+                   }
+                   // genericRequestVariables {
+                   //  genericRequestVariable {
+                   //   key("requestParameterName")
+                   //   regexpFilter("")
+                   //  }
+                   // }
+                   genericHeaderVariables {
+                    genericHeaderVariable {
+                     key("X-GitHub-Event")
+                     regexpFilter("^(push)*?")
+                    }
+                   }
+                   token('abc123')
+                   printContributedVariables(true)
+                   printPostContent(true)
+                   silentResponse(false)
+                   regexpFilterText("$ref")
+                   regexpFilterExpression("^refs/heads/master")
+                  }
                 }
-               }
-               // genericRequestVariables {
-               //  genericRequestVariable {
-               //   key("requestParameterName")
-               //   regexpFilter("")
-               //  }
-               // }
-               genericHeaderVariables {
-                genericHeaderVariable {
-                 key("X-GitHub-Event")
-                 regexpFilter("^(push)*?")
-                }
-               }
-               token('abc123')
-               printContributedVariables(true)
-               printPostContent(true)
-               silentResponse(false)
-               regexpFilterText("$ref")
-               regexpFilterExpression("^refs/heads/master")
-              }
             }
+
+            
 
             /** Git Stuff **/
 
