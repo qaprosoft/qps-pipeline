ae77ee6a9ea6a4fa0bb973eb78d719d251c1afb7
sandinosanchez <sandino320@gmail.com>
2020-09-08 18:56:07 -0300

    small fix for regExpFilter

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/hook/PushJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/hook/PushJobFactory.groovy
@@ -101,7 +101,7 @@ public class PushJobFactory extends PipelineFactory {
                    printPostContent(true)
                    silentResponse(false)
                    regexpFilterText("\$ref")
-                   regexpFilterExpression("^(refs/heads/master|master)*?")
+                   regexpFilterExpression("^(refs/heads/master|master)*?\$")
                   }
                 }
             }
