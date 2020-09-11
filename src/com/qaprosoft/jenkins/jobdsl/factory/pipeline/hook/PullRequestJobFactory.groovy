9b038857495b52da09ac669d6cbe06bcbfc31084
sandinosanchez <sandino320@gmail.com>
2020-09-11 15:54:32 -0300

    small fix

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/hook/PullRequestJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/hook/PullRequestJobFactory.groovy
@@ -86,7 +86,7 @@ public class PullRequestJobFactory extends PipelineFactory {
 
                      genericHeaderVariables {
                       genericHeaderVariable {
-                       key($headerEventName)
+                       key(headerEventName)
                        regexpFilter("")
                       }
                      }
