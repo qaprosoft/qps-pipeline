2259c74e3688df8038330830b5a8745855c16dc9
sandinosanchez <sandino320@gmail.com>
2020-09-17 16:11:07 -0300

    2

--- src/com/qaprosoft/jenkins/pipeline/tools/scm/github/GitHub.groovy
+++ src/com/qaprosoft/jenkins/pipeline/tools/scm/github/GitHub.groovy
@@ -101,7 +101,7 @@ class GitHub implements ISCM {
             def prNumber = Configuration.get('pr_number')
             def gitUrl = Configuration.resolveVars(gitHtmlUrl)
             logger.info("GitHub->clonePR\nGIT_URL: ${gitUrl}\nbranch: ${branch}")
-            context.checkout getCheckoutParams(gitUrl, "origin/merge-requests/${prNumber}/merge", ".", true, false, '+refs/merge-requests/*:refs/remotes/origin/merge-requests/*', credentialsId)
+            context.checkout getCheckoutParams(gitUrl, "origin/merge-requests/${prNumber}/heads", ".", true, false, '+refs/merge-requests/*:refs/remotes/origin/merge-requests/*', credentialsId)
         }
     }
 
