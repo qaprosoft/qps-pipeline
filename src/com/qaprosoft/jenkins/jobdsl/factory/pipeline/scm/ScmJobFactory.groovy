f382104a9f990773a6b4d223ee9a406ddb9c5e63
sandinosanchez <sandino320@gmail.com>
2020-10-05 14:43:24 -0300

    small fix

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/scm/ScmJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/scm/ScmJobFactory.groovy
@@ -1,7 +1,7 @@
 package com.qaprosoft.jenkins.jobdsl.factory.pipeline.scm
 
-import com.qaprosoft.jenkins.jobdsl.factory.PipelineFactory
 import groovy.transform.InheritConstructors
+import com.qaprosoft.jenkins.jobdsl.factory.pipeline.PipelineFactory
 
 @InheritConstructors
 class ScmJobFactory extends PipelineFactory {
